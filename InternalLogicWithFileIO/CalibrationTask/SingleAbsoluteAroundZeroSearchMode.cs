61143b93e3eb98698dc7e9f66a5e5acb1bc7a112
stefanks <solntsev@gmail.com>
2017-01-13 23:25:46 -0600

    style changes

--- InternalLogicWithFileIO/CalibrationTask/SingleAbsoluteAroundZeroSearchMode.cs
+++ InternalLogicWithFileIO/CalibrationTask/SingleAbsoluteAroundZeroSearchMode.cs
@@ -5,44 +5,44 @@ using System.Collections.Generic;
 
 namespace InternalLogicTaskLayer
 {
-    internal class SingleAbsoluteAroundZeroSearchMode : SearchMode
-    {
+	class SingleAbsoluteAroundZeroSearchMode : SearchMode
+	{
 
-        #region Private Fields
+		#region Private Fields
 
-        private string v;
-        private double value;
+		string v;
+		readonly double value;
 
-        #endregion Private Fields
+		#endregion Private Fields
 
-        #region Public Constructors
+		#region Public Constructors
 
-        public SingleAbsoluteAroundZeroSearchMode(string v, double value) : base(v)
-        {
-            this.v = v;
-            this.value = value;
-        }
+		public SingleAbsoluteAroundZeroSearchMode(string v, double value) : base(v)
+		{
+			this.v = v;
+			this.value = value;
+		}
 
-        #endregion Public Constructors
+		#endregion Public Constructors
 
-        #region Public Methods
+		#region Public Methods
 
-        public override bool Accepts(double scanPrecursorMass, double peptideMass)
-        {
+		public override bool Accepts(double scanPrecursorMass, double peptideMass)
+		{
 			return Math.Abs(scanPrecursorMass - peptideMass) < value;
-        }
+		}
 
-        public override IEnumerable<DoubleRange> GetAllowedPrecursorMassIntervals(double peptideMonoisotopicMass)
-        {
-            throw new NotImplementedException();
-        }
+		public override IEnumerable<DoubleRange> GetAllowedPrecursorMassIntervals(double peptideMonoisotopicMass)
+		{
+			yield return new DoubleRange(-value, value);
+		}
 
-        public override string SearchModeString()
-        {
+		public override string SearchModeString()
+		{
 			return "SingleAbsoluteAroundZeroSearchMode" + value;
-        }
+		}
 
-        #endregion Public Methods
+		#endregion Public Methods
 
-    }
+	}
 }
