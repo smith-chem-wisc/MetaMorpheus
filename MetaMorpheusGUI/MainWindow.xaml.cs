b5bdb7a199e0dd3408878d0579fbc114e635fd85
Stefan S <solntsev@wisc.edu>
2017-01-17 13:56:33 -0600

    whtt

--- MetaMorpheusGUI/MainWindow.xaml.cs
+++ MetaMorpheusGUI/MainWindow.xaml.cs
@@ -55,7 +55,7 @@ namespace MetaMorpheusGUI
 
             LoadSearchModesFromFile();
 
-            //xmlDBobservableCollection.Add(new XMLdb(@"C:\Users\stepa\Data\CalibrationPaperData\OrigData\uniprot-mouse-reviewed-1-17-2017.xml"));
+            //xmlDBobservableCollection.Add(new XMLdb(@"C:\Users\stepa\Data\CalibrationPaperData\OrigData\uniprot-mouse-reviewed-1-17-2017.xml.gz"));
             //xmlDBobservableCollection.Add(new XMLdb(@"C:\Users\stepa\Data\CalibrationPaperData\OrigData\cRAP-11-11-2016.xml"));
 
             //rawDataObservableCollection.Add(new RawData(@"C:\Users\stepa\Data\CalibrationPaperData\Step2\Mouse\Calib-0.1.2\04-29-13_B6_Frac9_9p5uL-Calibrated.mzML"));
@@ -78,6 +78,8 @@ namespace MetaMorpheusGUI
             //rawDataObservableCollection.Add(new RawData(@"C:\Users\stepa\Data\CalibrationPaperData\OrigData\Mouse\04-30-13_CAST_Frac7_6uL.raw"));
             //rawDataObservableCollection.Add(new RawData(@"C:\Users\stepa\Data\CalibrationPaperData\OrigData\Mouse\04-30-13_CAST_Frac8_9p5uL.raw"));
             //rawDataObservableCollection.Add(new RawData(@"C:\Users\stepa\Data\CalibrationPaperData\OrigData\Mouse\04-30-13_CAST_Frac9_9p5uL.raw"));
+
+            //rawDataObservableCollection.Add(new RawData(@"C:\Users\stepa\Data\CalibrationPaperData\OrigData\Mouse\2017-01-17-13-30-41\Task1Calibrate\04-30-13_CAST_Frac5_4uL-Calibrated.mzML"));
 
             //rawDataObservableCollection.Add(new RawData(@"C:\Users\stepa\Data\CalibrationPaperData\OrigData\Mouse\04-29-13_B6_Frac9_9p5uL-Calibrated.mzML"));
 
@@ -273,7 +275,7 @@ namespace MetaMorpheusGUI
                         break;
 
                     case ".xml":
-                    case ".xml.gz":
+                    case ".gz":
                         xmlDBobservableCollection.Add(new XMLdb(file));
                         break;
                 }
