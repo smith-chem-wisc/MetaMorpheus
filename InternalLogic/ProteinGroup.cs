47126d241af0b3b2f510eef3f809b46c5e818569
rmillikin <rmillikin@wisc.edu>
2017-01-19 18:05:57 -0600

    rewriting protein group stuff

--- InternalLogic/ProteinGroup.cs
+++ InternalLogic/ProteinGroup.cs
@@ -11,7 +11,7 @@ namespace InternalLogicEngineLayer
         #region Public Fields
 
         public readonly bool isDecoy;
-        public readonly double proteinGroupScore;
+        public double proteinGroupScore;
 
         #endregion Public Fields
 
@@ -21,8 +21,8 @@ namespace InternalLogicEngineLayer
         {
             Proteins = proteins;
             PsmList = psmList;
-            PeptideList = new List<CompactPeptide>();
-            UniquePeptideList = new List<CompactPeptide>();
+            PeptideList = new HashSet<CompactPeptide>();
+            UniquePeptideList = new HashSet<CompactPeptide>();
             proteinGroupScore = 0;
             QValue = 0;
 
@@ -54,7 +54,29 @@ namespace InternalLogicEngineLayer
 
         internal ProteinGroup(HashSet<Protein> proteins, HashSet<CompactPeptide> peptides, HashSet<CompactPeptide> allUniquePeptides)
         {
-            
+            Proteins = proteins;
+            PeptideList = peptides;
+            UniquePeptideList = new HashSet<CompactPeptide>();
+            PsmList = null;
+            proteinGroupScore = 0;
+            QValue = 0;
+            isDecoy = false;
+
+            // if any of the proteins in the protein group are decoys, the protein group is a decoy
+            foreach (var protein in proteins)
+            {
+                if (protein.IsDecoy)
+                    isDecoy = true;
+            }
+
+            // build list of unique peptides associated with the protein group
+            foreach (var peptide in peptides)
+            {
+                if (allUniquePeptides.Contains(peptide))
+                {
+                    UniquePeptideList.Add(peptide);
+                }
+            }
         }
 
         #endregion Internal Constructors
@@ -81,9 +103,9 @@ namespace InternalLogicEngineLayer
         }
 
         public HashSet<Protein> Proteins { get; private set; }
-        public List<NewPsmWithFdr> PsmList { get; private set; }
-        public List<CompactPeptide> PeptideList { get; private set; }
-        public List<CompactPeptide> UniquePeptideList { get; private set; }
+        public List<NewPsmWithFdr> PsmList { get; set; }
+        public HashSet<CompactPeptide> PeptideList { get; set; }
+        public HashSet<CompactPeptide> UniquePeptideList { get; set; }
         public double QValue { get; set; }
         public int cumulativeTarget { get; set; }
         public int cumulativeDecoy { get; set; }
@@ -151,6 +173,20 @@ namespace InternalLogicEngineLayer
             sb.Append("\t");
 
             return sb.ToString();
+        }
+
+        public void scoreThisProteinGroup()
+        {
+            if (UniquePeptideList.Any())
+            {
+                foreach (var psm in PsmList)
+                {
+                    proteinGroupScore += psm.thisPSM.Score;
+                }
+            }
+
+            else
+                proteinGroupScore = 0;
         }
 
         #endregion Public Methods
