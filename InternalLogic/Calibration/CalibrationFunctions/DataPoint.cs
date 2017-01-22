08f9e13fcd9cdb8689dd7fd6a0c4385675f94261
stefanks <solntsev@gmail.com>
2017-01-22 14:08:11 -0600

    packages

--- InternalLogic/Calibration/CalibrationFunctions/DataPoint.cs
+++ InternalLogic/Calibration/CalibrationFunctions/DataPoint.cs
@@ -6,19 +6,9 @@
         public readonly double rt;
         public readonly int msnOrder;
         public readonly double intensity;
-        public readonly int selectedIonGuessChargeStateGuess;
-        public readonly double isolationMZ;
         public readonly double totalIonCurrent;
         public readonly double injectionTime;
-        public readonly double relativeMZ;
 
-        public DataPoint(double mz, double rt, int msnOrder, double intensity, double totalIonCurrent, double injectionTime, int selectedIonGuessChargeStateGuess, double isolationMZ, double relativeMZ)
-			:this(mz,  rt,  msnOrder,  intensity,  totalIonCurrent,  injectionTime)
-		{
-			this.selectedIonGuessChargeStateGuess = selectedIonGuessChargeStateGuess;
-			this.isolationMZ = isolationMZ;
-			this.relativeMZ = relativeMZ;
-		}
         public DataPoint(double mz, double rt, int msnOrder, double intensity, double totalIonCurrent, double injectionTime)
         {
             this.mz = mz;
