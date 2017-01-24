ae4b7eb0742a48b03d5926d0381295d16bbfa918
rmillikin <rmillikin@wisc.edu>
2017-01-24 16:29:39 -0600

    scoring/fdr tests

--- Test/RobTest.cs
+++ Test/RobTest.cs
@@ -29,16 +29,19 @@ namespace Test
             var totalVirtualPeptideList = new HashSet<PeptideWithSetModifications>();
 
             var p1 = new Protein(sequence1, "1", temp1, temp3, temp3, null, "Test1", "TestFullName1", 0, false, false);
-            var p2 = new Protein(sequence2, "2", temp1, temp3, temp3, null, "Test2", "TestFullName2", 0, false, false);
+            var p2 = new Protein(sequence2, "2", temp1, temp3, temp3, null, "DECOY_Test2", "DECOY_TestFullName2", 0, true, false);
             var p3 = new Protein(sequence3, "3", temp1, temp3, temp3, null, "Test3", "TestFullName3", 0, false, false);
 
             IEnumerable<PeptideWithPossibleModifications> digestedList1 = p1.Digest(protease, 2, InitiatorMethionineBehavior.Variable);
             IEnumerable<PeptideWithPossibleModifications> digestedList2 = p2.Digest(protease, 2, InitiatorMethionineBehavior.Variable);
             IEnumerable<PeptideWithPossibleModifications> digestedList3 = p3.Digest(protease, 2, InitiatorMethionineBehavior.Variable);
+            IEnumerable<PeptideWithSetModifications> peptides1 = null;
+            IEnumerable<PeptideWithSetModifications> peptides2 = null;
+            IEnumerable<PeptideWithSetModifications> peptides3 = null;
 
             foreach (var protein in digestedList1)
             {
-                IEnumerable<PeptideWithSetModifications> peptides1 = protein.GetPeptideWithSetModifications(temp2, 4098, 3);
+                peptides1 = protein.GetPeptideWithSetModifications(temp2, 4098, 3);
 
                 foreach (var peptide in peptides1)
                     totalVirtualPeptideList.Add(peptide);
@@ -46,7 +49,7 @@ namespace Test
 
             foreach (var protein in digestedList2)
             {
-                IEnumerable<PeptideWithSetModifications> peptides2 = protein.GetPeptideWithSetModifications(temp2, 4098, 3);
+                peptides2 = protein.GetPeptideWithSetModifications(temp2, 4098, 3);
 
                 foreach (var peptide in peptides2)
                     totalVirtualPeptideList.Add(peptide);
@@ -54,7 +57,7 @@ namespace Test
 
             foreach (var protein in digestedList3)
             {
-                IEnumerable<PeptideWithSetModifications> peptides3 = protein.GetPeptideWithSetModifications(temp2, 4098, 3);
+                peptides3 = protein.GetPeptideWithSetModifications(temp2, 4098, 3);
 
                 foreach (var peptide in peptides3)
                     totalVirtualPeptideList.Add(peptide);
@@ -95,11 +98,18 @@ namespace Test
                     dictionary.Add(peptides[i], virtualPeptideSets[i]);
                 }
             }
-            List<ProteinGroup> pg = new List<ProteinGroup>();
+            List<ProteinGroup> proteinGroups = new List<ProteinGroup>();
             AnalysisEngine ae = new AnalysisEngine(null, dictionary, null, null, null, null, null, null, null, null, null, null, null, true, 0, 0, false, new List<ProductType> { ProductType.B, ProductType.Y });
 
+            Dictionary<CompactPeptide, HashSet<PeptideWithSetModifications>> initialDictionary = new Dictionary<CompactPeptide, HashSet<PeptideWithSetModifications>>();
+            foreach(var kvp in dictionary)
+            {
+                initialDictionary.Add(kvp.Key, kvp.Value);
+            }
+
+
             // apply parsimony to initial dictionary
-            ae.ApplyProteinParsimony(out pg);
+            ae.ApplyProteinParsimony(out proteinGroups);
 
             var parsimonyProteinList = new List<Protein>();
             string[] parsimonyBaseSequences = new string[3];
@@ -121,50 +131,88 @@ namespace Test
                 }
             }
 
-            /*
+            
+
+            List<NewPsmWithFdr> psms = new List<NewPsmWithFdr>();
+
+            foreach(var kvp in dictionary)
+            {
+                foreach(var peptide in kvp.Value)
+                {
+                    HashSet<PeptideWithSetModifications> hashSet = new HashSet<PeptideWithSetModifications>();
+                    hashSet.Add(peptide);
+
+                    switch (peptide.BaseSequence)
+                    {
+                        case "AK": psms.Add(new NewPsmWithFdr(new PSMwithProteinHashSet(new ClassicSpectrumMatch(peptide, null, 0, 0, 0, 0, 0, 0, 0, 0, 1), hashSet, null, null, null), 1, 0, 0.29)); break;
+                        case "CK": psms.Add(new NewPsmWithFdr(new PSMwithProteinHashSet(new ClassicSpectrumMatch(peptide, null, 0, 0, 0, 0, 0, 0, 0, 0, 9), hashSet, null, null, null), 1, 0, 0.0)); break;
+                        case "BK": psms.Add(new NewPsmWithFdr(new PSMwithProteinHashSet(new ClassicSpectrumMatch(peptide, null, 0, 0, 0, 0, 0, 0, 0, 0, 8), hashSet, null, null, null), 1, 0, 0.0)); break;
+                        case "AKCK": psms.Add(new NewPsmWithFdr(new PSMwithProteinHashSet(new ClassicSpectrumMatch(peptide, null, 0, 0, 0, 0, 0, 0, 0, 0, 7), hashSet, null, null, null), 1, 0, 0.0)); break;
+                        case "CKBK": psms.Add(new NewPsmWithFdr(new PSMwithProteinHashSet(new ClassicSpectrumMatch(peptide, null, 0, 0, 0, 0, 0, 0, 0, 0, 6), hashSet, null, null, null), 1, 0, 0.0)); break;
+                        case "AKCKBK": psms.Add(new NewPsmWithFdr(new PSMwithProteinHashSet(new ClassicSpectrumMatch(peptide, null, 0, 0, 0, 0, 0, 0, 0, 0, 5), hashSet, null, null, null), 1, 0, 0.0)); break;
+                        case "DK": psms.Add(new NewPsmWithFdr(new PSMwithProteinHashSet(new ClassicSpectrumMatch(peptide, null, 0, 0, 0, 0, 0, 0, 0, 0, 4), hashSet, null, null, null), 1, 0, 0.2)); break;
+                        case "DKCK": psms.Add(new NewPsmWithFdr(new PSMwithProteinHashSet(new ClassicSpectrumMatch(peptide, null, 0, 0, 0, 0, 0, 0, 0, 0, 3), hashSet, null, null, null), 1, 0, 0.4)); break;
+                        case "AAAAK": psms.Add(new NewPsmWithFdr(new PSMwithProteinHashSet(new ClassicSpectrumMatch(peptide, null, 0, 0, 0, 0, 0, 0, 0, 0, 2), hashSet, null, null, null), 1, 0, 0.33)); break;
+                    }
+                }
+            }
+
+            ae.ScoreProteinGroups(proteinGroups, psms);
+            ae.DoProteinFdr(proteinGroups);
+
             // prints initial dictionary
             List<Protein> proteinList = new List<Protein>();
 
-            Console.WriteLine("----Initial Dictionary----");
-            Console.WriteLine("PEPTIDE\t\t\tPROTEIN\t\t\tPeptideWithSetModifications");
+            System.Console.WriteLine("----Initial Dictionary----");
+            System.Console.WriteLine("PEPTIDE\t\t\tPROTEIN\t\t\tPeptideWithSetModifications");
             foreach (var kvp in initialDictionary)
             {
                 proteinList = new List<Protein>();
-                Console.Write(string.Join("", kvp.Key.BaseSequence.Select(b => char.ConvertFromUtf32(b))) + "  \t\t\t  ");
+                System.Console.Write(string.Join("", kvp.Key.BaseSequence.Select(b => char.ConvertFromUtf32(b))) + "  \t\t\t  ");
                 foreach (var peptide in kvp.Value)
                 {
-                    if (!proteinList.Contains(peptide.protein))
+                    if (!proteinList.Contains(peptide.Protein))
                     {
-                        Console.Write(peptide.protein.BaseSequence + " ;; ");
-                        proteinList.Add(peptide.protein);
+                        System.Console.Write(peptide.Protein.BaseSequence + " ;; ");
+                        proteinList.Add(peptide.Protein);
                     }
                 }
-                Console.WriteLine();
+                System.Console.WriteLine();
             }
 
             // prints parsimonious dictionary
-            Console.WriteLine("----Parsimonious Dictionary----");
-            Console.WriteLine("PEPTIDE\t\t\tPROTEIN\t\t\tPeptideWithSetModifications");
-            foreach (var kvp in parsimonyTest)
+            System.Console.WriteLine("----Parsimonious Dictionary----");
+            System.Console.WriteLine("PEPTIDE\t\t\tPROTEIN\t\t\tPeptideWithSetModifications");
+            foreach (var kvp in dictionary)
             {
                 proteinList = new List<Protein>();
-                Console.Write(string.Join("", kvp.Key.BaseSequence.Select(b => char.ConvertFromUtf32(b))) + "  \t\t\t  ");
+                System.Console.Write(string.Join("", kvp.Key.BaseSequence.Select(b => char.ConvertFromUtf32(b))) + "  \t\t\t  ");
                 foreach (var peptide in kvp.Value)
                 {
-                    if (!proteinList.Contains(peptide.protein))
+                    if (!proteinList.Contains(peptide.Protein))
                     {
-                        Console.Write(peptide.protein.BaseSequence + " ;; ");
-                        proteinList.Add(peptide.protein);
+                        System.Console.Write(peptide.Protein.BaseSequence + " ;; ");
+                        proteinList.Add(peptide.Protein);
                     }
                 }
-                Console.WriteLine();
+                System.Console.WriteLine();
             }
-            */
+
+
+            // prints protein groups after scoring/fdr
+            System.Console.WriteLine(ProteinGroup.TabSeparatedHeader);
+            foreach (var proteinGroup in proteinGroups)
+            {
+                System.Console.WriteLine(proteinGroup);
+            }
+
 
             Assert.That(parsimonyProteinList.Count == 3);
             Assert.That(parsimonyBaseSequences.Contains(sequence1));
             Assert.That(parsimonyBaseSequences.Contains(sequence2));
             Assert.That(parsimonyBaseSequences.Contains(sequence3));
+            Assert.That(proteinGroups.Count == 2);
+            Assert.That(proteinGroups.First().proteinGroupScore > 10);
         }
 
         #endregion Public Methods
