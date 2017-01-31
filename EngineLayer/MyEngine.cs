83d624b45f5c036630397c8dd7dd98df2aa74bce
Stefan S <solntsev@wisc.edu>
2017-01-31 16:30:34 -0600

    mkay

--- EngineLayer/MyEngine.cs
+++ EngineLayer/MyEngine.cs
@@ -5,6 +5,7 @@ using System;
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.IO;
+using System.Linq;
 using System.Net;
 using System.Reflection;
 
@@ -24,6 +25,8 @@ namespace EngineLayer
         private static readonly string elementsLocation = Path.Combine("Data", @"elements.dat");
         private static readonly string unimodLocation = Path.Combine("Data", @"unimod_tables.xml");
         private static readonly string uniprotLocation = Path.Combine("Data", @"ptmlist.txt");
+
+        private static readonly double[] mms = new double[] { 1.0025, 2.005, 3.0075, 4.010 };
 
         #endregion Private Fields
 
@@ -83,10 +86,20 @@ namespace EngineLayer
         {
             foreach (var heh in myMSDataFile)
             {
-                int? chargeState;
-                if (heh.TryGetSelectedIonGuessChargeStateGuess(out chargeState) && chargeState.HasValue)
+                if (heh.MsnOrder == 2)
                 {
-                    yield return new LocalMS2Scan(heh);
+                    int kk;
+                    heh.TryGetPrecursorOneBasedScanNumber(out kk);
+                    var uu = myMSDataFile.GetOneBasedScan(kk);
+                    double isolationMz;
+                    heh.TryGetIsolationMZ(out isolationMz);
+                    int? monoisotopicPrecursorChargehere;
+                    int mc;
+                    if (heh.TryGetSelectedIonGuessChargeStateGuess(out monoisotopicPrecursorChargehere) && monoisotopicPrecursorChargehere.HasValue)
+                        mc = monoisotopicPrecursorChargehere.Value;
+                    else
+                        mc = GuessCharge(uu.MassSpectrum.NewSpectrumExtract(isolationMz - 2.1, isolationMz + 2.1));
+                    yield return new LocalMS2Scan(heh, mc);
                 }
             }
         }
@@ -127,6 +140,27 @@ namespace EngineLayer
         #endregion Protected Methods
 
         #region Private Methods
+
+        private static int GuessCharge(IMzSpectrum<MzPeak> mzSpectrum)
+        {
+            double tolHere = 0.01;
+            int[] chargeCount = new int[4]; // charges 1,2,3,4
+            for (int i = 0; i < mzSpectrum.Count; i++)
+                for (int j = i + 1; j < mzSpectrum.Count; j++)
+                {
+                    for (int charge = 1; charge <= 4; charge++)
+                    {
+                        for (int isotope = 0; isotope < 4; isotope++)
+                        {
+                            if (Math.Abs(mzSpectrum.XArray[j] - mzSpectrum.XArray[i] - mms[isotope] / charge) < tolHere)
+                            {
+                                chargeCount[charge - 1]++;
+                            }
+                        }
+                    }
+                }
+            return Array.IndexOf(chargeCount, chargeCount.Max()) + 1;
+        }
 
         private void startingSingleEngine()
         {
