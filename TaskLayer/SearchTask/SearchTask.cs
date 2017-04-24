5e01046c047572db77432950df9815ba574dd85b
stefanks <solntsev@wisc.edu>
2017-04-24 13:21:04 -0500

    Fix

--- TaskLayer/SearchTask/SearchTask.cs
+++ TaskLayer/SearchTask/SearchTask.cs
@@ -193,7 +193,7 @@ namespace TaskLayer
 
             Status("Loading proteins...", new List<string> { taskId });
             Dictionary<string, Modification> um;
-            var proteinList = dbFilenameList.SelectMany(b => LoadProteinDb(b.FileName, true, localizeableModifications, b.IsContaminant, null, out um)).ToList();
+            var proteinList = dbFilenameList.SelectMany(b => LoadProteinDb(b.FileName, true, localizeableModifications, b.IsContaminant, out um)).ToList();
 
             List<CompactPeptide> peptideIndex = null;
             float[] keys = null;
