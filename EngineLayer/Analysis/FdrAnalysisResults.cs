dfe2e94946866b749ba8b4553777332fe92d8907
stefanks <solntsev@wisc.edu>
2017-07-05 10:18:59 -0500

    fixup

--- EngineLayer/Analysis/FdrAnalysisResults.cs
+++ EngineLayer/Analysis/FdrAnalysisResults.cs
@@ -1,17 +1,9 @@
-using System.Collections.Generic;
-using System.Linq;
 using System.Text;
 
 namespace EngineLayer.Analysis
 {
     public class FdrAnalysisResults : MetaMorpheusEngineResults
     {
-
-        #region Public Fields
-
-        public List<PsmParent>[] allResultingPeptides;
-
-        #endregion Public Fields
 
         #region Public Constructors
 
@@ -27,7 +19,6 @@ namespace EngineLayer.Analysis
         {
             var sb = new StringBuilder();
             sb.Append(base.ToString());
-            sb.AppendLine("All target PSMS within 1% FDR: " + string.Join(", ", allResultingPeptides.Select(b => b.Count(c => c.FdrInfo.QValue <= 0.01 && !c.Pli.IsDecoy))));
             return sb.ToString();
         }
 
