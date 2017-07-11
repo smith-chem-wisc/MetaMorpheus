7cdbefa9ee3c10ae8cd4b3c43caf32e0140e595c
lonelu <lonelur@gmail.com>
2017-07-11 12:11:19 -0500

    XLResults remove useless code

--- EngineLayer/CrosslinkAnalysis/CrosslinkAnalysisResults.cs
+++ EngineLayer/CrosslinkAnalysis/CrosslinkAnalysisResults.cs
@@ -6,14 +6,6 @@ namespace EngineLayer.CrosslinkAnalysis
 {
     public class CrosslinkAnalysisResults : MetaMorpheusEngineResults
     {
-
-        #region Internal Fields
-
-        internal Dictionary<string, int> allModsOnPeptides;
-        internal Dictionary<string, int> allModsSeen;
-
-        #endregion Internal Fields
-
         #region Public Constructors
 
         public CrosslinkAnalysisResults(CrosslinkAnalysisEngine s) : base(s)
