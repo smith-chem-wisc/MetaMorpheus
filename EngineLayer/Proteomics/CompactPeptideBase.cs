c8e53668e8d2d59bbed2a9daf59ea4ab2ffad1ef
Zachary Rolfs <zrolfs@wisc.edu>
2017-08-08 15:35:02 -0500

    Moved ProductMassesMightHaveDuplicatesAndNaNs back from CP to CPB

--- EngineLayer/Proteomics/CompactPeptideBase.cs
+++ EngineLayer/Proteomics/CompactPeptideBase.cs
@@ -28,6 +28,71 @@ namespace EngineLayer
 
         #region Public Methods
 
+        public double[] ProductMassesMightHaveDuplicatesAndNaNs(List<ProductType> productTypes)
+        {
+            int massLen = 0;
+            bool containsAdot = productTypes.Contains(ProductType.Adot);
+            bool containsB = productTypes.Contains(ProductType.B);
+            bool containsBnoB1 = productTypes.Contains(ProductType.BnoB1ions);
+            bool containsC = productTypes.Contains(ProductType.C);
+            bool containsX = productTypes.Contains(ProductType.X);
+            bool containsY = productTypes.Contains(ProductType.Y);
+            bool containsZdot = productTypes.Contains(ProductType.Zdot);
+
+            if (containsAdot)
+                throw new NotImplementedException();
+            if (containsBnoB1)
+                massLen += NTerminalMasses.Length - 1;
+            if (containsB)
+                massLen += NTerminalMasses.Length;
+            if (containsC)
+                massLen += NTerminalMasses.Length;
+            if (containsX)
+                throw new NotImplementedException();
+            if (containsY)
+                massLen += CTerminalMasses.Length;
+            if (containsZdot)
+                massLen += CTerminalMasses.Length;
+
+            double[] massesToReturn = new double[massLen];
+
+            int i = 0;
+            for (int j = 0; j < NTerminalMasses.Length; j++)
+            {
+                var hm = NTerminalMasses[j];
+                if (containsBnoB1 && j > 0)
+                {
+                    massesToReturn[i] = hm;
+                    i++;
+                }
+                if (containsB)
+                {
+                    massesToReturn[i] = hm;
+                    i++;
+                }
+                if (containsC)
+                {
+                    massesToReturn[i] = hm + nitrogenAtomMonoisotopicMass + 3 * hydrogenAtomMonoisotopicMass;
+                    i++;
+                }
+            }
+            for (int j = 0; j < CTerminalMasses.Length; j++)
+            {
+                var hm = CTerminalMasses[j];
+                if (containsY)
+                {
+                    massesToReturn[i] = hm + waterMonoisotopicMass;
+                    i++;
+                }
+                if (containsZdot)
+                {
+                    massesToReturn[i] = hm + oxygenAtomMonoisotopicMass - nitrogenAtomMonoisotopicMass;
+                    i++;
+                }
+            }
+            return massesToReturn;
+        }
+
         public override bool Equals(object obj)
         {
             var cp = obj as CompactPeptideBase;
