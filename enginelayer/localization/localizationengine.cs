0de27a1570820f377b3d168c3a8975d81d4d73f1
Zachary Rolfs <zrolfs@wisc.edu>
2017-08-23 11:34:15 -0500

    Fix issue #563

---
+++ enginelayer/localization/localizationengine.cs
@@ -0,0 +1,82 @@
+﻿using MassSpectrometry;
+using MzLibUtil;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace EngineLayer
+{
+    public class LocalizationEngine : MetaMorpheusEngine
+    {
+        #region Private Fields
+
+        private readonly IEnumerable<Psm> allResultingIdentifications;
+        private readonly List<ProductType> lp;
+        private readonly IMsDataFile<IMsDataScan<IMzSpectrum<IMzPeak>>> myMsDataFile;
+        private readonly Tolerance fragmentTolerance;
+        private readonly bool addCompIons;
+
+        #endregion Private Fields
+
+        #region Public Constructors
+
+        public LocalizationEngine(IEnumerable<Psm> allResultingIdentifications, List<ProductType> lp, IMsDataFile<IMsDataScan<IMzSpectrum<IMzPeak>>> myMsDataFile, Tolerance fragmentTolerance, List<string> nestedIds, bool addCompIons) : base(nestedIds)
+        {
+            this.allResultingIdentifications = allResultingIdentifications;
+            this.lp = lp;
+            this.myMsDataFile = myMsDataFile;
+            this.fragmentTolerance = fragmentTolerance;
+            this.addCompIons = addCompIons;
+        }
+
+        #endregion Public Constructors
+
+        #region Protected Methods
+
+        protected override MetaMorpheusEngineResults RunSpecific()
+        {
+            TerminusType terminusType = ProductTypeToTerminusType.IdentifyTerminusType(lp);
+            foreach (var ok in allResultingIdentifications)
+            {
+                var matchedIonDictPositiveIsMatch = new Dictionary<ProductType, double[]>();
+                var theScan = myMsDataFile.GetOneBasedScan(ok.ScanNumber);
+                double thePrecursorMass = ok.ScanPrecursorMass;
+                foreach (var huh in lp)
+                {
+                    var ionMasses = ok.CompactPeptides.First().Key.ProductMassesMightHaveDuplicatesAndNaNs(new List<ProductType> { huh });
+                    Array.Sort(ionMasses);
+                    double[] matchedIonMassesListPositiveIsMatch = new double[ionMasses.Length];
+                    Psm.MatchIons(theScan, fragmentTolerance, ionMasses, matchedIonMassesListPositiveIsMatch, this.addCompIons, thePrecursorMass, this.lp);
+                    matchedIonDictPositiveIsMatch.Add(huh, matchedIonMassesListPositiveIsMatch);
+                }
+
+                ok.MatchedIonDictPositiveIsMatch = new MatchedIonMassesListPositiveIsMatch(matchedIonDictPositiveIsMatch);
+
+                if (ok.NumDifferentCompactPeptides == 1)
+                {
+                    if (ok.FullSequence == null)
+                        continue;
+
+                    var representative = ok.CompactPeptides.First().Value.Item2.First();
+
+                    var localizedScores = new List<double>();
+                    for (int indexToLocalize = 0; indexToLocalize < representative.Length; indexToLocalize++)
+                    {
+                        PeptideWithSetModifications localizedPeptide = representative.Localize(indexToLocalize, ok.ScanPrecursorMass - representative.MonoisotopicMass);
+
+                        var gg = localizedPeptide.CompactPeptide(terminusType).ProductMassesMightHaveDuplicatesAndNaNs(lp);
+                        Array.Sort(gg);
+                        double[] matchedIonMassesListPositiveIsMatch = new double[gg.Length];
+                        var score = Psm.MatchIons(theScan, fragmentTolerance, gg, matchedIonMassesListPositiveIsMatch, this.addCompIons, thePrecursorMass, this.lp);
+                        localizedScores.Add(score);
+                    }
+
+                    ok.LocalizedScores = localizedScores;
+                }
+            }
+            return new LocalizationEngineResults(this);
+        }
+
+        #endregion Protected Methods
+    }
+}
