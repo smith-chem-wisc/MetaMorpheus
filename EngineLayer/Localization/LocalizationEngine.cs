3c821abc83ddba425753d8c4ce7b92f1cece2064
Zachary Rolfs <zrolfs@wisc.edu>
2017-08-28 11:11:39 -0500

    matchedIonMassesOnlyMatches

--- EngineLayer/Localization/LocalizationEngine.cs
+++ EngineLayer/Localization/LocalizationEngine.cs
@@ -48,7 +48,8 @@ namespace EngineLayer
                     Array.Sort(ionMasses);
                     double[] matchedIonMassesListPositiveIsMatch = new double[ionMasses.Length];
                     MatchIons(theScan, fragmentTolerance, ionMasses, matchedIonMassesListPositiveIsMatch, this.addCompIons, thePrecursorMass, this.lp);
-                    matchedIonDictPositiveIsMatch.Add(huh, matchedIonMassesListPositiveIsMatch);
+                    double[] matchedIonMassesOnlyMatches = new List<double>(matchedIonMassesListPositiveIsMatch).Where(m => m > 0).ToArray();
+                    matchedIonDictPositiveIsMatch.Add(huh, matchedIonMassesOnlyMatches);
                 }
 
                 ok.MatchedIonDictPositiveIsMatch = new MatchedIonMassesListPositiveIsMatch(matchedIonDictPositiveIsMatch);
