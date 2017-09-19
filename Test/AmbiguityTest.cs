8eed64c2cd22c868f578dec71082d9345cd0f37f
zrolfs <zrolfs@wisc.edu>
2017-09-19 11:41:41 -0500

    Fix ResolveAmbiguities+Unit test (#666)
    * Bring back some old GUI features that got swept away...
    
    * revert unnecessary code
    
    * Impliment a horrible semi-trypsin search to show mine is (hopefully) better
    
    * I hear and I obey, Codacy
    
    * Revert "Bring back some old GUI features that got swept away..."
    
    This reverts commit 0b39ae10b952f27cf75ffd5be93fe0b6b96b96e3.
    
    * Revert "revert unnecessary code"
    
    This reverts commit 707fbf7ceb5359f158af38d34399154aebcef8cf.
    
    * Revert "Revert "revert unnecessary code""
    
    This reverts commit f2beda04543c02e17fad39340ef7f235119397a8.
    
    * Revert "Revert "Bring back some old GUI features that got swept away...""
    
    This reverts commit 781aa21344c6d791d84426647192e07d21b11811.
    
    * Improve unit test
    
    * Made unit test even more betterer
    
    * Change proteolysis handling for semi, update unit tests
    
    * Hack away
    
    * Remove terminusType from searchtask and pass in List<ProductTypes> instead.
    
    * Removed FullMax specificities and made the whole thing feel a whole lot hackier.
    
    * test commit
    
    * revert hacky changes
    
    * Clean up after revert conflicts
    
    * Stefan pls.
    
    * LINQ u so fancy
    
    * Codacy why are you like this.
    
    * Oops
    
    * yup
    
    * Oops
    
    * Fix ResolveAmbiguities+Unit test
    
    * Update AmbiguityTest.cs

---
+++ Test/AmbiguityTest.cs
@@ -0,0 +1,73 @@
+﻿using Chemistry;
+using EngineLayer;
+using EngineLayer.ClassicSearch;
+using EngineLayer.Indexing;
+using EngineLayer.ModernSearch;
+using MassSpectrometry;
+using MzLibUtil;
+using NUnit.Framework;
+using Proteomics;
+using System.Collections.Generic;
+using System.Linq;
+using TaskLayer;
+
+namespace Test
+{
+    [TestFixture]
+    internal static class AmbiguityTest
+    {
+        [Test]
+        public static void TestResolveAmbiguities()
+        {
+            CommonParameters CommonParameters = new CommonParameters
+            {
+                DigestionParams = new DigestionParams
+                {
+                    Protease = new Protease("Custom Protease", new List<string> { "K" }, new List<string>(), TerminusType.C, CleavageSpecificity.Full, null, null, null),
+                    MinPeptideLength = null,
+                },
+                ConserveMemory = false,
+                ScoreCutoff = 1,
+                ReportAllAmbiguity = false
+            };
+
+            var myMsDataFile = new TestDataFile();
+            var variableModifications = new List<ModificationWithMass>();
+            var fixedModifications = new List<ModificationWithMass>();
+            var proteinList = new List<Protein> { new Protein("MNNKNKNKQQQ", "Prot1", null, null, null, null, null, true), new Protein("MNNNKQQQ", "Prot2") };
+
+            var searchModes = new SinglePpmAroundZeroSearchMode(5);
+
+            bool DoPrecursorDeconvolution = true;
+            bool UseProvidedPrecursorInfo = true;
+            double DeconvolutionIntensityRatio = 4;
+            int DeconvolutionMaxAssumedChargeState = 10;
+            Tolerance DeconvolutionMassTolerance = new PpmTolerance(5);
+
+            var listOfSortedms2Scans = MetaMorpheusTask.GetMs2Scans(myMsDataFile, null, DoPrecursorDeconvolution, UseProvidedPrecursorInfo, DeconvolutionIntensityRatio, DeconvolutionMaxAssumedChargeState, DeconvolutionMassTolerance).OrderBy(b => b.PrecursorMass).ToArray();
+
+            Psm[] allPsmsArrayt = new Psm[listOfSortedms2Scans.Length];
+            Psm[] allPsmsArrayf = new Psm[listOfSortedms2Scans.Length];
+            new ClassicSearchEngine(allPsmsArrayt, listOfSortedms2Scans, variableModifications, fixedModifications, proteinList, new List<ProductType> { ProductType.B, ProductType.Y }, searchModes, false, CommonParameters, new List<string>()).Run();
+            new ClassicSearchEngine(allPsmsArrayf, listOfSortedms2Scans, variableModifications, fixedModifications, proteinList, new List<ProductType> { ProductType.B, ProductType.Y }, searchModes, false, CommonParameters, new List<string>()).Run();
+
+            var haht = (SequencesToActualProteinPeptidesEngineResults)new SequencesToActualProteinPeptidesEngine(new List<Psm> { allPsmsArrayt[0] }, proteinList, fixedModifications, variableModifications, new List<ProductType> { ProductType.B, ProductType.Y }, new List<DigestionParams> { CommonParameters.DigestionParams }, true, new List<string>()).Run();
+            var hahf = (SequencesToActualProteinPeptidesEngineResults)new SequencesToActualProteinPeptidesEngine(new List<Psm> { allPsmsArrayf[0] }, proteinList, fixedModifications, variableModifications, new List<ProductType> { ProductType.B, ProductType.Y }, new List<DigestionParams> { CommonParameters.DigestionParams }, CommonParameters.ReportAllAmbiguity, new List<string>()).Run();
+
+            foreach (var huh in allPsmsArrayt)
+                if (huh != null && huh.MostProbableProteinInfo == null)
+                    huh.MatchToProteinLinkedPeptides(haht.CompactPeptideToProteinPeptideMatching);
+
+            foreach (var huh in allPsmsArrayf)
+                if (huh != null && huh.MostProbableProteinInfo == null)
+                    huh.MatchToProteinLinkedPeptides(hahf.CompactPeptideToProteinPeptideMatching);
+
+            Assert.AreEqual("QQQ", allPsmsArrayt[0].BaseSequence);
+            Assert.AreEqual("QQQ", allPsmsArrayf[0].BaseSequence);
+            Assert.IsTrue(allPsmsArrayt[0].ProteinLength == null);
+            Assert.IsTrue(allPsmsArrayf[0].ProteinLength == 8);
+            Assert.IsTrue(allPsmsArrayt[0].OneBasedStartResidueInProtein == null);
+            Assert.IsTrue(allPsmsArrayf[0].OneBasedStartResidueInProtein == 6);
+        }
+    }
+}
