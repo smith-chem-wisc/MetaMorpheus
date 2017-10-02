3602c9840e20fc94f5a0666962bd0580121d51a4
stefanks <solntsev@wisc.edu>
2017-10-02 14:54:34 -0500

    gggg

---
+++ TaskLayer/CalibrationParameters.cs
@@ -0,0 +1,31 @@
+﻿using MzLibUtil;
+
+namespace TaskLayer
+{
+    public class CalibrationParameters
+    {
+        #region Public Constructors
+
+        public CalibrationParameters()
+        {
+            NonLinearCalibration = true;
+            WriteIntermediateFiles = false;
+            MinMS1IsotopicPeaksNeededForConfirmedIdentification = 3;
+            MinMS2IsotopicPeaksNeededForConfirmedIdentification = 2;
+            NumFragmentsNeededForEveryIdentification = 10;
+        }
+
+        #endregion Public Constructors
+
+        #region Public Properties
+        
+        public bool NonLinearCalibration { get; set; }
+        public bool WriteIntermediateFiles { get; set; }
+
+        public int MinMS1IsotopicPeaksNeededForConfirmedIdentification { get; set; }
+        public int MinMS2IsotopicPeaksNeededForConfirmedIdentification { get; set; }
+        public int NumFragmentsNeededForEveryIdentification { get; set; }
+
+        #endregion Public Properties
+    }
+}
