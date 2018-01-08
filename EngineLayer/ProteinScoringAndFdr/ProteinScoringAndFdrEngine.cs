b371cdb69ca3caf772a38b3d2011d361a927698b
trishorts <mshort@chem.wisc.edu>
2018-01-08 11:59:13 -0600

    same

--- EngineLayer/ProteinScoringAndFdr/ProteinScoringAndFdrEngine.cs
+++ EngineLayer/ProteinScoringAndFdr/ProteinScoringAndFdrEngine.cs
@@ -173,7 +173,7 @@ namespace EngineLayer
 
         private static string F(string proteinGroupName) //we're keeping only the better scoring protein group for each target/decoy pair. to do that we need to strip decoy from the name temporarily. this is the "top-picked" method
         {
-            return proteinGroupName.Contains("DECEOY_") ? proteinGroupName.Replace("DECOY_", "") : proteinGroupName;
+            return proteinGroupName.Contains("DECOY_") ? proteinGroupName.Replace("DECOY_", "") : proteinGroupName;
         }
 
         #endregion Private Methods
