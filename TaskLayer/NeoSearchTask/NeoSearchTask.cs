b774d8a3101bf83eadea740a184c9cc1e40118f7
Zachary Rolfs <zrolfs@wisc.edu>
2018-01-17 13:17:22 -0600

    Buildable

---
+++ TaskLayer/NeoSearchTask/NeoSearchTask.cs
@@ -0,0 +1,43 @@
+﻿using Chemistry;
+using EngineLayer;
+using EngineLayer.Analysis;
+using EngineLayer.ClassicSearch;
+using EngineLayer.Indexing;
+using EngineLayer.ModernSearch;
+using EngineLayer.NonSpecificEnzymeSearch;
+using FlashLFQ;
+using MassSpectrometry;
+using MathNet.Numerics.Distributions;
+using MzLibUtil;
+using Proteomics;
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Xml;
+using System.Xml.Serialization;
+using UsefulProteomicsDatabases;
+
+namespace TaskLayer
+{
+    public class NeoSearchTask : MetaMorpheusTask
+    {
+        public NeoSearchTask() : base(MyTask.Search)
+        {
+            NeoParameters = new NeoParameters();
+        }
+
+        #region Public Properties
+
+        public NeoParameters NeoParameters { get; set; }
+
+        protected override MyTaskResults RunSpecific(string OutputFolder, List<DbForTask> dbFilenameList, List<string> currentRawFileList, string taskId, FileSpecificSettings[] fileSettingsList)
+        {
+            return new MyTaskResults(this);
+        }
+
+        #endregion Public Properties
+    }
+}
---
+++ TaskLayer/TaskParameters/NeoParameters.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TaskLayer
+{
+    public class NeoParameters
+    {
+        #region Public Constructors
+
+        public NeoParameters()
+        {
+            Calibrate = true;
+            GPTMD = true;
+            TargetSearch = true;
+            DecoySearch = true;
+            SearchNTerminus = true;
+            SearchCTerminus = true;
+            bIons = true;
+            yIons = true;
+            cIons = false;
+            zdotIons = false;
+            MaxMissedConsecutiveCleavages = 2;
+            MaxMissedCleavages = 5;
+            MaxCandidatesPerSpectrum = 2000;
+            NormalCis = true;
+            ReverseCis = true;
+        }
+
+        #endregion Public Constructors
+
+        #region Public Properties
+
+        public bool Calibrate { get; set; }
+        public double? PrecursorTolerancePPM { get; set; }
+        public double? ProductTolerancePPM { get; set; }
+        public bool GPTMD { get; set; }
+        public bool TargetSearch { get; set; }
+        public string TargetFilePath { get; set; }
+        public bool DecoySearch { get; set; }
+        public string DecoyFilePath { get; set; }
+
+        public bool SearchNTerminus { get; set; }
+        public string NFilePath { get; set; }
+        public bool SearchCTerminus { get; set; }
+        public string CFilePath { get; set; }
+        public bool bIons { get; set; }
+        public bool yIons { get; set; }
+        public bool cIons { get; set; }
+        public bool zdotIons { get; set; }
+
+        public int MaxMissedConsecutiveCleavages { get; set; }
+        public int MaxMissedCleavages { get; set; }
+        public int MaxCandidatesPerSpectrum { get; set; }
+
+        public int MinDistanceAllowed { get; set; }
+        public int MaxDistanceAllowed { get; set; }
+        public bool NormalCis { get; set; }
+        public bool ReverseCis { get; set; }
+
+        #endregion Public Properties
+    }
+}
