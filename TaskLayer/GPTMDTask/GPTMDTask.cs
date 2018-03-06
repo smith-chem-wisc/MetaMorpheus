e0b5b9d8b662c87301c2da8499048680e7c5e295
rob <rmillikin@wisc.edu>
2018-03-06 13:29:10 -0600

    0.05 notch q-value for first-pass gptmd

--- TaskLayer/GPTMDTask/GPTMDTask.cs
+++ TaskLayer/GPTMDTask/GPTMDTask.cs
@@ -78,7 +78,7 @@ namespace TaskLayer
             Status("Loading proteins...", new List<string> { taskId });
             Dictionary<string, Modification> um = null;
             //Decoys are currently not being searched with DecoyType.None
-            var proteinList = dbFilenameList.SelectMany(b => LoadProteinDb(b.FilePath, true, DecoyType.None, localizeableModificationTypes, b.IsContaminant, out um)).ToList();
+            var proteinList = dbFilenameList.SelectMany(b => LoadProteinDb(b.FilePath, true, DecoyType.Reverse, localizeableModificationTypes, b.IsContaminant, out um)).ToList();
 
             var numRawFiles = currentRawFileList.Count;
 
