732df08517ff58c1bd07dc08e1dfd573da6e7780
rmillikin <rmillikin@wisc.edu>
2018-04-02 11:38:18 -0500

    276 (#1067)
    * remove ToList after digestion in several places; remove localization stuff from calibration, gptmd task windows
    
    * hide "write intermediate files" in calibration window
    
    * XL pepXML localization
    
    * XL pepXML localization2
    
    * XL loop localization unit test
    
    * Codacy improvement
    
    * Xl improve Code
    
    * Update Xl output .mytsv
    
    * Xl CultureInfo.InvariantCulture
    
    * Update RNA mod
    
    * big calibration update
    
    * make codacy happy
    
    * RNA update2
    
    * annoying appveyor thing
    
    * check MS Thermo File Reader version when adding a raw file to the GUI (#1064)
    
    * fix installer issues w calibration (#1066)
    
    * fix installer issues w calibration
    
    * fix peak trimming

--- TaskLayer/CalibrationTask/CalibrationTask.cs
+++ TaskLayer/CalibrationTask/CalibrationTask.cs
@@ -146,7 +146,8 @@ namespace TaskLayer
                     myMsDataFile = myFileManager.LoadFile(originalUncalibratedFilePath, CommonParameters.TopNpeaks, CommonParameters.MinRatio, CommonParameters.TrimMs1Peaks, CommonParameters.TrimMsMsPeaks);
                 }
 
-                Status("Aquiring calibration data points...", new List<string> { taskId, "Individual Spectra Files" });
+                Status("Acquiring calibration data points...", new List<string> { taskId, "Individual Spectra Files" });
+
                 // get datapoints to fit calibration function to
                 var acquisitionResults = GetDataAcquisitionResults(myMsDataFile, originalUncalibratedFilePath, variableModifications, fixedModifications, proteinList, taskId, combinedParams, combinedParams.PrecursorMassTolerance, combinedParams.ProductMassTolerance);
                 
@@ -175,6 +176,7 @@ namespace TaskLayer
                 // generate calibration function and shift data points
                 Status("Calibrating...", new List<string> { taskId, "Individual Spectra Files" });
                 new CalibrationEngine(myMsDataFile, acquisitionResults.Item2, new List<string> { taskId, "Individual Spectra Files", originalUncalibratedFilenameWithoutExtension }).Run();
+                myFileManager.DoneWithFile(originalUncalibratedFilePath);
 
                 // do another search to evaluate calibration results
                 Status("Post-calibration search...", new List<string> { taskId, "Individual Spectra Files" });
