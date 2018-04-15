c89428387b260c4e39c6bc1a3483b27193eb3fa8
ROBERT MILLIKIN <rmillikin@wisc.edu>
2018-04-15 17:39:38 -0500

    remove nice calibration unit test :( takes too long in appveyor

--- Test/SlicedTest.cs
+++ Test/SlicedTest.cs
@@ -64,45 +64,6 @@ namespace Test
             Assert.AreEqual(2, File.ReadLines(protGroups).Count());
         }
 
-        [Test]
-        public static void CalibrationTest()
-        {
-            var preCalibrationSearch = new SearchTask();
-            var calibration = new CalibrationTask();
-            var postCalibrationSearch = new SearchTask();
-            var gptmd = new GptmdTask();
-            var postGptmdSearch = new SearchTask();
-
-            DbForTask db = new DbForTask(Path.Combine(TestContext.CurrentContext.TestDirectory, @"TestData\SmallYeastDb.fasta"), false);
-            string raw = Path.Combine(TestContext.CurrentContext.TestDirectory, @"TestData\SmallCalibratibleYeast.mzml");
-
-            EverythingRunnerEngine a = new EverythingRunnerEngine(new List<(string, MetaMorpheusTask)>
-            { ("Task1", preCalibrationSearch),
-                ("Task2", calibration),
-                ("Task3", postCalibrationSearch),
-                ("Task4", gptmd),
-                ("Task5", postGptmdSearch)
-            }, 
-                new List<string> { raw }, 
-                new List<DbForTask> { db }, 
-                Environment.CurrentDirectory);
-
-            a.Run();
-
-            //var thisTaskOutputFolder = MySetUpClass.outputFolder;
-
-            //var peaks = Path.Combine(thisTaskOutputFolder, "Task", "sliced-raw_QuantifiedPeaks.tsv");
-
-            //Assert.AreEqual(2, File.ReadLines(peaks).Count());
-
-            //var psms = Path.Combine(thisTaskOutputFolder, "Task", "sliced-raw_PSMs.psmtsv");
-
-            //Assert.AreEqual(3, File.ReadLines(psms).Count());
-            //var protGroups = Path.Combine(thisTaskOutputFolder, "Task", "sliced-raw_ProteinGroups.tsv");
-
-            //Assert.AreEqual(2, File.ReadLines(protGroups).Count());
-        }
-
         #endregion Public Methods
     }
 }
