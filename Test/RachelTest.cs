7eeaccb32a86ea783c208704fe6439bf4f0813b9
RACHEL M MILLER <rmmiller22@wisc.edu>
2018-04-30 10:30:47 -0500

    UnitTest for protein Pruned DB generation if complete and functions

--- Test/RachelTest.cs
+++ Test/RachelTest.cs
@@ -1,17 +1,76 @@
-﻿using System;
+﻿using EngineLayer;
+using MassSpectrometry;
+using MzLibUtil;
+using NUnit.Framework;
+using Proteomics;
+using System;
 using System.Collections.Generic;
-using System.Text;
+using System.IO;
+using System.Linq;
+using TaskLayer;
+using UsefulProteomicsDatabases;
 
 namespace Test
 {
-    public static class RachelTest
+    [TestFixture]
+    public class RachelTest
     {
-        // want to test that all proteins whose psms with base sequence !=null are being put into database
-        // want to check that all modifications are localized meaning that they come from psms with full sequences !=null
-        // need to make a fake MS file that goes through search and a fake DB for the PSMs
-        // want a psms whose base sequence is ambiguous (=null): make sure that this does not end up in confident psms
+        
         // want a psm whose base sequence is not ambigous but full sequence is (ptm is not localized): make sure this does not make it in DB
-        // want a psm whose base sequence is not ambigous and all psms are localized: no issues should be in pruned
-        // want a psm that could come from more than one protein: ensure that all options are put into the pruned DB
+       
+       [Test]
+        public static void TestPrunedGeneration()
+        {
+            //Create GPTMD Task
+            //Create Search Task
+            GptmdTask task1 = new GptmdTask
+            {
+                CommonParameters = new CommonParameters
+                {
+                    ConserveMemory = false
+                },
+            };
+
+            SearchTask task2 = new SearchTask
+            {
+                CommonParameters = new CommonParameters
+                {
+                    ConserveMemory = false
+                },
+                SearchParameters = new SearchParameters
+                {
+                    DoParsimony = true,
+                    SearchTarget = true,
+                    WritePrunedDatabase = true,
+                    SearchType = SearchType.Classic
+                }
+            };
+            List<(string, MetaMorpheusTask)> taskList = new List<(string, MetaMorpheusTask)>
+            {
+                ("task1", task1),
+                ("task2", task2),
+            };
+            string mzmlName = @"TestData\PrunedDbSpectra.mzml";
+            string fastaName = @"TestData\DbForPrunedDb.fasta";
+            var engine = new EverythingRunnerEngine(taskList, new List<string> { mzmlName }, new List<DbForTask> { new DbForTask(fastaName, false) }, Environment.CurrentDirectory);
+            engine.Run();
+            string outputFolderInThisTest = MySetUpClass.outputFolder;
+            string final = Path.Combine(MySetUpClass.outputFolder, "task2","DbForPrunedDbGPTMDproteinPruned.xml");
+            List<Protein> proteins = ProteinDbLoader.LoadProteinXML(final, true, DecoyType.Reverse, new List<Modification>(), false, new List<string>(), out var ok);
+            //ensures that protein out put contins the correct number of proteins to match the folowing conditions. 
+                // all proteins in DB have baseSequence!=null (not ambiguous)
+                // all proteins that belong to a protein group are written to DB
+            Assert.AreEqual(proteins.Count(),20);
+            int totalNumberOfMods = 0;
+            foreach (Protein p in proteins)
+            {
+                int numberOfMods = p.OneBasedPossibleLocalizedModifications.Count();
+                totalNumberOfMods=totalNumberOfMods + numberOfMods;
+            }
+            //tests that modifications are being done correctly
+            Assert.AreEqual(totalNumberOfMods, 0);
+
+        }
     }
-}
+}     
+
