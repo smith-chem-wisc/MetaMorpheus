4da476f51a8904b0e0a08479814e417f797a78c3
Zachary Rolfs <zrolfs@wisc.edu>
2018-05-31 13:55:48 -0500

    copy mgf test file

--- Test/MsDataFileTest.cs
+++ Test/MsDataFileTest.cs
@@ -20,7 +20,7 @@ namespace Test
             //The purpose of this test is to ensure that mgfs can be run without crashing. 
             //Whenever a new feature is added that may require things an mgf does not have, 
             //there should be a check that prevents mgfs from using that feature.
-            string mgfName = @"ok.mgf";
+            string mgfName = @"TestData\ok.mgf";
             string xmlName = "okk.xml";
 
             SearchTask task1 = new SearchTask
@@ -37,7 +37,7 @@ namespace Test
             };
             //run!
 
-            var engine = new EverythingRunnerEngine(taskList, new List<string> { mgfName }, new List<DbForTask> { new DbForTask(xmlName, false) }, TestContext.CurrentContext.TestDirectory);
+            var engine = new EverythingRunnerEngine(taskList, new List<string> { mgfName }, new List<DbForTask> { new DbForTask(xmlName, false) }, Environment.CurrentDirectory);
             engine.Run();
             //Just don't crash! There should also be at least one psm at 1% FDR, but can't check for that.
         }
