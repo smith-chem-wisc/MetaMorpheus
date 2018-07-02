49ddadbf5e87a65a061158390b3c7e92288929c8
lonelu <lonelur@gmail.com>
2018-07-02 15:12:59 -0500

    Add Dump

--- RealTimeGUI/DataReceiver.cs
+++ RealTimeGUI/DataReceiver.cs
@@ -68,8 +68,9 @@ namespace RealTimeGUI
                 //Console.WriteLine("\n{0:HH:mm:ss,fff} scan with {1} centroids arrived", DateTime.Now, scan.CentroidCount);
                 string x = "\n" + DateTime.Now + " " + Thread.CurrentThread.Name;
                 //DataReceiverNotificationEventHandler?.Invoke(this, new NotificationEventArgs(x));
-                DataReceiverNotificationEventHandler?.BeginInvoke(this, new NotificationEventArgs(x), callBack, null);
+                //DataReceiverNotificationEventHandler?.BeginInvoke(this, new NotificationEventArgs(x), callBack, null);
                 //ListScan.Add(scan);
+                Dump("Specific", scan.SpecificInformation);
                 logD.Debug("Get one scan");
 
             }
@@ -85,6 +86,30 @@ namespace RealTimeGUI
 		{
             string x = "\n" + DateTime.Now + "Acquisition stream opens (start of method)" + "\n";
             DataReceiverNotificationEventHandler?.Invoke(this, new NotificationEventArgs(x));
+        }
+
+        private void Dump(string title, IInfoContainer container)
+        {
+            //Console.WriteLine(title);
+            foreach (string key in container.Names)
+            {
+                string value;
+                // the source has to be "Unknown" to match all sources. Otherwise, the source has to match.
+                // Sources can address values appearing in Tune files, general settings, but also values from
+                // status log or additional values to a scan.
+                MsScanInformationSource source = MsScanInformationSource.Unknown;   // show everything
+                try
+                {
+                    if (container.TryGetValue(key, out value, ref source))
+                    {
+                        string descr = source.ToString();
+                        descr = descr.Substring(0, Math.Min(11, descr.Length));
+                        //Console.WriteLine("   {0,-11} {1,-35} = {2}", descr, key, value);
+                        logD.Debug(descr);
+                    }
+                }
+                catch { /* up to and including 2.8SP1 there is a bug displaying items which are null and if Foundation 3.1SP4 is used with CommonCore */ }
+            }
         }
 
         private void callBack(IAsyncResult asyncResult)
