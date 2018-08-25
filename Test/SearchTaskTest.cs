cca85f9948de96627635a638ddd8efd4816a31c9
ianhirsch <39777768+ianhirsch@users.noreply.github.com>
2018-08-25 10:43:40 -0500

    Unit tests for Search Task (#1376)
    * QValueParam
    
    * Changes to file writing
    
    * Small Changes
    
    * Smaller Changes
    
    * Update PostSearchAnalysisTask.cs
    
    * Update TestPsm.cs
    
    * Update MetaMorpheusTask.cs
    
    * Unit tests for search task
    
    * add nonspceific test
    
    * non specific test revision
    
    * changes fixed
    
    * Update FdrAnalysisEngine.cs

---
+++ Test/SearchTaskTest.cs
@@ -0,0 +1,91 @@
+﻿using EngineLayer;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using TaskLayer;
+
+namespace Test
+{
+    [TestFixture]
+    public static class SearchTaskTest
+    {
+        [Test]
+        public static void MassDiffAceptorTest()
+        {
+            SearchTask searchTask = new SearchTask();
+            var result = SearchTask.GetMassDiffAcceptor(searchTask.CommonParameters.PrecursorMassTolerance, searchTask.SearchParameters.MassDiffAcceptorType, searchTask.SearchParameters.CustomMdac);
+            Assert.That(result.FileNameAddition.Equals("1mm"));
+
+            result = SearchTask.GetMassDiffAcceptor(searchTask.CommonParameters.PrecursorMassTolerance, MassDiffAcceptorType.TwoMM, searchTask.SearchParameters.CustomMdac);
+            Assert.That(result.FileNameAddition.Equals("2mm"));
+
+            result = SearchTask.GetMassDiffAcceptor(searchTask.CommonParameters.PrecursorMassTolerance, MassDiffAcceptorType.ThreeMM, searchTask.SearchParameters.CustomMdac);
+            Assert.That(result.FileNameAddition.Equals("3mm"));
+
+            result = SearchTask.GetMassDiffAcceptor(searchTask.CommonParameters.PrecursorMassTolerance, MassDiffAcceptorType.ModOpen, searchTask.SearchParameters.CustomMdac);
+            Assert.That(result.FileNameAddition.Equals("-187andUp"));
+
+            result = SearchTask.GetMassDiffAcceptor(searchTask.CommonParameters.PrecursorMassTolerance, MassDiffAcceptorType.Open, searchTask.SearchParameters.CustomMdac);
+            Assert.That(result.FileNameAddition.Equals("OpenSearch"));
+
+            result = SearchTask.GetMassDiffAcceptor(searchTask.CommonParameters.PrecursorMassTolerance, MassDiffAcceptorType.Custom, "custom ppmAroundZero 4");
+            Assert.That(result.FileNameAddition.Equals("4ppmAroundZero"));
+
+            result = SearchTask.GetMassDiffAcceptor(searchTask.CommonParameters.PrecursorMassTolerance, MassDiffAcceptorType.Exact, searchTask.SearchParameters.CustomMdac);
+            Assert.That(result.FileNameAddition.Equals("5ppmAroundZero"));
+        }
+
+        [Test]
+        public static void ParseSearchModeTest()
+        {
+            SearchTask searchTask = new SearchTask();
+            var result = SearchTask.GetMassDiffAcceptor(searchTask.CommonParameters.PrecursorMassTolerance, MassDiffAcceptorType.Custom, "TestCustom dot 5 ppm 0,1.0029,2.0052");
+            Assert.That(result.NumNotches == 3);
+
+            result = SearchTask.GetMassDiffAcceptor(searchTask.CommonParameters.PrecursorMassTolerance, MassDiffAcceptorType.Custom, "TestCustom dot 5 da 0,1.0029,2.0052");
+            Assert.That(result.NumNotches == 3);
+
+            result = SearchTask.GetMassDiffAcceptor(searchTask.CommonParameters.PrecursorMassTolerance, MassDiffAcceptorType.Custom, "TestCustom interval [0;5],[0;5]");
+            Assert.That(result.NumNotches == 1);
+
+            result = SearchTask.GetMassDiffAcceptor(searchTask.CommonParameters.PrecursorMassTolerance, MassDiffAcceptorType.Custom, "TestCustom OpenSearch 5");
+            Assert.That(result.FileNameAddition.Equals("OpenSearch"));
+
+            result = SearchTask.GetMassDiffAcceptor(searchTask.CommonParameters.PrecursorMassTolerance, MassDiffAcceptorType.Custom, "TestCustom daltonsAroundZero 5");
+            Assert.That(result.FileNameAddition.Equals("5daltonsAroundZero"));
+
+            result = SearchTask.GetMassDiffAcceptor(searchTask.CommonParameters.PrecursorMassTolerance, MassDiffAcceptorType.Custom, "TestCustom ppmAroundZero 5");
+            Assert.That(result.FileNameAddition.Equals("5ppmAroundZero"));
+
+            Assert.That(() => SearchTask.GetMassDiffAcceptor(searchTask.CommonParameters.PrecursorMassTolerance, MassDiffAcceptorType.Custom, "TestCustom Test 5"),
+                Throws.TypeOf<MetaMorpheusException>());
+        }
+
+        [Test]
+        public static void SemiSpecificTest()
+        {
+            SearchTask searchTask = new SearchTask()
+            {
+                SearchParameters = new SearchParameters
+                {
+                    SearchType = SearchType.NonSpecific
+                },
+            };
+
+            string outputFolder = Path.Combine(TestContext.CurrentContext.TestDirectory, @"TestSameSettingsOutput");
+            string myFile = Path.Combine(TestContext.CurrentContext.TestDirectory, @"TestData\PrunedDbSpectra.mzml");
+            string myDatabase = Path.Combine(TestContext.CurrentContext.TestDirectory, @"TestData\DbForPrunedDb.fasta");
+            DbForTask db = new DbForTask(myDatabase, false);
+
+            List<(string, MetaMorpheusTask)> taskList = new List<(string, MetaMorpheusTask)> { ("TestSemiSpecific", searchTask) };
+
+            var engine = new EverythingRunnerEngine(taskList, new List<string> { myFile }, new List<DbForTask> { new DbForTask(myDatabase, false) }, Environment.CurrentDirectory);
+            engine.Run();
+
+            string outputPath = Path.Combine(TestContext.CurrentContext.TestDirectory, @"TestSemiSpecific\AllPSMs.psmtsv");
+            var output = File.ReadAllLines(outputPath);
+            Assert.That(output.Length == 9);
+        }
+    }
+}
