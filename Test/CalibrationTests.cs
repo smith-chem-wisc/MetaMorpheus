4b8b2e21434e89bca8c7945347526d5c67e33933
IAN HIRSCH <ihirsch@wisc.edu>
2018-08-31 11:50:28 -0500

    full engine run

--- Test/CalibrationTests.cs
+++ Test/CalibrationTests.cs
@@ -16,7 +16,7 @@ namespace Test
             string outputFolder = Path.Combine(TestContext.CurrentContext.TestDirectory, @"TestCalibration");
             string myFile = Path.Combine(TestContext.CurrentContext.TestDirectory, @"TestData\SmallCalibratible_Yeast.raw");
             string myDatabase = Path.Combine(TestContext.CurrentContext.TestDirectory, @"TestData\smalldb.fasta");
-            calibrationTask.RunTask(outputFolder, new List<DbForTask> { new DbForTask(myDatabase, false) }, new List<string> { myFile }, "test");
+            var engine = new EverythingRunnerEngine(new List<(string, MetaMorpheusTask)> { ( "Calibration", calibrationTask ) }, new List<string> { myFile }, new List<DbForTask> { new DbForTask(myDatabase, false) }, outputFolder);
         }
     }
 }
