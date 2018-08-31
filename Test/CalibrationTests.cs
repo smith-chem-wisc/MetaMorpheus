082ea4d1bd3ed4a3072e0b6018993a835f8df1e6
IAN HIRSCH <ihirsch@wisc.edu>
2018-08-31 12:44:27 -0500

    Use mzML

--- Test/CalibrationTests.cs
+++ Test/CalibrationTests.cs
@@ -10,13 +10,14 @@ namespace Test
     class CalibrationTests
     {
         [Test]
-        public static void RunCalibrationTest()
+        public static void RunCalibrationEngineTest()
         {
             CalibrationTask calibrationTask = new CalibrationTask();
             string outputFolder = Path.Combine(TestContext.CurrentContext.TestDirectory, @"TestCalibration");
-            string myFile = Path.Combine(TestContext.CurrentContext.TestDirectory, @"TestData\SmallCalibratible_Yeast.raw");
+            string myFile = Path.Combine(TestContext.CurrentContext.TestDirectory, @"TestData\SmallCalibratible_Yeast.mzML");
             string myDatabase = Path.Combine(TestContext.CurrentContext.TestDirectory, @"TestData\smalldb.fasta");
             var engine = new EverythingRunnerEngine(new List<(string, MetaMorpheusTask)> { ( "Calibration", calibrationTask ) }, new List<string> { myFile }, new List<DbForTask> { new DbForTask(myDatabase, false) }, outputFolder);
+            engine.Run();
         }
     }
 }
