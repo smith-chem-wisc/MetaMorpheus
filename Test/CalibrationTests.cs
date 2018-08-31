8076bd8c16faced2eb9f4535d5eb7b2994ea47d0
IAN HIRSCH <ihirsch@wisc.edu>
2018-08-31 11:12:23 -0500

    Calibration Test

---
+++ Test/CalibrationTests.cs
@@ -0,0 +1,21 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using TaskLayer;
+
+namespace Test
+{
+    class CalibrationTests
+    {
+        public static void RunCalibrationTest()
+        {
+            CalibrationTask calibrationTask = new CalibrationTask();
+            string outputFolder = Path.Combine(TestContext.CurrentContext.TestDirectory, @"TestCalibration");
+            string myFile = Path.Combine(TestContext.CurrentContext.TestDirectory, @"TestData\SmallCalibratible_Yeast.raw");
+            string myDatabase = Path.Combine(TestContext.CurrentContext.TestDirectory, @"TestData\smalldb.fasta");
+            calibrationTask.RunTask(outputFolder, new List<DbForTask> { new DbForTask(myDatabase, false) }, new List<string> { myFile }, "test");
+        }
+    }
+}
