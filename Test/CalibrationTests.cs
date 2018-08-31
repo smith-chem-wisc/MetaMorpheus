348807a00e7b944f7f9c40ae604421347aa480d0
IAN HIRSCH <ihirsch@wisc.edu>
2018-08-31 14:12:50 -0500

    new test

--- Test/CalibrationTests.cs
+++ Test/CalibrationTests.cs
@@ -16,8 +16,27 @@ namespace Test
             string outputFolder = Path.Combine(TestContext.CurrentContext.TestDirectory, @"TestCalibration");
             string myFile = Path.Combine(TestContext.CurrentContext.TestDirectory, @"TestData\SmallCalibratible_Yeast.mzML");
             string myDatabase = Path.Combine(TestContext.CurrentContext.TestDirectory, @"TestData\smalldb.fasta");
-            var engine = new EverythingRunnerEngine(new List<(string, MetaMorpheusTask)> { ( "Calibration", calibrationTask ) }, new List<string> { myFile }, new List<DbForTask> { new DbForTask(myDatabase, false) }, outputFolder);
-            engine.Run();
+
+            calibrationTask.RunTask(outputFolder, new List<DbForTask> { new DbForTask(myDatabase, false) }, new List<string> { myFile }, "test");
+
+        }
+
+        [Test]
+        public static void ExperimentalDesignTest()
+        {
+            CalibrationTask calibrationTask = new CalibrationTask();
+            string outputFolder = Path.Combine(TestContext.CurrentContext.TestDirectory, @"TestCalibration");
+            string myFile = Path.Combine(TestContext.CurrentContext.TestDirectory, @"TestData\SmallCalibratible_Yeast.mzML");
+            string myDatabase = Path.Combine(TestContext.CurrentContext.TestDirectory, @"TestData\smalldb.fasta");
+            string filePath = Path.Combine(TestContext.CurrentContext.TestDirectory, @"TestData\ExperimentalDesign.tsv");
+            using (StreamWriter output = new StreamWriter(filePath))
+            {
+                output.WriteLine("FileName\tCondition\tBiorep\tFraction\tTechrep");
+                output.WriteLine("PrunedDbSpectra" + "\t" + "condition" + "\t" + "1" + "\t" + "1" + "\t" + "1");
+            }
+
+            calibrationTask.RunTask(outputFolder, new List<DbForTask> { new DbForTask(myDatabase, false) }, new List<string> { myFile }, "test");
+            File.Delete(filePath);
         }
     }
 }
