57c42edcad1ca2511433c86d5d79a52b3c8bef61
IAN HIRSCH <ihirsch@wisc.edu>
2018-09-10 15:29:30 -0500

    Changes Fixed

--- Test/CalibrationTests.cs
+++ Test/CalibrationTests.cs
@@ -1,8 +1,7 @@
 ﻿using NUnit.Framework;
-using System;
 using System.Collections.Generic;
 using System.IO;
-using System.Text;
+using System.Text.RegularExpressions;
 using TaskLayer;
 
 namespace Test
@@ -20,10 +19,22 @@ namespace Test
             using (StreamWriter output = new StreamWriter(filePath))
             {
                 output.WriteLine("FileName\tCondition\tBiorep\tFraction\tTechrep");
-                output.WriteLine("PrunedDbSpectra" + "\t" + "condition" + "\t" + "1" + "\t" + "1" + "\t" + "1");
+                output.WriteLine("SmallCalibratable_Yeast" + "\t" + "condition" + "\t" + "1" + "\t" + "1" + "\t" + "1");
             }
 
             calibrationTask.RunTask(outputFolder, new List<DbForTask> { new DbForTask(myDatabase, false) }, new List<string> { myFile }, "test");
+            var expDesignPath = Path.Combine(outputFolder, @"ExperimentalDesign.tsv");
+            var expDesign = File.ReadAllLines(expDesignPath);
+            Assert.That(expDesign[1].Contains("SmallCalibratable_Yeast-calib"));
+            Assert.That(File.Exists(Path.Combine(outputFolder, @"SmallCalibratible_Yeast-calib.mzML")));
+            Assert.That(File.Exists(Path.Combine(outputFolder, @"SmallCalibratible_Yeast-calib.toml")));
+            var lines = File.ReadAllLines(Path.Combine(outputFolder, @"SmallCalibratible_Yeast-calib.toml"));
+            var tolerance = Regex.Match(lines[0], @"\d+\.\d*").Value;
+            var tolerance1 = Regex.Match(lines[1], @"\d+\.\d*").Value;
+            Assert.That(double.TryParse(tolerance, out double tol) == true);
+            Assert.That(double.TryParse(tolerance1, out double tol1) == true);
+            Assert.That(lines[0].Contains("PrecursorMassTolerance"));
+            Assert.That(lines[1].Contains("ProductMassTolerance"));
             File.Delete(filePath);
         }
     }
