1daaa0c3c576284e3079aad3b8a3bc3a4a611120
IAN HIRSCH <ihirsch@wisc.edu>
2018-10-11 16:54:28 -0500

    test actually fixed

--- Test/XLSearchOutputTest.cs
+++ Test/XLSearchOutputTest.cs
@@ -19,8 +19,9 @@ namespace Test
             xLSearch.RunTask(outputFolder, new List<DbForTask> { new DbForTask(myDatabase, false) }, new List<string> { myFile }, "test");
 
             var resultsPath = File.ReadAllLines(Path.Combine(outputFolder, @"XL_Interlinks.tsv"));
-            string expected = "C:\\MetaMorpheus\\Test\\bin\\Debug\\netcoreapp2.0\\XLTestData\\BSA_DSS_23747.mzML\t9\t808.380676269531\t4\t3229.49359921061\tInter\tK;K\t3336842\t151\tPDPNTLCDEFKADEK\tPDPNTLC[Common Fixed:Carbamidomethyl on C]DEFKADEK(11)\t1777.782879933\t4.01654288568969\t42\t\tDECOY_3336842\t364\tTVEVFEAKPFK\tTVEVFEAKPFK(11)\t1293.696888652\t4.00857616644258\t30\t\t8.02511905213227\t158.013830625609\t0;0\t0\t-\tD\t∞\t";
-            Assert.That(resultsPath[1].Equals(expected));
+            var sections = resultsPath[1].Split('\t');
+            Assert.That(resultsPath.Length == 2);
+            Assert.That(sections.Length == 31);
 
         }
     }
