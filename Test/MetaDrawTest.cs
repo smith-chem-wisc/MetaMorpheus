3cb0aaa243c474ac6931bb56ae72df50baafef27
Robert Millikin <rmillikin@wisc.edu>
2018-11-10 13:09:54 -0600

    fix metadraw psm reading issues; add unit test for metadraw (#1517) (#1518)
    * fix metadraw psm reading issues; add unit test for metadraw
    
    * move stuff over to EngineLayer for unit testing

---
+++ Test/MetaDrawTest.cs
@@ -0,0 +1,36 @@
+﻿using EngineLayer;
+using NUnit.Framework;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using TaskLayer;
+
+namespace Test
+{
+    [TestFixture]
+    public class MetaDrawTest
+    {
+        [Test]
+        public static void TestMetaDrawReadPsmFile()
+        {
+            SearchTask searchTask = new SearchTask();
+
+            string myFile = Path.Combine(TestContext.CurrentContext.TestDirectory, @"TestData\PrunedDbSpectra.mzml");
+            string myDatabase = Path.Combine(TestContext.CurrentContext.TestDirectory, @"TestData\DbForPrunedDb.fasta");
+            string folderPath = Path.Combine(TestContext.CurrentContext.TestDirectory, @"TestMetaDrawReadPsmFile");
+
+            DbForTask db = new DbForTask(myDatabase, false);
+            Directory.CreateDirectory(folderPath);
+
+            searchTask.RunTask(folderPath, new List<DbForTask> { db }, new List<string> { myFile }, "metadraw");
+            string psmFile = Directory.GetFiles(folderPath).First(f => f.Contains("AllPSMs.psmtsv"));
+
+            List<MetaDrawPsm> parsedPsms = TsvResultReader.ReadTsv(psmFile, out var warnings);
+
+            Assert.AreEqual(11, parsedPsms.Count);
+            Assert.AreEqual(0, warnings.Count);
+
+            Directory.Delete(folderPath, true);
+        }
+    }
+}
