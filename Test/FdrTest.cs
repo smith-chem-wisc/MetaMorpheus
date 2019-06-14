2ebcc4bb07cff528caa5859d97c53b8763e66511
trishorts <mshort@chem.wisc.edu>
2019-06-14 13:14:48 -0500

    remove this peptide unit test

--- Test/FdrTest.cs
+++ Test/FdrTest.cs
@@ -257,6 +257,28 @@ namespace Test
         }
 
         [Test]
+        public static void TestRemoveThisAmbiguousePeptide()
+        {
+            Ms2ScanWithSpecificMass scanB = new Ms2ScanWithSpecificMass(
+                new MsDataScan(
+                    new MzSpectrum(new double[] { }, new double[] { }, false),
+                    2, 1, true, Polarity.Positive, double.NaN, null, null, MZAnalyzerType.Orbitrap, double.NaN, null, null, "scan=1", double.NaN, null, null, double.NaN, null, DissociationType.AnyActivationType, 1, null),
+                100, 1, null, new CommonParameters(), null);
+
+            PeptideSpectralMatch psm1 = new PeptideSpectralMatch(new PeptideWithSetModifications(new Protein("PEPTIDE", "ACCESSION", "ORGANISM"), new DigestionParams(), 1, 2, CleavageSpecificity.Full, "", 0, new Dictionary<int, Modification>(), 0), 0, 10, 1, scanB, new DigestionParams(), new List<MatchedFragmentIon>(), 0);
+
+            PeptideWithSetModifications pwsm = new PeptideWithSetModifications(new Protein("PEPTIDE", "ACCESSION", "ORGANISM"), new DigestionParams(), 1, 2, CleavageSpecificity.Full, "", 0, new Dictionary<int, Modification>(), 0);
+
+            psm1.AddOrReplace(pwsm, 10, 1, true, new List<MatchedFragmentIon>(), 0);
+
+            Assert.AreEqual(2, psm1.BestMatchingPeptides.Count());
+
+            psm1.RemoveThisAmbiguousePeptide(1, pwsm);
+
+            Assert.AreEqual(1, psm1.BestMatchingPeptides.Count());
+        }
+
+        [Test]
         public static void TestPValuesReduceAmbiguity()
         {
             //TODO
