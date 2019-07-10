6105c1da4edcdd601c227ce92a3b651ba4c2c2cc
trishorts-smrf <52470573+trishorts-smrf@users.noreply.github.com>
2019-07-10 13:37:07 -0500

    Added TMT 11-plex and updated target platform to x64 on most projects (#1690)
    * Added TMT 11-plex and updated target platform to x64 on most projects
    
    * uncomment code
    
    * Change protein sequence coverage % to fraction
    
    * change default G-PTM-D mods to only common biological and common artifact
    
    * fewer default mods means fewer proteins in the pruned database
    
    * add metals back to gptmd
    
    * returning unit test to its former glory

--- TaskLayer/GPTMDTask/GPTMDParameters.cs
+++ TaskLayer/GPTMDTask/GPTMDParameters.cs
@@ -5,13 +5,16 @@ namespace EngineLayer
 {
     public class GptmdParameters
     {
+        /// <summary>
+        /// The default modifications for a G-PTM-D search can be altered here by adding to b.Modification.Type.Equals("  NAME OF MODS TO ADD TO DEFAULT ")
+        /// </summary>
         public GptmdParameters()
         {
             ListOfModsGptmd = GlobalVariables.AllModsKnown.Where(b =>
-                b.ModificationType.Equals("Common Artifact") ||
-                b.ModificationType.Equals("Common Biological") ||
-                b.ModificationType.Equals("Metal") ||
-                b.ModificationType.Equals("Less Common")
+                b.ModificationType.Equals("Common Artifact")
+                || b.ModificationType.Equals("Common Biological")
+                || b.ModificationType.Equals("Metal") 
+                //|| b.ModificationType.Equals("Less Common")
             ).Select(b => (b.ModificationType, b.IdWithMotif)).ToList();
         }
 
