9901d8efd008cd1b042dd937e42d9bd82fac2b16
rob <rmillikin@wisc.edu>
2019-12-16 18:08:50 -0600

    ignore cmd unit test

--- Test/TestCmd.cs
+++ Test/TestCmd.cs
@@ -9,19 +9,23 @@ namespace Test
     public static class TestCmd
     {
         [Test]
-        //[Ignore("Ignored on AppVeyor")]
+        [Ignore("Ignored on AppVeyor")]
         public static void TestCommandLineMicroVignette()
         {
             Stopwatch s = new Stopwatch();
             s.Start();
 
+            string path = Path.Combine(TestContext.CurrentContext.TestDirectory, @"CommandLineMicroVignette");
+
             // run the micro vignette via command-line
             MetaMorpheusCommandLine.Program.Main(new string[] {
                 "-v",
-                "-o" + Path.Combine(TestContext.CurrentContext.TestDirectory, @"CommandLineMicroVignette") } );
+                "-o" + path } );
 
             s.Stop();
             Console.WriteLine("Command-line microvignette took: " + s.ToString());
+
+            Directory.Delete(path, true);
         }
     }
 }
