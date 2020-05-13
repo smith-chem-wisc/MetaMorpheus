ef6a0acd1ff3c0032629020cf0b6b37aa5d05b78
rob <rmillikin@wisc.edu>
2020-05-13 15:10:19 -0500

    change verbosity/microvignette cmd flags

--- CMD/CommandLineSettings.cs
+++ CMD/CommandLineSettings.cs
@@ -30,10 +30,10 @@ namespace MetaMorpheusCommandLine
         [Option('g', HelpText = "Generate default task tomls")]
         public bool GenerateDefaultTomls { get; set; }
 
-        [Option('v', HelpText = "Runs a small test search using a database and yeast data file included with this MetaMorpheus installation")]
+        [Option("test", HelpText = "Runs a small test search using a database and yeast data file included with this MetaMorpheus installation")]
         public bool RunMicroVignette { get; set; }
 
-        [Option("verbosity", Default = VerbosityType.normal, HelpText = "Determines how much text is written. Options are no output ('none'), minimal output and errors  ('minimal'), or normal ('normal')")]
+        [Option('v', Default = VerbosityType.normal, HelpText = "Determines how much text is written. Options are no output ('none'), minimal output and errors  ('minimal'), or normal ('normal')")]
         public VerbosityType Verbosity { get; set; }
 
         public enum VerbosityType { none, minimal, normal };
