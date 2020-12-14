4a2ff9f9439a7142fe001a090db3b0e1fbac3b64
lonelu <lonelur@gmail.com>
2020-12-13 19:40:10 -0800

    update glyco GUI

--- TaskLayer/GlycoSearchTask/GlycoSearchParameters.cs
+++ TaskLayer/GlycoSearchTask/GlycoSearchParameters.cs
@@ -12,10 +12,12 @@ namespace TaskLayer
             OGlycanDatabasefile = "OGlycan.gdb";
             NGlycanDatabasefile = "NGlycan182.gdb";
             GlycoSearchType = GlycoSearchType.OGlycanSearch;
+            MixedGlycoAllowed = false;
             OxoniumIonFilt = true;
             DecoyType = DecoyType.Reverse;
             GlycoSearchTopNum = 50;
             MaximumOGlycanAllowed = 4;
+            MaximumNGlycanAllowed = 1;
             Indexing_by_ion = true;
             IndexingChildScan = false;
             IndexingChildScanDiffIndex = false;
@@ -24,10 +26,14 @@ namespace TaskLayer
         public string OGlycanDatabasefile { get; set; }
         public string NGlycanDatabasefile { get; set; }
         public GlycoSearchType GlycoSearchType { get; set; }
+
+        public bool MixedGlycoAllowed { get; set; } 
         public bool OxoniumIonFilt { get; set; }
         public DecoyType DecoyType { get; set; }
         public int GlycoSearchTopNum { get; set; }
         public int MaximumOGlycanAllowed { get; set; }
+
+        public int MaximumNGlycanAllowed { get; set; }
         public bool Indexing_by_ion { get; set; }
         public bool IndexingChildScan { get; set; }
         public bool IndexingChildScanDiffIndex { get; set; }
