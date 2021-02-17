c4b487bb6bfd5c243fe09541a8a26e49050b3339
Robert Millikin <rmillikin@wisc.edu>
2021-02-17 16:20:37 -0600

    small experimental design GUI changes (#2025)
    * copy and paste into exp design; fix exp design gui bug
    
    * some color changes to exp design window, and fix a crash on trying to edit file name cell
    
    * add case for when pasting into file column

--- GUI/ForDisplayingInDataGrids/ExperimentalDesignForDataGrid.cs
+++ GUI/ForDisplayingInDataGrids/ExperimentalDesignForDataGrid.cs
@@ -4,10 +4,11 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.IO;
+using System.ComponentModel;
 
 namespace MetaMorpheusGUI
 {
-    public class ExperimentalDesignForDataGrid
+    public class ExperimentalDesignForDataGrid : INotifyPropertyChanged
     {
         public ExperimentalDesignForDataGrid(string fullFilePathWithExtension)
         {
@@ -17,9 +18,23 @@ namespace MetaMorpheusGUI
 
         public string FullFilePathWithExtension { get; private set; }
         public string FileNameWithExtension { get; private set; }
-        public string Condition { get; set; }
-        public string Biorep { get; set; }
-        public string Fraction { get; set; }
-        public string Techrep { get; set; }
+        public string Condition { get { return _condition; } set { _condition = value; RaisePropertyChanged("Condition"); } }
+        public string Biorep { get { return _biorep; } set { _biorep = value; RaisePropertyChanged("Biorep"); } }
+        public string Fraction { get { return _fraction; } set { _fraction = value; RaisePropertyChanged("Fraction"); } }
+        public string Techrep { get { return _techrep; } set { _techrep = value; RaisePropertyChanged("Techrep"); } }
+
+        private string _condition;
+        private string _biorep;
+        private string _fraction;
+        private string _techrep;
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        private void RaisePropertyChanged(string propertyName)
+        {
+            if (this.PropertyChanged != null)
+            {
+                this.PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+            }
+        }
     }
 }
