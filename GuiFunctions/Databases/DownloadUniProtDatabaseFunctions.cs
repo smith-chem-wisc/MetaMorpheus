fb523ab3d7af0fbecd0bb7996a239d6707829ce3
trishorts <mshort@chem.wisc.edu>
2021-03-18 08:57:32 -0500

    refactored HTML string method using stringbuilder

--- GuiFunctions/Databases/DownloadUniProtDatabaseFunctions.cs
+++ GuiFunctions/Databases/DownloadUniProtDatabaseFunctions.cs
@@ -12,117 +12,246 @@ namespace GuiFunctions.Databases
     public class DownloadUniProtDatabaseFunctions
     {
 
+        //public static string GetUniProtHtmlQueryString(string proteomeID, bool reviewed, bool isoforms, bool xmlFormat, bool compressed)
+        //{
+        //    string htmlQueryString;
+        //    if (!xmlFormat) //fasta
+        //    {
+        //        if (reviewed)
+        //        {
+        //            if (isoforms)
+        //            {
+        //                if (compressed)
+        //                {
+        //                    htmlQueryString = "https://www.uniprot.org/uniprot/?query=proteome:" + proteomeID + " reviewed:yes" + "&compress=yes" + "&format=fasta" + "&include:yes";
+        //                }
+        //                else //not compressed
+        //                {
+        //                    htmlQueryString = "https://www.uniprot.org/uniprot/?query=proteome:" + proteomeID + " reviewed:yes" + "&compress=no" + "&format=fasta" + "&include:yes";
+        //                }
+        //            }
+        //            else//no isoforms
+        //            {
+        //                if (compressed)
+        //                {
+        //                    htmlQueryString = "https://www.uniprot.org/uniprot/?query=proteome:" + proteomeID + " reviewed:yes" + "&compress=yes" + "&format=fasta" + "&include:no";
+        //                }
+        //                else //not compressed
+        //                {
+        //                    htmlQueryString = "https://www.uniprot.org/uniprot/?query=proteome:" + proteomeID + " reviewed:yes" + "&compress=no" + "&format=fasta" + "&include:no";
+        //                }
+        //            }
+        //        }
+        //        else //revied and unreviewed both
+        //        {
+        //            if (isoforms)
+        //            {
+        //                if (compressed)
+        //                {
+        //                    htmlQueryString = "https://www.uniprot.org/uniprot/?query=proteome:" + proteomeID + "&compress=yes" + "&format=fasta" + "&include:yes";
+        //                }
+        //                else //not compressed
+        //                {
+        //                    htmlQueryString = "https://www.uniprot.org/uniprot/?query=proteome:" + proteomeID + "&compress=no" + "&format=fasta" + "&include:yes";
+        //                }
+        //            }
+        //            else//no isoforms
+        //            {
+        //                if (compressed)
+        //                {
+        //                    htmlQueryString = "https://www.uniprot.org/uniprot/?query=proteome:" + proteomeID + "&compress=yes" + "&format=fasta" + "&include:no";
+        //                }
+        //                else //not compressed
+        //                {
+        //                    htmlQueryString = "https://www.uniprot.org/uniprot/?query=proteome:" + proteomeID + "&compress=no" + "&format=fasta" + "&include:no";
+        //                }
+        //            }
+        //        }
+        //    }
+        //    else //xml -- currently isoforms are automatically included (I think)
+        //    {
+        //        if (reviewed)
+        //        {
+        //            if (isoforms)
+        //            {
+        //                if (compressed)
+        //                {
+        //                    htmlQueryString = "https://www.uniprot.org/uniprot/?query=proteome:" + proteomeID + " reviewed:yes" + "&compress=yes" + "&format=xml" + "&include:yes";
+        //                }
+        //                else //not compressed
+        //                {
+        //                    htmlQueryString = "https://www.uniprot.org/uniprot/?query=proteome:" + proteomeID + " reviewed:yes" + "&compress=no" + "&format=xml" + "&include:yes";
+        //                }
+        //            }
+        //            else//no isoforms
+        //            {
+        //                if (compressed)
+        //                {
+        //                    htmlQueryString = "https://www.uniprot.org/uniprot/?query=proteome:" + proteomeID + " reviewed:yes" + "&compress=yes" + "&format=xml" + "&include:no";
+        //                }
+        //                else //not compressed
+        //                {
+        //                    htmlQueryString = "https://www.uniprot.org/uniprot/?query=proteome:" + proteomeID + " reviewed:yes" + "&compress=no" + "&format=xml" + "&include:no";
+        //                }
+        //            }
+        //        }
+        //        else //revied and unreviewed both
+        //        {
+        //            if (isoforms)
+        //            {
+        //                if (compressed)
+        //                {
+        //                    htmlQueryString = "https://www.uniprot.org/uniprot/?query=proteome:" + proteomeID + "&compress=yes" + "&format=xml" + "&include:yes";
+        //                }
+        //                else //not compressed
+        //                {
+        //                    htmlQueryString = "https://www.uniprot.org/uniprot/?query=proteome:" + proteomeID + "&compress=no" + "&format=xml" + "&include:yes";
+        //                }
+        //            }
+        //            else//no isoforms
+        //            {
+        //                if (compressed)
+        //                {
+        //                    htmlQueryString = "https://www.uniprot.org/uniprot/?query=proteome:" + proteomeID + "&compress=yes" + "&format=xml" + "&include:no";
+        //                }
+        //                else //not compressed
+        //                {
+        //                    htmlQueryString = "https://www.uniprot.org/uniprot/?query=proteome:" + proteomeID + "&compress=no" + "&format=xml" + "&include:no";
+        //                }
+        //            }
+        //        }
+        //    }
+
+        //    return htmlQueryString;
+        //}
+
         public static string GetUniProtHtmlQueryString(string proteomeID, bool reviewed, bool isoforms, bool xmlFormat, bool compressed)
         {
-            string htmlQueryString;
+            StringBuilder sb = new StringBuilder();
+            sb.Append("https://www.uniprot.org/uniprot/?query=proteome:" + proteomeID + " ");
+
             if (!xmlFormat) //fasta
             {
+                sb.Append("&format=fasta");
                 if (reviewed)
                 {
+                    sb.Append("&reviewed:yes");
                     if (isoforms)
                     {
+                        sb.Append("&include:yes");
                         if (compressed)
                         {
-                            htmlQueryString = "https://www.uniprot.org/uniprot/?query=proteome:" + proteomeID + " reviewed:yes" + "&compress=yes" + "&format=fasta" + "&include:yes";
+                            sb.Append("&compress=yes");
                         }
                         else //not compressed
                         {
-                            htmlQueryString = "https://www.uniprot.org/uniprot/?query=proteome:" + proteomeID + " reviewed:yes" + "&compress=no" + "&format=fasta" + "&include:yes";
+                            sb.Append("&compress=no");
                         }
                     }
                     else//no isoforms
                     {
+                        sb.Append("&include:no");
                         if (compressed)
                         {
-                            htmlQueryString = "https://www.uniprot.org/uniprot/?query=proteome:" + proteomeID + " reviewed:yes" + "&compress=yes" + "&format=fasta" + "&include:no";
+                            sb.Append("&compress=yes");
                         }
                         else //not compressed
                         {
-                            htmlQueryString = "https://www.uniprot.org/uniprot/?query=proteome:" + proteomeID + " reviewed:yes" + "&compress=no" + "&format=fasta" + "&include:no";
+                            sb.Append("&compress=no");
                         }
                     }
                 }
                 else //revied and unreviewed both
                 {
+                    //we don't need to append anything if we want both reviewed and unreviewed
                     if (isoforms)
                     {
+                        sb.Append("&include:yes");
                         if (compressed)
                         {
-                            htmlQueryString = "https://www.uniprot.org/uniprot/?query=proteome:" + proteomeID + "&compress=yes" + "&format=fasta" + "&include:yes";
+                            sb.Append("&compress=yes");
                         }
                         else //not compressed
                         {
-                            htmlQueryString = "https://www.uniprot.org/uniprot/?query=proteome:" + proteomeID + "&compress=no" + "&format=fasta" + "&include:yes";
+                            sb.Append("&compress=no");
                         }
                     }
                     else//no isoforms
                     {
+                        sb.Append("&include:no");
                         if (compressed)
                         {
-                            htmlQueryString = "https://www.uniprot.org/uniprot/?query=proteome:" + proteomeID + "&compress=yes" + "&format=fasta" + "&include:no";
+                            sb.Append("&compress=yes");
                         }
                         else //not compressed
                         {
-                            htmlQueryString = "https://www.uniprot.org/uniprot/?query=proteome:" + proteomeID + "&compress=no" + "&format=fasta" + "&include:no";
+                            sb.Append("&compress=no");
                         }
                     }
                 }
             }
             else //xml -- currently isoforms are automatically included (I think)
             {
+                sb.Append("&format=xml");
                 if (reviewed)
                 {
+                    sb.Append("&reviewed:yes");
                     if (isoforms)
                     {
+                        sb.Append("&include:yes");
                         if (compressed)
                         {
-                            htmlQueryString = "https://www.uniprot.org/uniprot/?query=proteome:" + proteomeID + " reviewed:yes" + "&compress=yes" + "&format=xml" + "&include:yes";
+                            sb.Append("&compress=yes");
                         }
                         else //not compressed
                         {
-                            htmlQueryString = "https://www.uniprot.org/uniprot/?query=proteome:" + proteomeID + " reviewed:yes" + "&compress=no" + "&format=xml" + "&include:yes";
+                            sb.Append("&compress=no");
                         }
                     }
                     else//no isoforms
                     {
+                        sb.Append("&include:no");
                         if (compressed)
                         {
-                            htmlQueryString = "https://www.uniprot.org/uniprot/?query=proteome:" + proteomeID + " reviewed:yes" + "&compress=yes" + "&format=xml" + "&include:no";
+                            sb.Append("&compress=yes");
                         }
                         else //not compressed
                         {
-                            htmlQueryString = "https://www.uniprot.org/uniprot/?query=proteome:" + proteomeID + " reviewed:yes" + "&compress=no" + "&format=xml" + "&include:no";
+                            sb.Append("&compress=no");
                         }
                     }
                 }
                 else //revied and unreviewed both
                 {
+                    //we don't need to append anything if we want both reviewed and unreviewed
                     if (isoforms)
                     {
+                        sb.Append("&include:yes");
                         if (compressed)
                         {
-                            htmlQueryString = "https://www.uniprot.org/uniprot/?query=proteome:" + proteomeID + "&compress=yes" + "&format=xml" + "&include:yes";
+                            sb.Append("&compress=yes");
                         }
                         else //not compressed
                         {
-                            htmlQueryString = "https://www.uniprot.org/uniprot/?query=proteome:" + proteomeID + "&compress=no" + "&format=xml" + "&include:yes";
+                            sb.Append("&compress=no");
                         }
                     }
                     else//no isoforms
                     {
+                        sb.Append("&include:no");
                         if (compressed)
                         {
-                            htmlQueryString = "https://www.uniprot.org/uniprot/?query=proteome:" + proteomeID + "&compress=yes" + "&format=xml" + "&include:no";
+                            sb.Append("&compress=yes");
                         }
                         else //not compressed
                         {
-                            htmlQueryString = "https://www.uniprot.org/uniprot/?query=proteome:" + proteomeID + "&compress=no" + "&format=xml" + "&include:no";
+                            sb.Append("&compress=no");
                         }
                     }
                 }
             }
 
-            return htmlQueryString;
+            return sb.ToString();
         }
 
         public static string GetUniprotFilename(string uniprotProteomeId, bool reviewed, bool isoforms, bool xmlFormat, bool compressed)
--- Test/GuiFunctionsTest.cs
+++ Test/GuiFunctionsTest.cs
@@ -1,8 +1,5 @@
 ﻿using GuiFunctions.Databases;
 using NUnit.Framework;
-using System;
-using System.Collections.Generic;
-using System.Text;
 
 namespace Test
 {
@@ -10,24 +7,24 @@ namespace Test
     public static class GuiFunctionsTest
     {
         [Test]
-        [TestCase("UP000000625", true, false, false, true, "https://www.uniprot.org/uniprot/?query=proteome:UP000000625 reviewed:yes&compress=yes&format=fasta&include:no")]
-        [TestCase("UP000000625", false, true, false, true, "https://www.uniprot.org/uniprot/?query=proteome:UP000000625&compress=yes&format=fasta&include:yes")]
-        [TestCase("UP000000625", false, true, false, false, "https://www.uniprot.org/uniprot/?query=proteome:UP000000625&compress=no&format=fasta&include:yes")]
-        [TestCase("UP000000625", false, false, false, true, "https://www.uniprot.org/uniprot/?query=proteome:UP000000625&compress=yes&format=fasta&include:no")]
-        [TestCase("UP000000625", false, false, false, false, "https://www.uniprot.org/uniprot/?query=proteome:UP000000625&compress=no&format=fasta&include:no")]
-        [TestCase("UP000000625", false, false, true, true, "https://www.uniprot.org/uniprot/?query=proteome:UP000000625&compress=yes&format=xml&include:no")]
-        [TestCase("UP000000625", false, false, true, false, "https://www.uniprot.org/uniprot/?query=proteome:UP000000625&compress=no&format=xml&include:no")]
-        [TestCase("UP000000625", true, true, true, true, "https://www.uniprot.org/uniprot/?query=proteome:UP000000625 reviewed:yes&compress=yes&format=xml&include:yes")]
-        [TestCase("UP000000625", true, true, true, false, "https://www.uniprot.org/uniprot/?query=proteome:UP000000625 reviewed:yes&compress=no&format=xml&include:yes")]
-        [TestCase("UP000000625", true, true, false, true, "https://www.uniprot.org/uniprot/?query=proteome:UP000000625 reviewed:yes&compress=yes&format=fasta&include:yes")]
-        [TestCase("UP000000625", true, false, true, true, "https://www.uniprot.org/uniprot/?query=proteome:UP000000625 reviewed:yes&compress=yes&format=xml&include:no")]
-        [TestCase("UP000000625", false, true, true, true, "https://www.uniprot.org/uniprot/?query=proteome:UP000000625&compress=yes&format=xml&include:yes")]
-        [TestCase("UP000000625", true, true, false, false, "https://www.uniprot.org/uniprot/?query=proteome:UP000000625 reviewed:yes&compress=no&format=fasta&include:yes")]
-        [TestCase("UP000000625", true, false, true, false, "https://www.uniprot.org/uniprot/?query=proteome:UP000000625 reviewed:yes&compress=no&format=xml&include:no")]
-        [TestCase("UP000000625", false, true, true, false, "https://www.uniprot.org/uniprot/?query=proteome:UP000000625&compress=no&format=xml&include:yes")]
-        [TestCase("UP000000625", true, false, false, false, "https://www.uniprot.org/uniprot/?query=proteome:UP000000625 reviewed:yes&compress=no&format=fasta&include:no")]
-        [TestCase("UP000000625", false, false, true, false, "https://www.uniprot.org/uniprot/?query=proteome:UP000000625&compress=no&format=xml&include:no")]
-        [TestCase("UP000000625", false, false, false, false, "https://www.uniprot.org/uniprot/?query=proteome:UP000000625&compress=no&format=fasta&include:no")]
+        [TestCase("UP000000625", true, false, false, true, "https://www.uniprot.org/uniprot/?query=proteome:UP000000625 &format=fasta&reviewed:yes&include:no&compress=yes")]
+        [TestCase("UP000000625", false, true, false, true, "https://www.uniprot.org/uniprot/?query=proteome:UP000000625 &format=fasta&include:yes&compress=yes")]
+        [TestCase("UP000000625", false, true, false, false, "https://www.uniprot.org/uniprot/?query=proteome:UP000000625 &format=fasta&include:yes&compress=no")]
+        [TestCase("UP000000625", false, false, false, true, "https://www.uniprot.org/uniprot/?query=proteome:UP000000625 &format=fasta&include:no&compress=yes")]
+        [TestCase("UP000000625", false, false, false, false, "https://www.uniprot.org/uniprot/?query=proteome:UP000000625 &format=fasta&include:no&compress=no")]
+        [TestCase("UP000000625", false, false, true, true, "https://www.uniprot.org/uniprot/?query=proteome:UP000000625 &format=xml&include:no&compress=yes")]
+        [TestCase("UP000000625", false, false, true, false, "https://www.uniprot.org/uniprot/?query=proteome:UP000000625 &format=xml&include:no&compress=no")]
+        [TestCase("UP000000625", true, true, true, true, "https://www.uniprot.org/uniprot/?query=proteome:UP000000625 &format=xml&reviewed:yes&include:yes&compress=yes")]
+        [TestCase("UP000000625", true, true, true, false, "https://www.uniprot.org/uniprot/?query=proteome:UP000000625 &format=xml&reviewed:yes&include:yes&compress=no")]
+        [TestCase("UP000000625", true, true, false, true, "https://www.uniprot.org/uniprot/?query=proteome:UP000000625 &format=fasta&reviewed:yes&include:yes&compress=yes")]
+        [TestCase("UP000000625", true, false, true, true, "https://www.uniprot.org/uniprot/?query=proteome:UP000000625 &format=xml&reviewed:yes&include:no&compress=yes")]
+        [TestCase("UP000000625", false, true, true, true, "https://www.uniprot.org/uniprot/?query=proteome:UP000000625 &format=xml&include:yes&compress=yes")]
+        [TestCase("UP000000625", true, true, false, false, "https://www.uniprot.org/uniprot/?query=proteome:UP000000625 &format=fasta&reviewed:yes&include:yes&compress=no")]
+        [TestCase("UP000000625", true, false, true, false, "https://www.uniprot.org/uniprot/?query=proteome:UP000000625 &format=xml&reviewed:yes&include:no&compress=no")]
+        [TestCase("UP000000625", false, true, true, false, "https://www.uniprot.org/uniprot/?query=proteome:UP000000625 &format=xml&include:yes&compress=no")]
+        [TestCase("UP000000625", true, false, false, false, "https://www.uniprot.org/uniprot/?query=proteome:UP000000625 &format=fasta&reviewed:yes&include:no&compress=no")]
+        [TestCase("UP000000625", false, false, true, false, "https://www.uniprot.org/uniprot/?query=proteome:UP000000625 &format=xml&include:no&compress=no")]
+        [TestCase("UP000000625", false, false, false, false, "https://www.uniprot.org/uniprot/?query=proteome:UP000000625 &format=fasta&include:no&compress=no")]
         public static void TestGetUniProtHtmlQueryString(string proteomeID, bool reviewed, bool isoforms, bool xmlFormat, bool compressed, string expectedResult)
         {
             string queryString = DownloadUniProtDatabaseFunctions.GetUniProtHtmlQueryString(proteomeID, reviewed, isoforms, xmlFormat, compressed);
