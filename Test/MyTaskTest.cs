45cf7047982d4330f214e9cfe73fab9b64aa907f
trishorts <mshort@chem.wisc.edu>
2021-03-17 19:47:59 -0500

    codemaid

--- Test/MyTaskTest.cs
+++ Test/MyTaskTest.cs
@@ -236,7 +236,6 @@ namespace Test
 
             string mzmlName = @"MakeSureFdrDoesntSkip.mzML";
 
-
             var theProteins = ProteinDbLoader.LoadProteinXML(xmlName, true, DecoyType.Reverse, new List<Modification>(), false, new List<string>(), out Dictionary<string, Modification> ok);
 
             List<Modification> fixedModifications = new List<Modification>();
@@ -407,7 +406,7 @@ namespace Test
             var protein = ProteinDbLoader.LoadProteinXML(xmlName, true, DecoyType.Reverse, new List<Modification>(), false, new List<string>(), out Dictionary<string, Modification> ok);
             var setList1 = protein[0].Digest(testPeptides.CommonParameters.DigestionParams, new List<Modification> { }, variableModifications).ToList();
             Assert.AreEqual(4, setList1.Count);
-            
+
             //Finally Write MZML file
             MsDataFile myMsDataFile = new TestDataFile(new List<PeptideWithSetModifications> { setList1[0], setList1[1], setList1[2], setList1[3], setList1[0], setList1[1] });
             string mzmlName = @"singleProteinWithRepeatedMods.mzML";
@@ -526,7 +525,7 @@ namespace Test
 
         /// <summary>
         /// Tests that pepXML is written
-        /// 
+        ///
         /// TODO: Assert pepXML properties
         /// </summary>
         [Test]
