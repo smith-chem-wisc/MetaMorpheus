2e5123d24db882e0b24060337e4341d0933ab7ef
trishorts <mshort@chem.wisc.edu>
2021-10-22 11:35:37 -0500

    add unit tests

---
+++ Test/PsvTsvTest.cs
@@ -0,0 +1,26 @@
+﻿using EngineLayer;
+using NUnit.Framework;
+using System.Collections.Generic;
+
+namespace Test
+{
+    [TestFixture]
+    public static class PsvTsvTest
+    {
+        [Test]
+        public static void ReadOGlycoSinglePsms()
+        {
+            string psmFile = @"Data\oglycoSinglePsms.psmtsv";
+            List<PsmFromTsv> parsedPsms = PsmTsvReader.ReadTsv(psmFile, out var warnings);
+            Assert.AreEqual(2, parsedPsms.Count);
+        }
+
+        [Test]
+        public static void ReadOGlycoPsms()
+        {
+            string psmFile = @"Data\oglyco.psmtsv";
+            List<PsmFromTsv> parsedPsms = PsmTsvReader.ReadTsv(psmFile, out var warnings);
+            Assert.AreEqual(9, parsedPsms.Count);
+        }
+    }
+}
