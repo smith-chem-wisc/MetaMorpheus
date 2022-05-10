f300eb3f29632a0fcd177ba6f71d94465d397992
MICHAEL SHORTREED <mrshortreed@wisc.edu>
2022-05-10 12:02:49 -0500

    miniClassicSearchEngineTest

--- Test/MbrAnalysisTest.cs
+++ Test/MbrAnalysisTest.cs
@@ -64,6 +64,60 @@ namespace Test
         }
 
         [Test]
+        public static void Bubba()
+        {
+            var testDir = Path.Combine(TestContext.CurrentContext.TestDirectory, @"TestData\SpectralLibrarySearch");
+            string myFile = Path.Combine(testDir, @"slicedMouse.raw");
+            MyFileManager myFileManager = new MyFileManager(true);
+            CommonParameters commonParameters = new(maxThreadsToUsePerFile: 1, scoreCutoff: 1);
+            MsDataFile myMsDataFile = myFileManager.LoadFile(myFile, commonParameters);
+
+            var variableModifications = new List<Modification>();
+            var fixedModifications = new List<Modification>();
+            var proteinList = new List<Protein> { new Protein("QTATIAHVTTMLGEVIGFNDHIVK", "P16858") };
+
+            string targetSpectralLibrary = Path.Combine(testDir, @"P16858_target.msp");
+            string decoySpectralLibrary = Path.Combine(testDir, @"P16858_decoy.msp");
+
+            List<string> specLibs = new List<string> { targetSpectralLibrary, decoySpectralLibrary };
+
+            SpectralLibrary sl = new(specLibs);
+
+            var searchModes = new SinglePpmAroundZeroSearchMode(5);
+
+            Ms2ScanWithSpecificMass[] listOfSortedms2Scans = MetaMorpheusTask.GetMs2Scans(myMsDataFile, null, new CommonParameters()).OrderBy(b => b.PrecursorMass).ToArray();
+
+            PeptideSpectralMatch[] allPsmsArray = new PeptideSpectralMatch[listOfSortedms2Scans.Length];
+            bool writeSpectralLibrary = false;
+            new ClassicSearchEngine(allPsmsArray, listOfSortedms2Scans, variableModifications, fixedModifications, null, null, null,
+                proteinList, searchModes, commonParameters, null, sl, new List<string>(), writeSpectralLibrary).Run();
+
+            // Single search mode
+            Assert.AreEqual(7, allPsmsArray.Length);
+            Assert.IsTrue(allPsmsArray[5].Score > 38);
+            Assert.AreEqual("VIHDNFGIVEGLMTTVHAITATQK", allPsmsArray[5].BaseSequence);
+            Assert.IsTrue(allPsmsArray[5].IsDecoy);
+
+            SpectralLibrarySearchFunction.CalculateSpectralAngles(sl, allPsmsArray, listOfSortedms2Scans, commonParameters);
+            Assert.That(allPsmsArray[5].SpectralAngle, Is.EqualTo(0.69).Within(0.01));
+
+
+            foreach (PeptideSpectralMatch psm in allPsmsArray.Where(p=>p != null))
+            {
+                PeptideWithSetModifications pwsm = psm.BestMatchingPeptides.First().Peptide;
+
+                List<(string FileName, CommonParameters Parameters)> fileSpecificParameters = new() {(myFile,commonParameters) };
+
+                new MiniClassicSearchEngine(pwsm, listOfSortedms2Scans, variableModifications, fixedModifications, searchModes,
+                    commonParameters, fileSpecificParameters, sl, new List<string>()).Run();
+
+            }
+
+
+            Assert.IsTrue(false);
+        }
+
+        [Test]
         public static void FalseMbrMCSETest()
         {
             SearchTask classicSearch = new SearchTask()
@@ -83,7 +137,8 @@ namespace Test
             var fileSpecificToml = Toml.ReadFile(filePath, MetaMorpheusTask.tomlConfig);
             fileSpecificParameters[0] = new FileSpecificParameters(fileSpecificToml);
 
-            List<PeptideSpectralMatch> allPsms = classicSearch.RunForPTMs(outputFolder, new List<DbForTask> { new DbForTask(fastaName, false) }, rawSlices, "PtmReturn", fileSpecificParameters);
+            //put this back when it gets fixed.
+            //List<PeptideSpectralMatch> allPsms = classicSearch.RunForPTMs(outputFolder, new List<DbForTask> { new DbForTask(fastaName, false) }, rawSlices, "PtmReturn", fileSpecificParameters);
 
             var engine = new EverythingRunnerEngine(new List<(string, MetaMorpheusTask)> { ("ClassicSearch", classicSearch) }, rawSlices, new List<DbForTask> { new DbForTask(fastaName, false) }, outputFolder);
             engine.Run();
