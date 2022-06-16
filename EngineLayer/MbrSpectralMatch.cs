fac7383daf71eec40bc389325a9f65444d38f9fe
Alexander <AlexSolivais@gmail.com>
2022-06-16 12:08:56 -0500

    Finished writers for MbrSpectralMatch

--- EngineLayer/MbrSpectralMatch.cs
+++ EngineLayer/MbrSpectralMatch.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
+using System.Globalization;
 using Proteomics;
 using Proteomics.ProteolyticDigestion;
 using FlashLFQ;
@@ -12,13 +12,13 @@ namespace EngineLayer
 {
     public class MbrSpectralMatch
     {
-        public PeptideSpectralMatch spectralLibraryMatch;
+        public PeptideSpectralMatch spectralLibraryMatch { get; set; }
 
-        public ChromatographicPeak acceptorPeak;
+        public ChromatographicPeak acceptorPeak { get; set; }
 
-        public bool originalMatchFound;
+        public bool originalMatchFound { get; private set; }
 
-        public FdrInfo originalFdrInfo;
+        public FdrInfo originalFdrInfo { get; private set; }
 
         public MbrSpectralMatch(PeptideSpectralMatch spectralLibraryMatch, ChromatographicPeak acceptorPeak)
         {
@@ -47,9 +47,28 @@ namespace EngineLayer
                 var sb = new StringBuilder();
                 sb.Append(PeptideSpectralMatch.GetTabSeparatedHeader());
                 sb.Append('\t');
+                sb.Append(OriginalFdrHeader);
+                sb.Append('\t');
                 sb.Append(ChromatographicPeak.TabSeparatedHeader);
                 return sb.ToString();
             }
+        }
+
+        public static string OriginalFdrHeader
+        {
+            get
+            {
+                List<string> originalFdrHeader = new();
+                originalFdrHeader.Add("Original Cumulative Target");
+                originalFdrHeader.Add("Original Cumulative Decoy");
+                originalFdrHeader.Add("Original Cumulative Target Notch");
+                originalFdrHeader.Add("Original Cumulative Decoy Notch");
+                originalFdrHeader.Add("Original QValue");
+                originalFdrHeader.Add("Original QValue Notch");
+                originalFdrHeader.Add("Original PEP");
+                originalFdrHeader.Add("Original PEP_QValue");
+                return string.Join('\t', originalFdrHeader);
+           }
         }
 
         public override string ToString()
@@ -57,8 +76,39 @@ namespace EngineLayer
             var sb = new StringBuilder();
             sb.Append(spectralLibraryMatch.ToString());
             sb.Append('\t');
+            sb.Append(OriginalFdrWriter());
+            sb.Append('\t');
             sb.Append(acceptorPeak.ToString());
             return sb.ToString();
+        }
+
+        private string OriginalFdrWriter()
+        {
+            string cumulativeTarget = " ";
+            string cumulativeDecoy = " ";
+            string cumulativeTargetNotch = " ";
+            string cumulativeDecoyNotch = " ";
+            string qValue = " ";
+            string qValueNotch = " ";
+            string PEP = " ";
+            string PEP_Qvalue = " ";
+
+            if (originalMatchFound && originalFdrInfo != null)
+            {
+                cumulativeTarget = originalFdrInfo.CumulativeTarget.ToString(CultureInfo.InvariantCulture);
+                cumulativeDecoy = originalFdrInfo.CumulativeDecoy.ToString(CultureInfo.InvariantCulture);
+                cumulativeTargetNotch = originalFdrInfo.CumulativeTargetNotch.ToString(CultureInfo.InvariantCulture);
+                qValue = originalFdrInfo.QValue.ToString("F6", CultureInfo.InvariantCulture);
+                cumulativeDecoyNotch = originalFdrInfo.CumulativeDecoyNotch.ToString(CultureInfo.InvariantCulture);
+                qValueNotch = originalFdrInfo.QValueNotch.ToString("F6", CultureInfo.InvariantCulture);
+                PEP = originalFdrInfo.PEP.ToString();
+                PEP_Qvalue = originalFdrInfo.PEP_QValue.ToString();
+            }
+
+            return string.Join('\t', new List<string> { cumulativeTarget, cumulativeDecoy,
+                                                        cumulativeTargetNotch, cumulativeDecoyNotch,
+                                                        qValue, qValueNotch, PEP, PEP_Qvalue});
+
         }
 
 
