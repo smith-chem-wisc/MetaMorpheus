8b828c509b8fd52c3287205a94234550d5ef1ca5
Zach Danaceau <100874533+zdanaceau@users.noreply.github.com>
2022-07-27 22:54:41 -0500

    Working on adding paired target decoy functionality.

---
+++ EngineLayer/ClassicSearch/TargetDecoyOptions.cs
@@ -0,0 +1,13 @@
+﻿namespace EngineLayer.ClassicSearch
+{
+    public enum TargetDecoyOptions
+    {
+        ReversedDecoy,
+        StandardScrambled,
+        //Items below here are potential other options
+        //SetScrambled,
+        //FragmentScrambled,
+        //OptimizedDissimilarity
+
+    }
+}
