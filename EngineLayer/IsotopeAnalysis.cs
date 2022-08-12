0370f8ee891983d5291af2cc0b854b1f302c5bb6
Alex <AlexSolivais@gmail.com>
2022-08-12 14:18:32 -0500

    Added theorIsoDist from FlashLFQ

--- EngineLayer/IsotopeAnalysis.cs
+++ EngineLayer/IsotopeAnalysis.cs
@@ -8,6 +8,7 @@ using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using IO.ThermoRawFileReader;
+using Proteomics.AminoAcidPolymer;
 
 namespace EngineLayer
 {
@@ -16,6 +17,122 @@ namespace EngineLayer
         public Dictionary<string, MsDataScan[]> _ms1Scans { get; private set; }
 
         public List<PeptideSpectralMatch> AllPsms { get; set; }
+
+        public Dictionary<string, List<(double, double)>> _modifiedSequenceToIsotopicDistribution {get; set; }
+
+        private void CalculateTheoreticalIsotopeDistributions()
+        {
+            _modifiedSequenceToIsotopicDistribution = new Dictionary<string, List<(double, double)>>();
+
+            // calculate averagine (used for isotopic distributions for unknown modifications)
+            double averageC = 4.9384;
+            double averageH = 7.7583;
+            double averageO = 1.4773;
+            double averageN = 1.3577;
+            double averageS = 0.0417;
+
+            double averagineMass =
+                PeriodicTable.GetElement("C").AverageMass * averageC +
+                PeriodicTable.GetElement("H").AverageMass * averageH +
+                PeriodicTable.GetElement("O").AverageMass * averageO +
+                PeriodicTable.GetElement("N").AverageMass * averageN +
+                PeriodicTable.GetElement("S").AverageMass * averageS;
+
+            // calculate monoisotopic masses and isotopic envelope for the base sequences
+            foreach (PeptideSpectralMatch psm in AllPsms)
+            {
+                if (_modifiedSequenceToIsotopicDistribution.ContainsKey(psm.FullSequence))
+                {
+                    continue;
+                }
+
+                ChemicalFormula formula = psm.ModsChemicalFormula;
+
+                var isotopicMassesAndNormalizedAbundances = new List<(double massShift, double abundance)>();
+
+                if (formula == null)
+                {
+                    if (psm.PeptideMonisotopicMass == null) continue;
+                    double massDiff = (double)psm.PeptideMonisotopicMass;
+
+                    if (!String.IsNullOrEmpty(psm.BaseSequence))
+                    {
+                        Peptide baseSequence = new Peptide(psm.BaseSequence);
+                        formula = baseSequence.GetChemicalFormula();
+                        // add averagine for any unknown mass difference (i.e., a modification)
+                        massDiff -= baseSequence.MonoisotopicMass;
+
+                        if (Math.Abs(massDiff) > 20)
+                        {
+                            double averagines = massDiff / averagineMass;
+
+                            formula.Add("C", (int)Math.Round(averagines * averageC, 0));
+                            formula.Add("H", (int)Math.Round(averagines * averageH, 0));
+                            formula.Add("O", (int)Math.Round(averagines * averageO, 0));
+                            formula.Add("N", (int)Math.Round(averagines * averageN, 0));
+                            formula.Add("S", (int)Math.Round(averagines * averageS, 0));
+                        }
+                    }
+                    else
+                    {
+                        double averagines = massDiff / averagineMass;
+                        string averagineFormulaString =
+                            "C" + (int)Math.Round(averagines * averageC, 0) +
+                            "H" + (int)Math.Round(averagines * averageH, 0) +
+                            "O" + (int)Math.Round(averagines * averageO, 0) +
+                            "N" + (int)Math.Round(averagines * averageN, 0) +
+                            "S" + (int)Math.Round(averagines * averageS, 0);
+                        formula = ChemicalFormula.ParseFormula(averagineFormulaString);
+                    }
+                }
+
+                var isotopicDistribution = IsotopicDistribution.GetDistribution(formula, 0.125, 1e-8);
+
+                double[] masses = isotopicDistribution.Masses.ToArray();
+                double[] abundances = isotopicDistribution.Intensities.ToArray();
+
+                for (int i = 0; i < masses.Length; i++)
+                {
+                    masses[i] += (id.MonoisotopicMass - formula.MonoisotopicMass);
+                }
+
+                double highestAbundance = abundances.Max();
+                int highestAbundanceIndex = Array.IndexOf(abundances, highestAbundance);
+
+                for (int i = 0; i < masses.Length; i++)
+                {
+                    // expected isotopic mass shifts for this peptide
+                    masses[i] -= id.MonoisotopicMass;
+
+                    // normalized abundance of each isotope
+                    abundances[i] /= highestAbundance;
+
+                    // look for these isotopes
+                    if (isotopicMassesAndNormalizedAbundances.Count < NumIsotopesRequired || abundances[i] > 0.1)
+                    {
+                        isotopicMassesAndNormalizedAbundances.Add((masses[i], abundances[i]));
+                    }
+                }
+
+                _modifiedSequenceToIsotopicDistribution.Add(id.ModifiedSequence, isotopicMassesAndNormalizedAbundances);
+            }
+
+            var minChargeState = _allIdentifications.Min(p => p.PrecursorChargeState);
+            var maxChargeState = _allIdentifications.Max(p => p.PrecursorChargeState);
+            _chargeStates = Enumerable.Range(minChargeState, (maxChargeState - minChargeState) + 1);
+
+            var peptideModifiedSequences = _allIdentifications.GroupBy(p => p.ModifiedSequence);
+            foreach (var identifications in peptideModifiedSequences)
+            {
+                // isotope where normalized abundance is 1
+                double mostAbundantIsotopeShift = _modifiedSequenceToIsotopicDistribution[identifications.First().ModifiedSequence].First(p => p.Item2 == 1.0).Item1;
+
+                foreach (Identification identification in identifications)
+                {
+                    identification.PeakfindingMass = identification.MonoisotopicMass + mostAbundantIsotopeShift;
+                }
+            }
+        }
 
         public void ScoreEnvelopes()
         {
