f8f67f1307b0e4c655fb60828e93c66868bcc6ca
weaversd <sweaver4@nd.edu>
2022-09-22 14:08:47 -0500

    Fixed bug for exporting data visualization. Will now create export directory if it doesn't already exist.

--- GUI/MetaDraw/MetaDraw.xaml.cs
+++ GUI/MetaDraw/MetaDraw.xaml.cs
@@ -658,6 +658,11 @@ namespace MetaMorpheusGUI
             plotViewStat.UpdateLayout();
             PlotViewStat_SizeChanged(plotViewStat, null);
 
+            if (!Directory.Exists(fileDirectory))
+            {
+                Directory.CreateDirectory(fileDirectory);
+            }
+
             using (Stream writePDF = File.Create(Path.Combine(fileDirectory, fileName)))
             {
                 PdfExporter.Export(plotViewStat.Model, writePDF, 1000, 700);
