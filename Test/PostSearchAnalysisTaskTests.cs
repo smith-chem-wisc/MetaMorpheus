4238e822af451368574d8427e2132345d347accb
MICHAEL SHORTREED <mrshortreed@wisc.edu>
2022-11-11 11:06:13 -0600

    test correct results with pep qvalue filter on

--- Test/PostSearchAnalysisTaskTests.cs
+++ Test/PostSearchAnalysisTaskTests.cs
@@ -5,8 +5,11 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using EngineLayer;
+using MassSpectrometry;
 using Nett;
 using NUnit.Framework;
+using Proteomics.ProteolyticDigestion;
+using Proteomics.RetentionTimePrediction;
 using TaskLayer;
 
 namespace Test
@@ -17,14 +20,15 @@ namespace Test
         [Test]
         public static void AllResultsAndResultsTxtTests()
         {
-            var myTomlPath = Path.Combine(TestContext.CurrentContext.TestDirectory, @"TestData\Task1-SearchTaskconfig.toml");
-            var searchTaskLoaded = Toml.ReadFile<SearchTask>(myTomlPath, MetaMorpheusTask.tomlConfig);
+            //First test that AllResults and Results display correct numbers of peptides and psms with q-value filter on
+            string myTomlPath = Path.Combine(TestContext.CurrentContext.TestDirectory, @"TestData\Task1-SearchTaskconfig.toml");
+            SearchTask searchTaskLoaded = Toml.ReadFile<SearchTask>(myTomlPath, MetaMorpheusTask.tomlConfig);
             string outputFolder = Path.Combine(TestContext.CurrentContext.TestDirectory, @"TestData\PostSearchAnalysisTaskTest");
             string myFile1 = Path.Combine(TestContext.CurrentContext.TestDirectory, @"TestData\sliced_b6.mzML");
             string myFile2 = Path.Combine(TestContext.CurrentContext.TestDirectory, @"TestData\sliced_b6_2.mzML");
             string myDatabase = Path.Combine(TestContext.CurrentContext.TestDirectory, @"TestData\LowResSnip_B6_mouse_11700_117500pruned.xml");
 
-            var engineToml = new EverythingRunnerEngine(new List<(string, MetaMorpheusTask)> { ("postSearchAnalysisTaskTestOutput", searchTaskLoaded) }, new List<string> { myFile1, myFile2 }, new List<DbForTask> { new DbForTask(myDatabase, false) }, outputFolder);
+            EverythingRunnerEngine engineToml = new EverythingRunnerEngine(new List<(string, MetaMorpheusTask)> { ("postSearchAnalysisTaskTestOutput", searchTaskLoaded) }, new List<string> { myFile1, myFile2 }, new List<DbForTask> { new DbForTask(myDatabase, false) }, outputFolder);
             engineToml.Run();
 
             string allResultsFile = Path.Combine(outputFolder, "allResults.txt");
@@ -51,6 +55,39 @@ namespace Test
             Assert.AreEqual("Target protein groups within 1 % FDR in sliced_b6_2: 3", results[20]);
             Assert.AreEqual("sliced_b6 Target peptides with q-value = 0.01 : 29", results[21]);
             Assert.AreEqual("sliced_b6_2 Target peptides with q-value = 0.01 : 3", results[23]);
+
+            Directory.Delete(outputFolder, true);
+
+            //Second test that AllResults and Results display correct numbers of peptides and psms with PEP q-value filter on
+            myTomlPath = Path.Combine(TestContext.CurrentContext.TestDirectory, @"TestData\Task2-SearchTaskconfig.toml");
+            searchTaskLoaded = Toml.ReadFile<SearchTask>(myTomlPath, MetaMorpheusTask.tomlConfig);
+            engineToml = new EverythingRunnerEngine(new List<(string, MetaMorpheusTask)> { ("postSearchAnalysisTaskTestOutput", searchTaskLoaded) }, new List<string> { myFile1, myFile2 }, new List<DbForTask> { new DbForTask(myDatabase, false) }, outputFolder);
+            engineToml.Run();
+
+            allResultsFile = Path.Combine(outputFolder, "allResults.txt");
+            allResults = File.ReadAllLines(allResultsFile);
+            Assert.AreEqual("All target PSMs with pep q-value = 0.01: 46", allResults[12]);
+            Assert.AreEqual("All target peptides with pep q-value = 0.01 : 35", allResults[13]);
+            Assert.AreEqual("All target protein groups within 1% FDR: 29", allResults[14]);
+            Assert.AreEqual("sliced_b6 target PSMs with pep q-value = 0.01: 39", allResults[18]);
+            Assert.AreEqual("sliced_b6_2 target PSMs with pep q-value = 0.01: 7", allResults[22]);
+            Assert.AreEqual("Target protein groups within 1 % FDR in sliced_b6: 29", allResults[24]);
+            Assert.AreEqual("Target protein groups within 1 % FDR in sliced_b6_2: 3", allResults[25]);
+            Assert.AreEqual("sliced_b6 Target peptides with pep q-value = 0.01 : 30", allResults[26]);
+            Assert.AreEqual("sliced_b6_2 Target peptides with pep q-value = 0.01 : 6", allResults[28]);
+
+
+            resultsFile = Path.Combine(outputFolder, @"postSearchAnalysisTaskTestOutput\results.txt");
+            results = File.ReadAllLines(resultsFile);
+            Assert.AreEqual("All target PSMs with pep q-value = 0.01: 46", results[7]);
+            Assert.AreEqual("All target peptides with pep q-value = 0.01 : 35", results[8]);
+            Assert.AreEqual("All target protein groups within 1% FDR: 29", results[9]);
+            Assert.AreEqual("sliced_b6 target PSMs with pep q-value = 0.01: 39", results[13]);
+            Assert.AreEqual("sliced_b6_2 target PSMs with pep q-value = 0.01: 7", results[17]);
+            Assert.AreEqual("Target protein groups within 1 % FDR in sliced_b6: 29", results[19]);
+            Assert.AreEqual("Target protein groups within 1 % FDR in sliced_b6_2: 3", results[20]);
+            Assert.AreEqual("sliced_b6 Target peptides with pep q-value = 0.01 : 30", results[21]);
+            Assert.AreEqual("sliced_b6_2 Target peptides with pep q-value = 0.01 : 6", results[23]);
 
             Directory.Delete(outputFolder, true);
         }
