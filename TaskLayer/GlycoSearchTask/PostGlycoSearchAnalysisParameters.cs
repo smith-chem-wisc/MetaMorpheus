74e980f419d97620de49dc4ca1cc69e324752182
MICHAEL SHORTREED <mrshortreed@wisc.edu>
2023-02-06 12:36:55 -0600

    lets see whats different

---
+++ TaskLayer/GlycoSearchTask/PostGlycoSearchAnalysisParameters.cs
@@ -0,0 +1,33 @@
+﻿
+using EngineLayer;
+using EngineLayer.GlycoSearch;
+using FlashLFQ;
+using Proteomics;
+using Proteomics.ProteolyticDigestion;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TaskLayer.GlycoSearchTask
+{
+    public class PostGlycoSearchAnalysisParameters
+    {
+        public MyTaskResults GlycoSearchTaskResults { get; set; }
+        public string SearchTaskId { get; set; }
+        public GlycoSearchParameters GlycoSearchParameters { get; set; }
+        public List<Protein> ProteinList { get; set; }
+        public List<Modification> VariableModifications { get; set; }
+        public List<Modification> FixedModifications { get; set; }
+        public HashSet<DigestionParams> ListOfDigestionParams { get; set; }
+        public List<GlycoSpectralMatch> AllPsms { get; set; }
+        public FlashLfqResults FlashLfqResults { get; set; }
+        public string OutputFolder { get; set; }
+        public string IndividualResultsOutputFolder { get; set; }
+        public FileSpecificParameters[] FileSettingsList { get; set; }
+        public MyFileManager MyFileManager { get; set; }
+        public List<DbForTask> DatabaseFilenameList { get; set; }
+        public List<string> CurrentRawFileList { get; set; }
+    }
+}
