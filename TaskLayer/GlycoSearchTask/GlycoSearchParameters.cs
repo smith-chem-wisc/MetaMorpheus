e8ffd9efef2e8bd3adc44c968c2194bc077b346e
MICHAEL SHORTREED <mrshortreed@wisc.edu>
2023-02-13 10:45:13 -0600

    delete unused codee

--- TaskLayer/GlycoSearchTask/GlycoSearchParameters.cs
+++ TaskLayer/GlycoSearchTask/GlycoSearchParameters.cs
@@ -11,26 +11,6 @@ namespace TaskLayer
         {
 
         }
-        public GlycoSearchParameters(string oGlycanDatabaseFile = "OGlycan.gdb", string nGlycanDatabaseFile = "NGlycan.gdb", GlycoSearchType glycoSearchType = GlycoSearchType.OGlycanSearch, 
-            bool oxoniumIonFit = true, DecoyType decoyType = DecoyType.Reverse, int glycoSearchTopNum = 50, int maxOglycansAllowed = 4, 
-            bool doParsimony = false, bool noOneHitWonders = false, bool modPeptidesAreDifferent = false, bool writeIndividualFiles = false, bool writeDecoys = false, bool writeContaminants = false)
-        {
-            OGlycanDatabasefile = oGlycanDatabaseFile;
-            NGlycanDatabasefile = nGlycanDatabaseFile;
-            GlycoSearchType = glycoSearchType;
-            OxoniumIonFilt = oxoniumIonFit;
-            DecoyType = decoyType;
-            GlycoSearchTopNum = glycoSearchTopNum;
-            MaximumOGlycanAllowed = maxOglycansAllowed;
-
-            DoParsimony = doParsimony;
-            NoOneHitWonders = noOneHitWonders;
-            ModPeptidesAreDifferent = modPeptidesAreDifferent;
-
-            WriteIndividualFiles = writeIndividualFiles;
-            WriteDecoys = writeDecoys;
-            WriteContaminants = writeContaminants;
-        }
         public string OGlycanDatabasefile { get; set; }
         public string NGlycanDatabasefile { get; set; }
         public GlycoSearchType GlycoSearchType { get; set; }
