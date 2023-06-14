5cd5b44c299023702d52526e1b858a1d6b8e668c
Alex <AlexSolivais@gmail.com>
2023-06-14 15:41:50 -0500

    Further changes to PostSearchAnalysisTaskTest

--- MetaMorpheus/Test/PostSearchAnalysisTaskTests.cs
+++ MetaMorpheus/Test/PostSearchAnalysisTaskTests.cs
@@ -36,10 +36,13 @@ namespace Test
 
             // b6_2 is searched twice. First with two files being searched simultaneously, then with b6_2 by itself
             // This allows us to compare the file specific results produced by in the two file search to the output
-            // produced by searching the file by itself. They should be identical
+            // produced by searching the file by itself. The number of PSMs and Peptides observed should be the same
+            // for both single-file and multi-file searches.
+            // The number of protein groups will be different, because protein inference is performed once, using every peptide
+            // identified across all files.
             int b6_2ExpectedPsms = 6;
             int b6_2ExpectedPeptides = 5;
-            int b6_2ExpectedProteinGroups = 4;
+            
 
             Assert.AreEqual("All target PSMs with q-value = 0.01: 41", allResults[12]);
             Assert.AreEqual("All target peptides with q-value = 0.01 : 32", allResults[13]);
@@ -50,7 +53,7 @@ namespace Test
             
             Assert.AreEqual("sliced_b6_2 target PSMs with q-value = 0.01: " + b6_2ExpectedPsms, allResults[22]);
             Assert.AreEqual("sliced_b6_2 Target peptides with q-value = 0.01 : " + b6_2ExpectedPeptides, allResults[28]);
-            //Assert.AreEqual("Target protein groups within 1 % FDR in sliced_b6_2: " + b6_2ExpectedProteinGroups, allResults[25]);
+            Assert.AreEqual("Target protein groups within 1 % FDR in sliced_b6_2: 3", allResults[25]);
 
             string resultsFile = Path.Combine(outputFolder, @"postSearchAnalysisTaskTestOutput\results.txt");
             string[] results = File.ReadAllLines(resultsFile);
@@ -60,10 +63,11 @@ namespace Test
             Assert.AreEqual("sliced_b6 target PSMs with q-value = 0.01: 37", results[13]);
             Assert.AreEqual("Target protein groups within 1 % FDR in sliced_b6: 29", results[19]);
             Assert.AreEqual("sliced_b6 Target peptides with q-value = 0.01 : 29", results[21]);
+            Assert.AreEqual("Target protein groups within 1 % FDR in sliced_b6_2: 3", results[20]);
 
             Assert.AreEqual("sliced_b6_2 target PSMs with q-value = 0.01: " + b6_2ExpectedPsms, results[17]);
             Assert.AreEqual("sliced_b6_2 Target peptides with q-value = 0.01 : " + b6_2ExpectedPeptides, results[23]);
-            //Assert.AreEqual("Target protein groups within 1 % FDR in sliced_b6_2: " + b6_2ExpectedProteinGroups, results[20]);
+            
 
             Directory.Delete(outputFolder, true);
 
@@ -77,7 +81,7 @@ namespace Test
             string[] singleFileResults = File.ReadAllLines(resultsFile);
             Assert.AreEqual("All target PSMs with q-value = 0.01: " + b6_2ExpectedPsms, singleFileResults[7]); 
             Assert.AreEqual("All target peptides with q-value = 0.01 : " + b6_2ExpectedPeptides, singleFileResults[8]); 
-            Assert.AreEqual("All target protein groups with q-value = 0.01 (1% FDR): " + b6_2ExpectedProteinGroups, singleFileResults[9]); 
+            Assert.AreEqual("All target protein groups with q-value = 0.01 (1% FDR): 4", singleFileResults[9]); 
 
             //Second test that AllResults and Results display correct numbers of peptides and psms with PEP q-value filter on
             myTomlPath = Path.Combine(TestContext.CurrentContext.TestDirectory, @"TestData\Task2-SearchTaskconfig.toml");
