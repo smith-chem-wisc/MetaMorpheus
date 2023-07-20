0944da5020ddcfab17544623febe2ae524cd330b
Michael Shortreed <mshort@chem.wisc.edu>
2023-07-20 15:18:24 -0500

    eliminated unneccessary usings

--- MetaMorpheus/GUI/MetaDraw/MetaDraw.xaml.cs
+++ MetaMorpheus/GUI/MetaDraw/MetaDraw.xaml.cs
@@ -1,11 +1,7 @@
 using Easy.Common.Extensions;
 using EngineLayer;
 using GuiFunctions;
-using Nett;
 using OxyPlot;
-using Proteomics;
-using Proteomics.Fragmentation;
-using Proteomics.ProteolyticDigestion;
 using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
@@ -16,14 +12,10 @@ using System.Globalization;
 using System.IO;
 using System.Linq;
 using System.Reflection;
-using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
 
 namespace MetaMorpheusGUI
 {
