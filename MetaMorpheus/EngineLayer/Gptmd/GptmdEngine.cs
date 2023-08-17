d0eeb4a1d5e055247e5ade7d08a836eea2a8caee
Michael Shortreed <mshort@chem.wisc.edu>
2023-08-17 11:54:31 -0500

    s

--- MetaMorpheus/EngineLayer/Gptmd/GptmdEngine.cs
+++ MetaMorpheus/EngineLayer/Gptmd/GptmdEngine.cs
@@ -6,7 +6,6 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Easy.Common.Extensions;
-using ThermoFisher.CommonCore.Data.Business;
 
 namespace EngineLayer.Gptmd
 {
