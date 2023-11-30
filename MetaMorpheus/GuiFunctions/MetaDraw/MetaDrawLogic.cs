beadd9697dce17654323a2fb29acc1b7c39e1296
Nic Bollis <nbollis@comcast.net>
2023-11-30 11:36:04 -0600

    Updated to MzLib 1.0.542

--- MetaMorpheus/GuiFunctions/MetaDraw/MetaDrawLogic.cs
+++ MetaMorpheus/GuiFunctions/MetaDraw/MetaDrawLogic.cs
@@ -22,6 +22,7 @@ using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
 using Org.BouncyCastle.Asn1.X509.Qualified;
 using Readers;
+using System.Threading;
 
 namespace GuiFunctions
 {
@@ -795,6 +796,7 @@ namespace GuiFunctions
                 {
                     connection.Value.CloseDynamicConnection();
                 }
+                Thread.Sleep(1000);
                 MsDataFiles.Clear();
             }
         }
