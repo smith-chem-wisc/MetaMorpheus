6e00eed33439dfc70f7db95be973f804525a670f
Alex <AlexSolivais@gmail.com>
2023-11-30 17:10:23 -0600

    Deleted unused code

--- MetaMorpheus/GuiFunctions/MetaDraw/SpectrumMatch/SpectrumMatchPlot.cs
+++ MetaMorpheus/GuiFunctions/MetaDraw/SpectrumMatch/SpectrumMatchPlot.cs
@@ -605,33 +605,6 @@ namespace GuiFunctions
 
             Model.Annotations.Add(annotation);
         }
-
-        public string CalculateSpectralAngleOnTheFly(LibrarySpectrum librarySpectrum)
-        {
-            List<MatchedFragmentIon> spectrumMatchFragments = new(SpectrumMatch.MatchedIons);
-            List<MatchedFragmentIon> libraryFragments = new(librarySpectrum.MatchedFragmentIons);
-            if(SpectrumMatch.BetaPeptideMatchedIons.IsNotNullOrEmpty() && librarySpectrum is CrosslinkLibrarySpectrum)
-            {
-                var xlLibrarySpectrum = (CrosslinkLibrarySpectrum)librarySpectrum;
-                spectrumMatchFragments.AddRange(SpectrumMatch.BetaPeptideMatchedIons);
-                if(xlLibrarySpectrum.BetaPeptideSpectrum?.MatchedFragmentIons != null)
-                    libraryFragments.AddRange(xlLibrarySpectrum.BetaPeptideSpectrum.MatchedFragmentIons);
-            }
-
-            SpectralSimilarity spectraComparison = new SpectralSimilarity(
-                spectrumMatchFragments.Select(f => f.Mz).ToArray(),
-                spectrumMatchFragments.Select(f => f.Intensity).ToArray(),
-                libraryFragments.Select(f => f.Mz).ToArray(),
-                libraryFragments.Select(f => f.Intensity).ToArray(),
-                SpectralSimilarity.SpectrumNormalizationScheme.mostAbundantPeak,
-                toleranceInPpm: 20,
-                allPeaks: true);
-            double? spectralContrastAngle = spectraComparison.SpectralContrastAngle();
-
-            return spectralContrastAngle == null
-                ? "N/A"
-                : ((double)spectralContrastAngle).ToString("F4");
-        }
     }
 
     //TODO: move this to mzLib (https://github.com/smith-chem-wisc/mzLib/blob/master/mzPlot/Annotations/PlotTextAnnotation.cs)
