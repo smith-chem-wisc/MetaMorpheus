ac8bce4de66b8abf94249a333bdb68d7dd286ba9
Michael Shortreed <mshort@chem.wisc.edu>
2024-01-23 11:19:15 -0600

    add unit test to test that chymotrypsin cleaves after L

---
+++ MetaMorpheus/Test/ProteaseTests.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using EngineLayer;
+using Nett;
+using NUnit.Framework;
+using TaskLayer;
+
+
+namespace Test
+{
+    [TestFixture]
+    public static class ProteaseTests
+    {
+        [Test]
+        public static void TestChymotrypsinCleaveAfterLeucine()
+        {
+            var myTomlPath = Path.Combine(TestContext.CurrentContext.TestDirectory, @"DatabaseTests\Task1-SearchTaskconfig.toml");
+            var searchTaskLoaded = Toml.ReadFile<SearchTask>(myTomlPath, MetaMorpheusTask.tomlConfig);
+            string outputFolder = Path.Combine(TestContext.CurrentContext.TestDirectory, @"DatabaseTests\TestProtease");
+            Directory.CreateDirectory(outputFolder);
+            string myFile = Path.Combine(TestContext.CurrentContext.TestDirectory, @"DatabaseTests\Q9UHB6_Chym_snip.mzML");
+            string myDatabase = Path.Combine(TestContext.CurrentContext.TestDirectory, @"DatabaseTests\Q9UHB6.fasta");
+
+            var engineToml = new EverythingRunnerEngine(new List<(string, MetaMorpheusTask)> { ("SearchTOML", searchTaskLoaded) }, new List<string> { myFile }, new List<DbForTask> { new DbForTask(myDatabase, false) }, outputFolder);
+            engineToml.Run();
+
+            string psmFile = Path.Combine(outputFolder, @"SearchTOML\AllPSMs.psmtsv");
+
+            List<PsmFromTsv> parsedPsms = PsmTsvReader.ReadTsv(psmFile, out var warnings);
+            PsmFromTsv psm = parsedPsms.First();
+            Assert.AreEqual("FTQTSGETTDADKEPAGEDK", psm.BaseSeq);
+            Directory.Delete(outputFolder,true);
+        }
+    }
+}
