de5f17a176b1fc3b7048ea06c617fb42cd2c2300
Michael Shortreed <mshort@chem.wisc.edu>
2024-02-12 11:52:36 -0600

    no change

--- MetaMorpheus/EngineLayer/PsmTsv/PsmTsvWriter.cs
+++ MetaMorpheus/EngineLayer/PsmTsv/PsmTsvWriter.cs
@@ -2,7 +2,6 @@
 using Proteomics;
 using Proteomics.Fragmentation;
 using Proteomics.ProteolyticDigestion;
-using System;
 using System.Collections.Generic;
 using System.Globalization;
 using System.IO;
