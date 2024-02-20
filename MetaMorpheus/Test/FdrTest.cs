0435b610a8044c054b64a3f933cbbd79d1b5097a
Michael Shortreed <mshort@chem.wisc.edu>
2024-02-20 10:33:55 -0600

    fasd

--- MetaMorpheus/Test/FdrTest.cs
+++ MetaMorpheus/Test/FdrTest.cs
@@ -694,6 +694,5 @@ namespace Test
             string topDownToString = "\t0\t1\t2\t3\t4\t5\t6\t8\t9\t10\t21\t22";
             Assert.AreEqual(topDownToString, pd.ToString("top-down"));
         }
-
     }
 }
