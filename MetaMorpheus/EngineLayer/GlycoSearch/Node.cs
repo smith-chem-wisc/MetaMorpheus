3f4ede358672b9c2e5a1d17edd8e9ad3b180d9d6
nbollis <nbollis@comcast.net>
2024-04-09 14:06:46 -0500

    removed unused node constructor

--- MetaMorpheus/EngineLayer/GlycoSearch/Node.cs
+++ MetaMorpheus/EngineLayer/GlycoSearch/Node.cs
@@ -4,15 +4,6 @@ namespace EngineLayer
 
     public class Node
     {
-        public Node(char v, Node l, Node r, Node m)
-        {
-            Value = v;
-            LeftChild = l;
-            RightChild = r;
-            MiddleChild = m;
-            Level = null;
-        }
-
         public Node(char v)
         {
             Value = v;
