65b1d458152a4c5c7a506b84a5504a88a6547a4f
nbollis <nbollis@comcast.net>
2024-05-21 15:25:55 -0500

    added additional tests for multiple custom fragmentation

--- MetaMorpheus/Test/CustomFragmentationTest.cs
+++ MetaMorpheus/Test/CustomFragmentationTest.cs
@@ -184,5 +184,214 @@ namespace Test
                 new XmlForTaskListEventArgs(new List<DbForTask> { new DbForTask(myDatabase, false) });
             Assert.That(xmlArgs.NewDatabases.Count == 1);
         }
+
+        [Test]
+        public static void CustomFragmentIonsManySearchTasksContainingDifferentIons()
+        {
+            // path and custom ion setup
+            string outputFolder = Path.Combine(TestContext.CurrentContext.TestDirectory, @"TestCustomFragmentations");
+            string myFile = Path.Combine(TestContext.CurrentContext.TestDirectory, @"TestData\SmallCalibratible_Yeast.mzML");
+            string myDatabase = Path.Combine(TestContext.CurrentContext.TestDirectory, @"TestData\smalldb.fasta");
+
+            var customIons1 = new List<ProductType> { ProductType.b, ProductType.c, ProductType.zDot };
+            var customIons2 = new List<ProductType> { ProductType.b, ProductType.c, ProductType.zDot, ProductType.y };
+            var customIons3 = new List<ProductType> { ProductType.b, ProductType.zDot, ProductType.x };
+
+            // Task setup
+            DissociationTypeCollection.ProductsFromDissociationType[DissociationType.Custom] = customIons1;
+            var calibCommonParams = new CommonParameters(
+                               dissociationType: DissociationType.Custom,
+                                              productMassTolerance: new PpmTolerance(25),
+                                              precursorMassTolerance: new PpmTolerance(15),
+                                              trimMsMsPeaks: false,
+                                              doPrecursorDeconvolution: false);
+            CalibrationTask calibrationTask = new CalibrationTask() { CommonParameters = calibCommonParams };
+
+            DissociationTypeCollection.ProductsFromDissociationType[DissociationType.Custom] = customIons2;
+            var gptmdCommonParams = new CommonParameters(dissociationType: DissociationType.Custom);
+            GptmdTask gptmdTask = new GptmdTask() { CommonParameters = gptmdCommonParams };
+
+            DissociationTypeCollection.ProductsFromDissociationType[DissociationType.Custom] = customIons1;
+            var searchTask1CommonParams = new CommonParameters(dissociationType: DissociationType.Custom);
+            SearchTask searchTask1 = new SearchTask() { CommonParameters = searchTask1CommonParams };
+
+            DissociationTypeCollection.ProductsFromDissociationType[DissociationType.Custom] = customIons2;
+            var searchTask2CommonParams = new CommonParameters(dissociationType: DissociationType.Custom);
+            SearchTask searchTask2 = new SearchTask() { CommonParameters = searchTask2CommonParams };
+
+            DissociationTypeCollection.ProductsFromDissociationType[DissociationType.Custom] = customIons3;
+            var searchTask3CommonParams = new CommonParameters(dissociationType: DissociationType.Custom);
+            SearchTask searchTask3 = new SearchTask() { CommonParameters = searchTask3CommonParams };
+
+            var taskCollection = new List<(string, MetaMorpheusTask)>
+            { ("Calibration", calibrationTask), ("GPTMD", gptmdTask), ("Search1", searchTask1), ("Search2", searchTask2), ("Search3", searchTask3) };
+
+            // assert custom ions are correct in common parameters for each task
+            foreach (var task in taskCollection)
+            {
+                if (task.Item1 is "Calibration" or "Search1")
+                    CollectionAssert.AreEquivalent(customIons1, task.Item2.CommonParameters.CustomIons);
+                else if (task.Item1 is "GPTMD" or "Search2")
+                    CollectionAssert.AreEquivalent(customIons2, task.Item2.CommonParameters.CustomIons);
+                else if (task.Item1 is "Search3")
+                    CollectionAssert.AreEquivalent(customIons3, task.Item2.CommonParameters.CustomIons);
+
+                Assert.That(task.Item2.CommonParameters.DissociationType, Is.EqualTo(DissociationType.Custom));
+            }
+
+            // run all tasks
+            List<DbForTask> database = new List<DbForTask> { new DbForTask(myDatabase, false) };
+            var engine = new EverythingRunnerEngine(taskCollection, new List<string> { myFile },
+                               database, outputFolder);
+            engine.Run();
+
+            // assert all file specific parameters contain the correct custom ions
+            foreach (var task in taskCollection)
+            {
+                var fileSpecificCustomIons = task.Item2.FileSpecificParameters.First().Parameters.CustomIons;
+                if (task.Item1 is "Calibration" or "Search1")
+                    CollectionAssert.AreEqual(customIons1, fileSpecificCustomIons);
+                else if (task.Item1 is "GPTMD" or "Search2")
+                    CollectionAssert.AreEqual(customIons2, fileSpecificCustomIons);
+                else if (task.Item1 is "Search3")
+                    CollectionAssert.AreEqual(customIons3, fileSpecificCustomIons);
+            }
+
+            // read generated toml settings and make sure custom fragmentations match are handled properly for each task
+            var loadedCalibrationTask = Toml.ReadFile<CalibrationTask>(
+                Path.Combine(outputFolder, "Task Settings", $"{taskCollection[0].Item1}Config.toml"),
+                MetaMorpheusTask.tomlConfig);
+            CollectionAssert.AreEquivalent(customIons1, loadedCalibrationTask.CommonParameters.CustomIons);
+            Assert.That(loadedCalibrationTask.CommonParameters.DissociationType == DissociationType.Custom);
+
+            var loadedGptmdTask = Toml.ReadFile<GptmdTask>(
+                Path.Combine(outputFolder, "Task Settings", $"{taskCollection[1].Item1}Config.toml"),
+                MetaMorpheusTask.tomlConfig);
+            CollectionAssert.AreEquivalent(customIons2, loadedGptmdTask.CommonParameters.CustomIons);
+            Assert.That(loadedGptmdTask.CommonParameters.DissociationType == DissociationType.Custom);
+
+            var loadedSearchTask1 = Toml.ReadFile<SearchTask>(
+                Path.Combine(outputFolder, "Task Settings", $"{taskCollection[2].Item1}Config.toml"),
+                MetaMorpheusTask.tomlConfig);
+            CollectionAssert.AreEquivalent(customIons1, loadedSearchTask1.CommonParameters.CustomIons);
+            Assert.That(loadedSearchTask1.CommonParameters.DissociationType == DissociationType.Custom);
+
+            var loadedSearchTask2 = Toml.ReadFile<SearchTask>(
+                Path.Combine(outputFolder, "Task Settings", $"{taskCollection[3].Item1}Config.toml"),
+                MetaMorpheusTask.tomlConfig);
+            CollectionAssert.AreEquivalent(customIons2, loadedSearchTask2.CommonParameters.CustomIons);
+            Assert.That(loadedSearchTask2.CommonParameters.DissociationType == DissociationType.Custom);
+
+            var loadedSearchTask3 = Toml.ReadFile<SearchTask>(
+                Path.Combine(outputFolder, "Task Settings", $"{taskCollection[4].Item1}Config.toml"),
+                MetaMorpheusTask.tomlConfig);
+            CollectionAssert.AreEquivalent(customIons3, loadedSearchTask3.CommonParameters.CustomIons);
+            Assert.That(loadedSearchTask3.CommonParameters.DissociationType == DissociationType.Custom);
+
+            // read gptmd and search results to ensure matched ions are correct
+            var gptmdResults = PsmTsvReader.ReadTsv(Path.Combine(outputFolder, "GPTMD", "GPTMD_Candidates.psmtsv"), out List<string> warnings);
+            Assert.That(!warnings.Any());
+            var productIons = gptmdResults.SelectMany(p => p.MatchedIons.Select(m => m.NeutralTheoreticalProduct.ProductType))
+                .Distinct();
+            CollectionAssert.AreEquivalent(customIons2, productIons);
+
+            var searchResults1 = PsmTsvReader.ReadTsv(Path.Combine(outputFolder, "Search1", "AllPSMs.psmtsv"), out warnings);
+            Assert.That(!warnings.Any());
+            productIons = searchResults1.SelectMany(p => p.MatchedIons.Select(m => m.NeutralTheoreticalProduct.ProductType))
+                .Distinct();
+            CollectionAssert.AreEquivalent(customIons1, productIons);
+
+            var searchResults2 =
+                PsmTsvReader.ReadTsv(Path.Combine(outputFolder, "Search2", "AllPSMs.psmtsv"), out warnings);
+            Assert.That(!warnings.Any());
+            productIons = searchResults2
+                .SelectMany(p => p.MatchedIons.Select(m => m.NeutralTheoreticalProduct.ProductType))
+                .Distinct();
+            CollectionAssert.AreEquivalent(customIons2, productIons);
+
+            var searchResults3 =
+                PsmTsvReader.ReadTsv(Path.Combine(outputFolder, "Search3", "AllPSMs.psmtsv"), out warnings);
+            Assert.That(!warnings.Any());
+            productIons = searchResults3
+                .SelectMany(p => p.MatchedIons.Select(m => m.NeutralTheoreticalProduct.ProductType))
+                .Distinct();
+            CollectionAssert.AreEquivalent(customIons3, productIons);
+
+            // reload gptmd and search tasks with just calibrated files
+            var newOutputFolder = Path.Combine(outputFolder, "ReSearch");
+            var calibratedSpectraPath = Path.Combine(outputFolder, "Calibration", "SmallCalibratible_Yeast-calib.mzML");
+            engine = new EverythingRunnerEngine(
+                new List<(string, MetaMorpheusTask)>
+                {
+                    ("GPTMD", loadedGptmdTask), ("Search1", loadedSearchTask1), ("Search2", loadedSearchTask2),
+                    ("Search3", loadedSearchTask3)
+                },
+                new List<string> { calibratedSpectraPath },
+                new List<DbForTask> { new DbForTask(myDatabase, false) }, newOutputFolder);
+            engine.Run();
+
+            // read generated toml settings and make sure custom fragmentations match are handled properly for each task
+            loadedGptmdTask = Toml.ReadFile<GptmdTask>(
+                Path.Combine(newOutputFolder, "Task Settings", $"{taskCollection[1].Item1}Config.toml"),
+                MetaMorpheusTask.tomlConfig);
+            CollectionAssert.AreEquivalent(customIons2, loadedGptmdTask.CommonParameters.CustomIons);
+            Assert.That(loadedGptmdTask.CommonParameters.DissociationType == DissociationType.Custom);
+
+            loadedSearchTask1 = Toml.ReadFile<SearchTask>(
+                Path.Combine(newOutputFolder, "Task Settings", $"{taskCollection[2].Item1}Config.toml"),
+                MetaMorpheusTask.tomlConfig);
+            CollectionAssert.AreEquivalent(customIons1, loadedSearchTask1.CommonParameters.CustomIons);
+            Assert.That(loadedSearchTask1.CommonParameters.DissociationType == DissociationType.Custom);
+
+            loadedSearchTask2 = Toml.ReadFile<SearchTask>(
+                Path.Combine(newOutputFolder, "Task Settings", $"{taskCollection[3].Item1}Config.toml"),
+                MetaMorpheusTask.tomlConfig);
+            CollectionAssert.AreEquivalent(customIons2, loadedSearchTask2.CommonParameters.CustomIons);
+            Assert.That(loadedSearchTask2.CommonParameters.DissociationType == DissociationType.Custom);
+
+            loadedSearchTask3 = Toml.ReadFile<SearchTask>(
+                Path.Combine(newOutputFolder, "Task Settings", $"{taskCollection[4].Item1}Config.toml"),
+                MetaMorpheusTask.tomlConfig);
+            CollectionAssert.AreEquivalent(customIons3, loadedSearchTask3.CommonParameters.CustomIons);
+            Assert.That(loadedSearchTask3.CommonParameters.DissociationType == DissociationType.Custom);
+
+            // read gptmd and search results to ensure matched ions are correct
+            gptmdResults = PsmTsvReader.ReadTsv(Path.Combine(newOutputFolder, "GPTMD", "GPTMD_Candidates.psmtsv"),
+                out warnings);
+            Assert.That(!warnings.Any());
+            productIons = gptmdResults
+                .SelectMany(p => p.MatchedIons.Select(m => m.NeutralTheoreticalProduct.ProductType))
+                .Distinct();
+            CollectionAssert.AreEquivalent(customIons2, productIons);
+
+            searchResults1 =
+                PsmTsvReader.ReadTsv(Path.Combine(newOutputFolder, "Search1", "AllPSMs.psmtsv"), out warnings);
+            Assert.That(!warnings.Any());
+            productIons = searchResults1
+                .SelectMany(p => p.MatchedIons.Select(m => m.NeutralTheoreticalProduct.ProductType))
+                .Distinct();
+            CollectionAssert.AreEquivalent(customIons1, productIons);
+
+            searchResults2 =
+                PsmTsvReader.ReadTsv(Path.Combine(newOutputFolder, "Search2", "AllPSMs.psmtsv"), out warnings);
+            Assert.That(!warnings.Any());
+            productIons = searchResults2
+                .SelectMany(p => p.MatchedIons.Select(m => m.NeutralTheoreticalProduct.ProductType))
+                .Distinct();
+            CollectionAssert.AreEquivalent(customIons2, productIons);
+
+            searchResults3 =
+                PsmTsvReader.ReadTsv(Path.Combine(newOutputFolder, "Search3", "AllPSMs.psmtsv"), out warnings);
+            Assert.That(!warnings.Any());
+            productIons = searchResults3
+                .SelectMany(p => p.MatchedIons.Select(m => m.NeutralTheoreticalProduct.ProductType))
+                .Distinct();
+            CollectionAssert.AreEquivalent(customIons3, productIons);
+
+            // clean up
+            Directory.Delete(outputFolder, true);
+
+
+        }
     }
 }
