59db49af62f0640149dd833ce10ebb1b35c804d1
trishorts <mshort@chem.wisc.edu>
2024-07-24 11:43:42 -0500

    dsg

--- MetaMorpheus/TaskLayer/SearchTask/PostSearchAnalysisTask.cs
+++ MetaMorpheus/TaskLayer/SearchTask/PostSearchAnalysisTask.cs
@@ -845,6 +845,15 @@ namespace TaskLayer
             string writtenFile = Path.Combine(Parameters.OutputFolder, fileName);
             WriteProteinGroupsToTsv(ProteinGroups, writtenFile, new List<string> { Parameters.SearchTaskId });
 
+            if (Parameters.CurrentRawFileList.Count > 1 && (Parameters.SearchParameters.WriteIndividualFiles
+                                                            || Parameters.SearchParameters.WriteMzId ||
+                                                            Parameters.SearchParameters.WritePepXml))
+            {
+
+                Directory.CreateDirectory(Parameters.IndividualResultsOutputFolder);
+            }
+
+
             var psmsGroupedByFile = Filter(Parameters.AllPsms,
                 includeDecoys: true,
                 includeContaminants: true,
@@ -886,11 +895,6 @@ namespace TaskLayer
             // write all individual file results to subdirectory
             // local protein fdr, global parsimony, global psm fdr
 
-            if (Parameters.CurrentRawFileList.Count > 1 && (Parameters.SearchParameters.WriteIndividualFiles
-                                                           || Parameters.SearchParameters.WriteMzId || Parameters.SearchParameters.WritePepXml))
-            {
-
-                Directory.CreateDirectory(Parameters.IndividualResultsOutputFolder);
 
                 //write the individual result files for each datafile
                 foreach (var fullFilePath in psmsGroupedByFile.Select(v => v.Key))
@@ -972,7 +976,7 @@ namespace TaskLayer
 
                     ReportProgress(new ProgressEventArgs(100, "Done!", new List<string> { Parameters.SearchTaskId, "Individual Spectra Files", fullFilePath }));
                 }
-            }
+            
         }
 
         private void WriteFlashLFQResults()
