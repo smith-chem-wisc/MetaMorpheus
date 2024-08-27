ed9f1c0f8ca467d3a99114efa82ce44060053e49
Alex <AlexSolivais@gmail.com>
2024-08-27 11:06:02 -0500

    Deleted B

--- MetaMorpheus/Test/MyTaskTest.cs
+++ MetaMorpheus/Test/MyTaskTest.cs
@@ -509,7 +509,7 @@ namespace Test
             Directory.Delete(thisTaskOutputFolder, true);
         }
 
-        /// <summary>B
+        /// <summary>
         /// This tests for a bug in annotating mods in the search task. The situation is that if you search with a fasta database (no mods annotated),
         /// and then do GPTMD, then search with the GPTMD database, the resulting PSM will have a UniProt mod annotated on it.
         /// Also, if GPTMD has a mod with the same name as a UniProt mod, the annotated PSM will be ambiguous between
