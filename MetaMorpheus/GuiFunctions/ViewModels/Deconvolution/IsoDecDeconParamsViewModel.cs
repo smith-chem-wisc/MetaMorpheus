6ea130af7d315a842f9ef2850574f3d41874a27c
nbollis <nbollis@comcast.net>
2024-10-30 19:42:14 -0500

    Built IsoDecParamsViewModel

--- MetaMorpheus/GuiFunctions/ViewModels/Deconvolution/IsoDecDeconParamsViewModel.cs
+++ MetaMorpheus/GuiFunctions/ViewModels/Deconvolution/IsoDecDeconParamsViewModel.cs
@@ -1,20 +1,146 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Diagnostics.CodeAnalysis;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using MassSpectrometry;
+﻿using MassSpectrometry;
 
-namespace GuiFunctions
+namespace GuiFunctions;
+
+public sealed class IsoDecDeconParamsViewModel : DeconParamsViewModel
 {
-    // Coming Soon
-    [ExcludeFromCodeCoverage]
-    public sealed class IsoDecDeconParamsViewModel : DeconParamsViewModel
+    private IsoDecDeconvolutionParameters _parameters;
+    public override DeconvolutionParameters Parameters
     {
-        public override DeconvolutionParameters Parameters { get; protected set; }
-
-
-        public override string ToString() => "IsoDec";
+        get => _parameters;
+        protected set
+        {
+            _parameters = (IsoDecDeconvolutionParameters)value;
+            OnPropertyChanged(nameof(Parameters));
+        }
     }
+
+    public int PhaseRes
+    {
+        get => _parameters.PhaseRes;
+        set
+        {
+            if (value != 4 && value != 8)
+                return;
+
+            _parameters.PhaseRes = value;
+            OnPropertyChanged(nameof(PhaseRes));
+        }
+    }
+
+    public float CssThreshold
+    {
+        get => _parameters.CssThreshold;
+        set
+        {
+            if (value is > 1 or < 0)
+                return;
+
+            _parameters.CssThreshold = value;
+            OnPropertyChanged(nameof(CssThreshold));
+        }
+    }
+
+    public float MatchTolerance
+    {
+        get => _parameters.MatchTolerance;
+        set
+        {
+            if (value <= 0)
+                return;
+
+            _parameters.MatchTolerance = value;
+            OnPropertyChanged(nameof(MatchTolerance));
+        }
+    }
+
+    public int MaximumShift
+    {
+        get => _parameters.MaxShift;
+        set
+        {
+            if (value < 0)
+                return;
+
+            _parameters.MaxShift = value;
+            OnPropertyChanged(nameof(MaximumShift));
+        }
+    }
+
+    public float MzWindowForIsotopeDistributionMinimum
+    {
+        get => _parameters.MzWindow[0];
+        set
+        {
+            if (value > 0)
+                return;
+
+            _parameters.MzWindow[0] = value;
+            OnPropertyChanged(nameof(MzWindowForIsotopeDistributionMinimum));
+        }
+    } 
+
+    public float MzWindowForIsotopeDistributionMaximum
+    {
+        get => _parameters.MzWindow[1];
+        set
+        {
+            if (value < 0)
+                return;
+
+            _parameters.MzWindow[1] = value;
+            OnPropertyChanged(nameof(MzWindowForIsotopeDistributionMaximum));
+        }
+    }
+
+    public int KnockdownRounds
+    {
+        get => _parameters.KnockdownRounds;
+        set
+        {
+            if (value <0)
+                return;
+
+            _parameters.KnockdownRounds = value;
+            OnPropertyChanged(nameof(KnockdownRounds));
+        }
+    }
+
+    public float MinAreaCovered
+    {
+        get => _parameters.MinAreaCovered;
+        set
+        {
+            if (value is > 1 or < 0)
+                return;
+
+            _parameters.MinAreaCovered = value;
+            OnPropertyChanged(nameof(MinAreaCovered));
+        }
+    }
+
+    public float DataThreshold
+    {
+        get => _parameters.DataThreshold;
+        set
+        {
+            if (value is > 1 or < 0)
+                return;
+
+            _parameters.DataThreshold = value;
+            OnPropertyChanged(nameof(DataThreshold));
+        }
+    }
+
+    public bool ReportMultipleMonoisotopicMasses
+    {
+        get => _parameters.ReportMulitpleMonoisos;
+        set
+        {
+            _parameters.ReportMulitpleMonoisos = value;
+            OnPropertyChanged(nameof(ReportMultipleMonoisotopicMasses));
+        }
+    }
+
+    public override string ToString() => "IsoDec";
 }
