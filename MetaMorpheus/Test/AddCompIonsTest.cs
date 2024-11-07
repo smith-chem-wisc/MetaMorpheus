07a009032bbebd2d962cd1c8a8533145d5a44531
trishorts <mshort@chem.wisc.edu>
2024-11-07 10:30:02 -0600

    remove nunit legacy from Ambiguity Test

--- MetaMorpheus/Test/AddCompIonsTest.cs
+++ MetaMorpheus/Test/AddCompIonsTest.cs
@@ -9,7 +9,6 @@ using NUnit.Framework;
 using Proteomics;
 using Omics.Fragmentation;
 using Proteomics.ProteolyticDigestion;
-using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
