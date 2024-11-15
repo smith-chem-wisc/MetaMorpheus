4e0bb6822b5444d7f7abab9fb725e87d18e291bd
trishorts <mshort@chem.wisc.edu>
2024-11-15 09:47:38 -0600

    mock style unit test

--- MetaMorpheus/TaskLayer/DbForTask.cs
+++ MetaMorpheus/TaskLayer/DbForTask.cs
@@ -2,6 +2,13 @@
 
 namespace TaskLayer
 {
+    public interface IdbForTask
+    {
+        string FilePath { get; }
+        bool IsContaminant { get; }
+        string FileName { get; }
+        bool IsSpectralLibrary { get; }
+    }
     public class DbForTask
     {
         public DbForTask(string filePath, bool isContaminant)
@@ -11,10 +18,16 @@ namespace TaskLayer
             FileName = System.IO.Path.GetFileName(filePath);
             IsSpectralLibrary = GlobalVariables.GetFileExtension(filePath).ToLowerInvariant() == ".msp";
         }
+        public DbForTask(IdbForTask dbForTask)
+        {
+            this.dbForTask = dbForTask;
+        }
 
         public bool IsSpectralLibrary { get; }
         public string FilePath { get; }
         public bool IsContaminant { get; }
         public string FileName { get; }
+
+        private readonly IdbForTask dbForTask;
     }
 }
---
+++ MetaMorpheus/Test/DbForTaskTests.cs
@@ -0,0 +1,23 @@
+﻿using NUnit.Framework;
+using TaskLayer;
+using Moq;
+
+namespace Test
+{
+    [TestFixture]
+    internal class DbForTaskTests
+    {
+
+        [Test]
+        public void MockDbForTask_IsSpectralLibrary_ReturnsTrue()
+        {
+            var dbMock = new Mock<IdbForTask>();
+            dbMock.Setup(x => x.FilePath).Returns("path");
+            dbMock.Setup(x => x.IsContaminant).Returns(false);
+            dbMock.Setup(x => x.FileName).Returns("name");
+            dbMock.Setup(x => x.IsSpectralLibrary).Returns(true);
+
+            Assert.That(dbMock.Object.IsSpectralLibrary, Is.True);
+        }
+    }
+}
