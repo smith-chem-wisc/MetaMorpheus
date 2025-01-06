734c0144f256b46c25ba7c6310f6ea285fbda014
Alex <AlexSolivais@gmail.com>
2025-01-06 17:22:52 -0600

    idk

---
+++ MetaMorpheus/Test/ReproducibilityTests.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Diagnostics.CodeAnalysis;
+using System.IO;
+using System.Linq;
+using NUnit.Framework;
+
+namespace Test
+{
+    /// <summary>
+    /// Uses test cases found in EverythingRunnerEngineTestCase.cs
+    /// </summary>
+    [TestFixture]
+    [ExcludeFromCodeCoverage]
+    public static class ReproducibilityTests
+    {
+        public static Array GetTestCases() => Enum.GetValues(typeof(EverythingRunnerEngineTestCases));
+
+
+
+    }
+}
