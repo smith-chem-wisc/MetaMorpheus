fcb799fefad26ce6c815c05dc442aea8d3c4d1fe
Nic Bollis <nbollis@comcast.net>
2025-01-13 15:12:56 -0600

    Deconvolution GUI Modularity (#2429)
    * Updated to MzLib 1.0.548 and fixed custom ions in search tasks
    
    * reverted calibration task change
    
    * merged in master bbbyy
    
    * Spectral Library from Command Line (#2386)
    
    * Updated to MzLib 1.0.548 and fixed custom ions in search tasks
    
    * reverted calibration task change
    
    * merged in master bbbyy
    
    * Enabled Library Loading from command line
    
    * Build ClassicDeconv Params View Model
    
    * Built classic deconvolution control and the Deconvolution Host Control
    
    * Refactor deconvolution param management with ViewModel
    
    Streamlined deconvolution parameter management by introducing `DeconHostViewModel` and custom control `HostDeconParamControl`. Updated `CommonParameters.cs` to change default `ProductDeconvolutionParameters` in negative mode from `-10` to `-20`. Modified `SearchTaskWindow.xaml` and `SearchTaskWindow.xaml.cs` to use the new view model, improving separation of concerns and maintainability. Updated `SaveButton_Click` and other relevant methods to retrieve parameters via the view model.
    
    * Completed the first phase of the new structure. Parameters can now be set from the GUI with the new code structure
    
    * Refactor UI and add comprehensive unit tests
    
    Refactored several XAML files to improve UI structure and readability:
    - Removed unnecessary `GroupBox` in `SearchTaskWindow.xaml` and added a new one for "Peak Trimming".
    - Adjusted layout in `ClassicDeconParamsControl.xaml` and `HostDeconParamControl.xaml`.
    
    Enhanced ViewModel logic:
    - Updated `DeconHostViewModel.cs` and `DeconParamsViewModel.cs` to include additional properties and validation logic.
    - Added `[ExcludeFromCodeCoverage]` attribute to `IsoDecDeconParamsViewModel.cs`.
    
    Expanded unit tests:
    - Removed obsolete `GuiFunctionsTest.cs`.
    - Added new test classes for `ClassicDeconParamsViewModel`, `DeconParamsViewModel`, `DeconHostViewModel`, and `MzLibExtensions`.
    - Included tests for property changes, validation logic, and conversion methods.
    
    * Expanded test coverage and cleaned up code
    
    * added control to GPTMD window
    
    * Expanded test coverage
    
    * Added control to the glyco and xl task windows
    
    ---------
    
    Co-authored-by: Nic Bollis <nbollis@wisc.edu>

---
+++ MetaMorpheus/GuiFunctions/MzLibExtensions.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using MassSpectrometry;
+
+namespace GuiFunctions
+{
+    /// <summary>
+    /// Provides additional functionality for the MzLib library.
+    /// </summary>
+    public static class MzLibExtensions
+    {
+
+        /// <summary>
+        /// Converts the given <see cref="DeconvolutionParameters"/> to a <see cref="DeconParamsViewModel"/>.
+        /// </summary>
+        /// <param name="parameters">The deconvolution parameters to convert.</param>
+        /// <returns>A <see cref="DeconParamsViewModel"/> representing the given parameters.</returns>
+        /// <exception cref="NotImplementedException">
+        /// Thrown when the type of <paramref name="parameters"/> is not supported.
+        /// </exception>
+        public static DeconParamsViewModel ToViewModel(this DeconvolutionParameters parameters)
+        {
+            if (parameters is ClassicDeconvolutionParameters classicParams)
+            {
+                return new ClassicDeconParamsViewModel(classicParams);
+            }
+            //else if (parameters is IsoDeconvolutionParameters isoParams)
+            //{
+            //    return new IsoDecDeconParamsViewModel(isoParams);
+            //}
+            else
+            {
+                throw new NotImplementedException();
+            }
+        }
+    }
+}
---
+++ MetaMorpheus/Test/GuiTests/DeconHostViewModelTest.cs
@@ -0,0 +1,224 @@
+﻿using System;
+using System.Linq;
+using EngineLayer;
+using GuiFunctions;
+using MassSpectrometry;
+using NUnit.Framework;
+
+namespace Test.GuiTests;
+
+[TestFixture]
+public class DeconHostViewModelTests
+{
+    internal ClassicDeconvolutionParameters ClassicPrecursorDeconvolutionParameters = new ClassicDeconvolutionParameters(1, 12, 4, 3);
+    internal ClassicDeconvolutionParameters ClassicProductDeconvolutionParameters = new ClassicDeconvolutionParameters(1, 10, 4, 3);
+
+    [Test]
+    public void Constructor_DefaultParameters_ShouldInitializeCorrectly()
+    {
+        // Arrange
+        var initialPrecursorParameters = ClassicPrecursorDeconvolutionParameters;
+        var initialProductParameters = ClassicProductDeconvolutionParameters;
+
+        // Act
+        var viewModel = new DeconHostViewModel(initialPrecursorParameters, initialProductParameters);
+
+        // Assert
+        Assert.That(viewModel.UseProvidedPrecursors, Is.False);
+        Assert.That(viewModel.DoPrecursorDeconvolution, Is.True);
+        Assert.That(viewModel.PrecursorDeconvolutionParametersList, Is.Not.Null);
+        Assert.That(viewModel.ProductDeconvolutionParametersList, Is.Not.Null);
+        Assert.That(viewModel.PrecursorDeconvolutionParametersList.Any(), Is.True);
+        Assert.That(viewModel.ProductDeconvolutionParametersList.Any(), Is.True);
+    }
+
+    [Test]
+    public void Constructor_WithProvidedParameters_ShouldSetCorrectly()
+    {
+        // Arrange
+        var initialPrecursorParameters = ClassicPrecursorDeconvolutionParameters;
+        var initialProductParameters = ClassicProductDeconvolutionParameters;
+
+        // Act
+        var viewModel = new DeconHostViewModel(initialPrecursorParameters, initialProductParameters, true, false);
+
+        // Assert
+        Assert.That(viewModel.UseProvidedPrecursors, Is.True);
+        Assert.That(viewModel.DoPrecursorDeconvolution, Is.False);
+        Assert.That(viewModel.PrecursorDeconvolutionParameters, Is.EqualTo(initialPrecursorParameters.ToViewModel()));
+        Assert.That(viewModel.ProductDeconvolutionParameters, Is.EqualTo(initialProductParameters.ToViewModel()));
+    }
+
+    [Test]
+    public void TestDeconHostViewModel_DefaultParameters()
+    {
+        // Arrange
+        DeconHostViewModel viewModel = new DeconHostViewModel(null, null);
+
+        // Act
+        var precursorParams = viewModel.PrecursorDeconvolutionParameters;
+        var productParams = viewModel.ProductDeconvolutionParameters;
+
+        // Assert
+        Assert.That(precursorParams, Is.Not.Null);
+        Assert.That(productParams, Is.Not.Null);
+        Assert.That(precursorParams.DeconvolutionType, Is.EqualTo(DeconvolutionType.ClassicDeconvolution));
+        Assert.That(productParams.DeconvolutionType, Is.EqualTo(DeconvolutionType.ClassicDeconvolution));
+    }
+
+    [Test]
+    public void PrecursorDeconvolutionParameters_Setter_ShouldTriggerPropertyChanged()
+    {
+        // Arrange
+        var viewModel = new DeconHostViewModel();
+        var newParameters = new ClassicDeconParamsViewModel(ClassicPrecursorDeconvolutionParameters);
+        bool propertyChangedTriggered = false;
+        viewModel.PropertyChanged += (sender, args) =>
+        {
+            if (args.PropertyName == nameof(viewModel.PrecursorDeconvolutionParameters))
+                propertyChangedTriggered = true;
+        };
+
+        // Act
+        viewModel.PrecursorDeconvolutionParameters = newParameters;
+
+        // Assert
+        Assert.That(propertyChangedTriggered, Is.True);
+    }
+
+    [Test]
+    public void ProductDeconvolutionParameters_Setter_ShouldTriggerPropertyChanged()
+    {
+        // Arrange
+        var viewModel = new DeconHostViewModel();
+        var newParameters = new ClassicDeconParamsViewModel(ClassicProductDeconvolutionParameters);
+        bool propertyChangedTriggered = false;
+        viewModel.PropertyChanged += (sender, args) =>
+        {
+            if (args.PropertyName == nameof(viewModel.ProductDeconvolutionParameters))
+                propertyChangedTriggered = true;
+        };
+
+        // Act
+        viewModel.ProductDeconvolutionParameters = newParameters;
+
+        // Assert
+        Assert.That(propertyChangedTriggered, Is.True);
+    }
+
+        
+    [Test]
+    [NonParallelizable]
+    public void TestDeconHostViewModel_GlobalVariables_Proteoform()
+    {
+        // Arrange
+        GlobalVariables.AnalyteType = AnalyteType.Proteoform;
+        DeconHostViewModel viewModel = new DeconHostViewModel(null, null);
+
+        // Act
+        var precursorParams = viewModel.PrecursorDeconvolutionParameters;
+        var productParams = viewModel.ProductDeconvolutionParameters;
+
+        // Assert
+        Assert.That(precursorParams, Is.Not.Null);
+        Assert.That(productParams, Is.Not.Null);
+        Assert.That(precursorParams.DeconvolutionType, Is.EqualTo(DeconvolutionType.ClassicDeconvolution));
+        Assert.That(productParams.DeconvolutionType, Is.EqualTo(DeconvolutionType.ClassicDeconvolution));
+        Assert.That(precursorParams.Parameters, Is.InstanceOf<ClassicDeconvolutionParameters>());
+        Assert.That(productParams.Parameters, Is.InstanceOf<ClassicDeconvolutionParameters>());
+        Assert.That(((ClassicDeconvolutionParameters)precursorParams.Parameters).MaxAssumedChargeState, Is.EqualTo(60));
+        Assert.That(((ClassicDeconvolutionParameters)productParams.Parameters).MaxAssumedChargeState, Is.EqualTo(10));
+
+        // Revert back to default
+        GlobalVariables.AnalyteType = AnalyteType.Peptide;
+    }
+
+    [Test]
+    [NonParallelizable]
+    public void TestDeconHostViewModel_GlobalVariables_Oligo()
+    {
+        // Arrange
+        GlobalVariables.AnalyteType = AnalyteType.Oligo;
+        DeconHostViewModel viewModel = new DeconHostViewModel(null, null);
+
+        // Act
+        var precursorParams = viewModel.PrecursorDeconvolutionParameters;
+        var productParams = viewModel.ProductDeconvolutionParameters;
+
+        // Assert
+        Assert.That(precursorParams, Is.Not.Null);
+        Assert.That(productParams, Is.Not.Null);
+        Assert.That(precursorParams.DeconvolutionType, Is.EqualTo(DeconvolutionType.ClassicDeconvolution));
+        Assert.That(productParams.DeconvolutionType, Is.EqualTo(DeconvolutionType.ClassicDeconvolution));
+        Assert.That(precursorParams.Parameters, Is.InstanceOf<ClassicDeconvolutionParameters>());
+        Assert.That(productParams.Parameters, Is.InstanceOf<ClassicDeconvolutionParameters>());
+        Assert.That(((ClassicDeconvolutionParameters)precursorParams.Parameters).MinAssumedChargeState, Is.EqualTo(-20));
+        Assert.That(((ClassicDeconvolutionParameters)productParams.Parameters).MinAssumedChargeState, Is.EqualTo(-10));
+
+        // Revert back to default
+        GlobalVariables.AnalyteType = AnalyteType.Peptide;
+    }
+
+    [Test]
+    [NonParallelizable]
+    public void TestDeconHostViewModel_GlobalVariables_Unknown()
+    {
+        // Arrange
+        GlobalVariables.AnalyteType = (AnalyteType)(-1);
+
+        // Act & Assert
+        Assert.Throws<ArgumentOutOfRangeException>(() =>
+        {
+            var deconHostViewModel = new DeconHostViewModel(null, null);
+        });
+
+        Assert.Throws<ArgumentOutOfRangeException>(() =>
+        {
+            var deconHostViewModel = new DeconHostViewModel(ClassicPrecursorDeconvolutionParameters, null);
+        });
+
+        // Revert back to default
+        GlobalVariables.AnalyteType = AnalyteType.Peptide;
+    }
+
+    [Test]
+    public void TestDisplayDeconSelectionComboBox_MultipleOptions_Precursor()
+    {
+        // Arrange
+        var viewModel = new DeconHostViewModel();
+        viewModel.PrecursorDeconvolutionParametersList.Add(ClassicPrecursorDeconvolutionParameters.ToViewModel());
+        
+        // Act
+        var result = viewModel.DisplayDeconSelectionComboBox;
+
+        // Assert
+        Assert.That(result, Is.True);
+    }
+
+    [Test]
+    public void TestDisplayDeconSelectionComboBox_MultipleOptions_Product()
+    {
+        // Arrange
+        var viewModel = new DeconHostViewModel();
+        viewModel.ProductDeconvolutionParametersList.Add(ClassicProductDeconvolutionParameters.ToViewModel());
+
+        // Act
+        var result = viewModel.DisplayDeconSelectionComboBox;
+
+        // Assert
+        Assert.That(result, Is.True);
+    }
+
+    [Test]
+    public void TestDisplayDeconSelectionComboBox_SingleOption()
+    {
+        // Arrange
+        var viewModel = new DeconHostViewModel();
+       
+        // Act
+        var result = viewModel.DisplayDeconSelectionComboBox;
+
+        // Assert
+        Assert.That(result, Is.False);
+    }
+}
