f10074659d6ab8d41844c0cabb948ed4ecfee1a6
nbollis <nbollis@comcast.net>
2025-03-05 13:10:01 -0600

    Add Comments

--- MetaMorpheus/GUI/Util/GuiGlobalParams.cs
+++ MetaMorpheus/GUI/Util/GuiGlobalParams.cs
@@ -1,9 +1,15 @@
 ﻿namespace MetaMorpheusGUI
 {
+    /// <summary>
+    /// Gui Parameters that are written out to a toml file. 
+    /// This gets parsed in and an exception is thrown, the file is deleted and a new default file is written out.
+    /// </summary>
     public class GuiGlobalParams
     {
         public bool AskAboutUpdating { get; internal set; } = true;
         public bool AskBeforeExitingMetaMorpheus { get; internal set; } = true;
+
+        // User can set a custom proteome directory. Be sure to use double slashes in the path, otherwise it will not be read in properly. 
         public string UserSpecifiedProteomeDir { get; internal set; } = "";
 
         //Ask about protease-specific parameter recommendations
