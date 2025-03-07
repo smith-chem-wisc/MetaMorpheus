a6e50b0b8ce12b4e3f72cc738f2999909d57a5ab
nbollis <nbollis@comcast.net>
2025-03-07 14:18:20 -0600

    Increase test coverage by add or create method

---
+++ MetaMorpheus/EngineLayer/Util/MzLibExtensions.cs
@@ -0,0 +1,15 @@
+﻿using Omics;
+using Proteomics.ProteolyticDigestion;
+
+namespace EngineLayer;
+
+public static class MzLibExtensions
+{
+    // Temporary method unit IBioPolymer has an IsVariant function. 
+    public static bool IsVariantPeptide(this IBioPolymerWithSetMods bpwsm)
+    {
+        if (bpwsm is PeptideWithSetModifications pwsm)
+            return pwsm.IsVariantPeptide();
+        return false;
+    }
+}
