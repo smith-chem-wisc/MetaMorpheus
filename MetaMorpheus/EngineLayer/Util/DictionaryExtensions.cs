af3249aac222d258e1ba104c2c9b89d73a1c8028
nbollis <nbollis@comcast.net>
2025-03-07 14:16:54 -0600

    AddOrCreate with Hashsets

--- MetaMorpheus/EngineLayer/Util/DictionaryExtensions.cs
+++ MetaMorpheus/EngineLayer/Util/DictionaryExtensions.cs
@@ -32,6 +32,27 @@ public static class DictionaryExtensions
     }
 
     /// <summary>
+    /// Adds a value to the HashSet associated with the specified key in the dictionary.
+    /// If the key does not exist, a new HashSet is created with the value and added to the dictionary.
+    /// </summary>
+    /// <typeparam name="TKey">The type of the keys in the dictionary.</typeparam>
+    /// <typeparam name="TValues">The type of the values in the hash set.</typeparam>
+    /// <param name="dictionary">The dictionary to operate on.</param>
+    /// <param name="key">The key whose value list to add to or create.</param>
+    /// <param name="value">The value to add to the list associated with the specified key.</param>
+    public static void AddOrCreate<TKey, TValues>(this IDictionary<TKey, HashSet<TValues>> dictionary, TKey key, TValues value)
+    {
+        if (dictionary.TryGetValue(key, out HashSet<TValues> values))
+        {
+            values.Add(value);
+        }
+        else
+        {
+            dictionary.Add(key, new HashSet<TValues> { value });
+        }
+    }
+
+    /// <summary>
     /// Adds a value to the list associated with the specified key in the dictionary.
     /// If the key does not exist, a new list is created with the value and added to the dictionary.
     /// </summary>
