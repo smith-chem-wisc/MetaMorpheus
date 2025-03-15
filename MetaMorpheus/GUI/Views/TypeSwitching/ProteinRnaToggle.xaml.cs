62c7df1fe4d8c781b19cc0fadb14746df7dfcc23
nbollis <nbollis@comcast.net>
2025-03-14 22:19:55 -0500

    Added switch to many tabs

--- MetaMorpheus/GUI/Views/TypeSwitching/ProteinRnaToggle.xaml.cs
+++ MetaMorpheus/GUI/Views/TypeSwitching/ProteinRnaToggle.xaml.cs
@@ -1,22 +1,6 @@
 ﻿using System.Windows.Controls;
-
 namespace MetaMorpheusGUI;
-
-/// <summary>
-/// Interaction logic for ProteinRnaToggle.xaml
-/// </summary>
 public partial class ProteinRnaToggle : UserControl
 {
-    public ProteinRnaToggle()
-    {
-        InitializeComponent();
-    }
-
-    private void ToggleCheckBox_Checked(object sender, System.Windows.RoutedEventArgs e)
-    {
-        var checkBox = (CheckBox)sender;
-        var checkState = checkBox.IsChecked;
-        UpdateGUISettings.Globals.IsRnaMode = checkState ?? true;
-        UpdateGUISettings.NotifyGlobalsChanged();
-    }
+    public ProteinRnaToggle() => InitializeComponent();
 }
