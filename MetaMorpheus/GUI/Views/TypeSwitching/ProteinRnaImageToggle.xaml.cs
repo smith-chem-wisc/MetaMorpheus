955aaa42387d1f74f3fd81f545f98c46e95b00b0
nbollis <nbollis@comcast.net>
2025-03-21 15:41:48 -0500

    Switched to image toggle

---
+++ MetaMorpheus/GUI/Views/TypeSwitching/ProteinRnaImageToggle.xaml.cs
@@ -0,0 +1,26 @@
+﻿using System.Windows.Controls;
+using System.Windows.Input;
+
+namespace MetaMorpheusGUI;
+
+/// <summary>
+/// Interaction logic for ProteinRnaImageToggle.xaml
+/// </summary>
+public partial class ProteinRnaImageToggle : UserControl
+{
+    public ProteinRnaImageToggle()
+    {
+        InitializeComponent();
+    }
+
+    private void Protein_Click(object sender, MouseButtonEventArgs e)
+    {
+        UpdateGUISettings.Globals.IsRnaMode = false;
+    }
+
+    private void Rna_Click(object sender, MouseButtonEventArgs e)
+    {
+        UpdateGUISettings.Globals.IsRnaMode = true;
+    }
+}
+
