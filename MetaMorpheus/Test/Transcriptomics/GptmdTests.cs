4620cd6c25e4bf7ebbb45efef210863b29633eb3
nbollis <nbollis@comcast.net>
2025-07-15 15:22:55 -0500

    First pass at testing task

--- MetaMorpheus/Test/Transcriptomics/GptmdTests.cs
+++ MetaMorpheus/Test/Transcriptomics/GptmdTests.cs
@@ -11,6 +11,9 @@ using System.Linq;
 using Chemistry;
 using Transcriptomics.Digestion;
 using Transcriptomics;
+using System.IO;
+using System.Xml.Linq;
+using TaskLayer;
 
 namespace Test.Transcriptomics;
 
@@ -188,5 +191,94 @@ public class GptmdTests
             Assert.That(gptmdResults.Mods.Count, Is.EqualTo(0));
         else
             Assert.That(gptmdResults.Mods["accession"].Count, Is.EqualTo(expectedGptmdMods));
+    }
+
+    [Test]
+    public void GptmdTask_Rna_AddsExpectedMethylationMods()
+    {
+        // Arrange
+        // Set up file paths
+        string testDir = TestContext.CurrentContext.TestDirectory;
+        string fastaPath = Path.Combine(testDir, "Transcriptomics", "TestData", "16mer2.fasta");
+        string xmlReferencePath = Path.Combine(testDir, "Transcriptomics", "TestData", "16mer2.xml");
+        string spectraPath = Path.Combine(testDir, "Transcriptomics", "TestData", "RnaStandard_Subset.mzML");
+        string outputDir = Path.Combine(testDir, "GptmdTask_Rna_AddsExpectedMethylationMods");
+        Directory.CreateDirectory(outputDir);
+
+        // Set up GptmdTask for RNA
+        var gptmdTask = new GptmdTask
+        {
+            CommonParameters = new CommonParameters(
+                digestionParams: new RnaDigestionParams("top-down"),
+                dissociationType: DissociationType.CID,
+                deconvolutionMaxAssumedChargeState: -20,
+                deconvolutionIntensityRatio: 3,
+                deconvolutionMassTolerance: new PpmTolerance(5),
+                precursorMassTolerance: new PpmTolerance(10),
+                productMassTolerance: new PpmTolerance(20),
+                scoreCutoff: 5,
+                totalPartitions: 1,
+                maxThreadsToUsePerFile: 1,
+                doPrecursorDeconvolution: true,
+                useProvidedPrecursorInfo: false
+            ),
+            GptmdParameters = new GptmdParameters
+            {
+                ListOfModsGptmd = GlobalVariables.AllRnaModsKnown
+                    .Where(m => m.IdWithMotif.Contains("Methylation"))
+                    .Select(m => (m.ModificationType, m.IdWithMotif)).ToList()
+            }
+        };
+
+        // Prepare DbForTask and run
+        var dbs = new List<DbForTask> { new DbForTask(fastaPath, false) };
+        var rawFiles = new List<string> { spectraPath };
+        var results = gptmdTask.RunTask(outputDir, dbs, rawFiles, "test");
+
+        // Find the output GPTMD XML file
+        string gptmdXml = Directory.GetFiles(outputDir, "*GPTMD.xml").FirstOrDefault();
+        Assert.That(gptmdXml, Is.Not.Null.And.Not.Empty, "GPTMD output XML should exist.");
+
+        // Parse the output XML and the reference XML
+        var outputDoc = XDocument.Load(gptmdXml);
+        var refDoc = XDocument.Load(xmlReferencePath);
+
+        // Get the entry node
+        var outputEntry = outputDoc.Descendants("entry").FirstOrDefault();
+        var refEntry = refDoc.Descendants("entry").FirstOrDefault();
+        Assert.That(outputEntry, Is.Not.Null, "Output XML should have an entry.");
+        Assert.That(refEntry, Is.Not.Null, "Reference XML should have an entry.");
+
+        // Check accession and sequence
+        string outAcc = outputEntry.Element("accession")?.Value;
+        string refAcc = refEntry.Element("accession")?.Value;
+        Assert.That(outAcc, Is.EqualTo(refAcc));
+        string outSeq = outputEntry.Element("sequence")?.Value;
+        string refSeq = refEntry.Element("sequence")?.Value;
+        Assert.That(outSeq, Is.EqualTo(refSeq));
+
+        // Check mod positions and types
+        var outMods = outputEntry.Elements("feature")
+            .Where(f => (string)f.Attribute("type") == "modified residue")
+            .Select(f => new
+            {
+                Description = (string)f.Attribute("description"),
+                Position = int.Parse(f.Element("location").Element("position").Attribute("position").Value)
+            }).OrderBy(f => f.Position).ToList();
+
+        var refMods = refEntry.Elements("feature")
+            .Where(f => (string)f.Attribute("type") == "modified residue")
+            .Select(f => new
+            {
+                Description = (string)f.Attribute("description"),
+                Position = int.Parse(f.Element("location").Element("position").Attribute("position").Value)
+            }).OrderBy(f => f.Position).ToList();
+
+        Assert.That(outMods.Count, Is.EqualTo(refMods.Count), "Number of mods should match reference.");
+        for (int i = 0; i < refMods.Count; i++)
+        {
+            Assert.That(outMods[i].Position, Is.EqualTo(refMods[i].Position), $"Mod position {i} should match.");
+            Assert.That(outMods[i].Description, Is.EqualTo(refMods[i].Description), $"Mod description {i} should match.");
+        }
     }
 }
