b891440769081c6732bc8845b9659827d067c527
RayMSMS <150720362+RayMSMS@users.noreply.github.com>
2025-07-16 11:40:12 -0500

    add the new searching engine for modPair and revise the ModBox class

--- MetaMorpheus/EngineLayer/GlycoSearch/ModBox.cs
+++ MetaMorpheus/EngineLayer/GlycoSearch/ModBox.cs
@@ -1,14 +1,18 @@
-﻿namespace EngineLayer
+﻿using System.Collections.Generic;
+using System.Linq;
+using Omics.Modifications;
+
+namespace EngineLayer
 {
     public class ModBox //The superclass of GlycanBox
     {
         //One peptide can have several modifications. The combined modifications are grouped as a modification box. Used for localization. 
         //ModBox -- a defined combination of modifications will be considered to modify on one peptide. The box means the combined group of modification. 
-        public ModBox(int[] ids)
+        public ModBox(int[] ids, bool targetDecoy = true)
         {
             ModIds = ids;
             NumberOfMods = ids.Length;
-            TargetDecoy = true;
+            TargetDecoy = targetDecoy;
         }
 
         public int[] ModIds { get;  }
@@ -20,6 +24,62 @@
         public double DecoyMass { get; set; }
 
         public bool TargetDecoy { get; set; }
+
+        public static Modification[] GlobalModifications { get; set; }
+
+        public ModBox[] ChildModBoxes { get; set; }
+
+
+        public static IEnumerable<ModBox> BuildModBoxes(int maxNum,  bool buildDecoy = false)
+        {
+            for (int i = 1; i <= maxNum; i++)
+            {
+                foreach (var idCombine in Glycan.GetKCombsWithRept(Enumerable.Range(0, GlobalModifications.Length), i))
+                {
+                    ModBox modBox = new ModBox(idCombine.ToArray());
+                    modBox.TargetDecoy = true;
+                    modBox.ChildModBoxes = BuildChildModBoxes(modBox.NumberOfMods, modBox.ModIds, modBox.TargetDecoy).ToArray();
+
+                    yield return modBox;
+
+                    if (buildDecoy)
+                    {
+                        ModBox modBox_decoy = new ModBox(idCombine.ToArray(), false); // decoy glycanBox
+                        modBox_decoy.TargetDecoy = false;
+                        modBox_decoy.ChildModBoxes = BuildChildModBoxes(modBox.NumberOfMods, modBox.ModIds, modBox.TargetDecoy).ToArray();
+                        yield return modBox_decoy;
+                    }
+                }
+            }
+        }
+
+        public static IEnumerable<ModBox> BuildChildModBoxes(int maxNum, int[] modIds, bool targetDecoy = true)
+        {
+            yield return new GlycanBox(new int[0], targetDecoy);
+            HashSet<string> seen = new HashSet<string>();
+            for (int i = 1; i <= maxNum; i++)
+            {
+                foreach (var idCombine in Glycan.GetKCombs(Enumerable.Range(0, maxNum), i)) //get all combinations of glycans on the peptide, ex. we have three glycosite and three glycan maybe on that (A,B,C) 
+                {                                                                           //the combination of glycans on the peptide can be (A),(A+B),(A+C),(B+C),(A+B+C) totally six 
+                    List<int> ids = new List<int>();
+                    foreach (var id in idCombine)
+                    {
+                        ids.Add(modIds[id]);
+                    }
+
+                    if (!seen.Contains(string.Join(",", ids.Select(p => p.ToString()))))
+                    {
+                        seen.Add(string.Join(",", ids.Select(p => p.ToString())));
+
+                        ModBox modBox = new ModBox(ids.ToArray(), targetDecoy);
+
+                        yield return modBox;
+                    }
+
+                }
+            }
+        }
+
 
     }
 }
---
+++ MetaMorpheus/EngineLayer/ModSearch/ModSearchEngine.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using EngineLayer.GlycoSearch;
+using EngineLayer.ModernSearch;
+using MzLibUtil;
+using Proteomics.ProteolyticDigestion;
+
+namespace EngineLayer.ModSearch
+{
+    public class ModSearchEngine : ModernSearchEngine
+    {
+        protected readonly List<GlycoSpectralMatch>[] GlobalMsms; // Global mod spectral matches
+
+        // For general modification settings
+        private readonly int TopN; // DDA top Peak number
+        private readonly int MaxModNumber;
+        private readonly Tolerance PrecursorSearchMode;
+        private readonly MassDiffAcceptor ProductSearchMode;
+        private readonly List<int>[] SecondFragmentIndex;
+
+        // For glyco settings
+        private readonly bool OxoniumIonFilter;
+        private readonly GlycoSearchType GlycoSearchType;
+        private readonly string OGlycanDatabaseFile;
+        private readonly string NGlycanDatabaseFile;
+
+        public ModSearchEngine(List<GlycoSpectralMatch>[] globalCsms, Ms2ScanWithSpecificMass[] listOfSortedms2Scans, List<PeptideWithSetModifications> peptideIndex,
+            List<int>[] fragmentIndex, List<int>[] secondFragmentIndex, int currentPartition, CommonParameters commonParameters, List<(string fileName, CommonParameters fileSpecificParameters)> fileSpecificParameters,
+            string oglycanDatabase, string nglycanDatabase, GlycoSearchType glycoSearchType, int modSearchTopNum, int maxModNum, bool oxoniumIonFilter, List<string> nestedIds)
+            : base(null, listOfSortedms2Scans, peptideIndex, fragmentIndex, currentPartition, commonParameters, fileSpecificParameters, new OpenSearchMode(), 0, nestedIds)
+        {
+            this.GlobalMsms = globalCsms;
+            this.SecondFragmentIndex = secondFragmentIndex;
+            this.TopN = modSearchTopNum;
+            this.MaxModNumber = maxModNum;
+            this.OxoniumIonFilter = oxoniumIonFilter;
+            this.GlycoSearchType = glycoSearchType;
+            this.OGlycanDatabaseFile = oglycanDatabase;
+            this.NGlycanDatabaseFile = nglycanDatabase;
+
+            this.SecondFragmentIndex = secondFragmentIndex;
+            this.ProductSearchMode = new SinglePpmAroundZeroSearchMode(20); //For ScanOxoniumIonFilter only
+            this.PrecursorSearchMode = commonParameters.PrecursorMassTolerance;
+
+            //Load glycan databases and build the modBox 
+
+        }
+
+
+
+    }
+}
