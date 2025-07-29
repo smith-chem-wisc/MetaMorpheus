ebfaf63820266d889677e6e2593e42f4a3f3a834
Zhuoxin Shi <zshi94@wisc.edu>
2025-07-29 17:36:12 -0500

    XicSplineForAllXics() removed Precursor/FragmentRank removed

--- MetaMorpheus/EngineLayer/DIA/PrecursorFragmentPair.cs
+++ MetaMorpheus/EngineLayer/DIA/PrecursorFragmentPair.cs
@@ -15,9 +15,6 @@ namespace EngineLayer.DIA
         public ExtractedIonChromatogram FragmentXic { get; set; }
         public double? Correlation { get; set; }
         public double? Overlap { get; set; } 
-        public int? PrecursorRank { get; set; }
-        public int? FragmentRank { get; set; }
-
         public PrecursorFragmentPair(ExtractedIonChromatogram precursorXic, ExtractedIonChromatogram fragmentXic, double? correlation = null, double? overlap = null)
         {
             PrecursorXic = precursorXic;
