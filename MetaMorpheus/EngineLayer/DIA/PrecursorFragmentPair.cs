f68b416ad310430db0c8ecd89994a2f8f54d73f9
Zhuoxin Shi <zshi94@wisc.edu>
2025-08-07 10:44:01 -0500

    sharedXic

--- MetaMorpheus/EngineLayer/DIA/PrecursorFragmentPair.cs
+++ MetaMorpheus/EngineLayer/DIA/PrecursorFragmentPair.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.InteropServices;
 using System.Text;
 using System.Threading.Tasks;
 using MassSpectrometry;
@@ -49,6 +50,55 @@ namespace EngineLayer.DIA
                     fragmentPairMap[fragmentXic][i].PrecursorRank = i + 1;
                 }
             }
+        }
+
+        public static double CalculateSharedXIC(ExtractedIonChromatogram xic1, ExtractedIonChromatogram xic2)
+        {
+            if (xic1.EndScanIndex <= xic2.StartScanIndex || xic2.EndScanIndex <= xic1.StartScanIndex)
+            {
+                return 0;
+            }
+
+            var overlapStart = Math.Max(xic1.StartScanIndex, xic2.StartScanIndex);
+            var overlapEnd = Math.Min(xic1.EndScanIndex, xic2.EndScanIndex);
+            var maxLength = overlapEnd - overlapStart + 1;
+
+            var overlapArea = new List<(double, double)>();
+            var scanCycles1 = xic1.Peaks.Select(p => p.ZeroBasedScanIndex).ToArray();
+            var scanCycles2 = xic2.Peaks.Select(p => p.ZeroBasedScanIndex).ToArray();
+            var index1 = Array.BinarySearch(scanCycles1, overlapStart);
+            var index2 = Array.BinarySearch(scanCycles2, overlapStart);
+
+            for (int i = 0; i < maxLength - 1; i++)
+            {
+                double diff0 = xic1.NormalizedPeakIntensities[index1 + i] - xic2.NormalizedPeakIntensities[index2 + i];
+                double diff1 = xic1.NormalizedPeakIntensities[index1 + i + 1] - xic2.NormalizedPeakIntensities[index2 + i + 1];
+
+                overlapArea.Add((overlapStart + i, Math.Min(xic1.NormalizedPeakIntensities[index1 + i], xic2.NormalizedPeakIntensities[index2 + i])));
+                if (diff0 * diff1 < 0)
+                {
+                    double slope = xic1.NormalizedPeakIntensities[index1 + i + 1] - xic1.NormalizedPeakIntensities[index1 + i];
+                    double y = xic1.NormalizedPeakIntensities[index1 + i] + slope * Math.Abs(diff0 / (diff1 - diff0));
+                    overlapArea.Add((overlapStart + i + Math.Abs(diff0 / (diff1 - diff0)), y));
+                }
+            }
+            double overlapAUC = CalculateNormalizedArea(overlapArea);
+            return overlapAUC;
+        }
+
+        public static double CalculateNormalizedArea(List<(double, double)> data)
+        {
+            double area = data[0].Item2 / 2;
+            for (int i = 1; i < data.Count; i++)
+            {
+                double x1 = data[i - 1].Item1;
+                double x2 = data[i].Item1;
+                double y1 = data[i - 1].Item2;
+                double y2 = data[i].Item2;
+                area += (x2 - x1) * (y1 + y2) / 2;
+            }
+            area += data[data.Count - 1].Item2 / 2;
+            return area;
         }
     }
 }
