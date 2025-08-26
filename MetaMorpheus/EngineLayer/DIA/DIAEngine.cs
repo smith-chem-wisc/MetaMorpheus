c3210b2a3caa2ac52ddc141af60303ee129e7bf3
Zhuoxin Shi <zshi94@wisc.edu>
2025-08-26 16:39:15 -0500

    pfGroupIndex++;

--- MetaMorpheus/EngineLayer/DIA/DIAEngine.cs
+++ MetaMorpheus/EngineLayer/DIA/DIAEngine.cs
@@ -56,6 +56,7 @@ namespace EngineLayer.DIA
                 pfGroup.PFgroupIndex = pfGroupIndex;
                 var pseudoScan = PrecursorFragmentsGroup.GetPseudoMs2ScanFromPfGroup(pfGroup, DIAparams.PseudoMs2ConstructionType, CommonParameters, DataFile.FilePath);
                 PseudoMs2Scans.Add(pseudoScan);
+                pfGroupIndex++;
             }
             return new MetaMorpheusEngineResults(this);
         }
