505f163ec1e8260d4a4d94d93e042bc140425732
nbollis <nbollis@comcast.net>
2025-09-19 11:36:07 -0500

    comments

--- MetaMorpheus/EngineLayer/SpectrumMatch/DisambiguationEngine.cs
+++ MetaMorpheus/EngineLayer/SpectrumMatch/DisambiguationEngine.cs
@@ -44,8 +44,6 @@ public class DisambiguationEngine : MetaMorpheusEngine
             FdrAnalysisEngine.DoFalseDiscoveryRateAnalysis(_allSpectralMatches, false, FileSpecificParameters, null, null);
         }
 
-
-
         Status("Done.");
         return new DisambiguationEngineResults(this)
         {
