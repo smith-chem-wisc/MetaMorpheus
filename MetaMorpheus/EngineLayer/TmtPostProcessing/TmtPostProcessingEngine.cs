31b4601abeebe30e9e4ca8aaa51a6b4cd6742679
trishorts <mshort@chem.wisc.edu>
2025-11-21 12:41:19 -0600

    tmt post processing a start

---
+++ MetaMorpheus/EngineLayer/TmtPostProcessing/TmtPostProcessingEngine.cs
@@ -0,0 +1,34 @@
+﻿using EngineLayer.FdrAnalysis;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EngineLayer.TmtPostProcessing
+{
+    public class TmtPostProcessingEngine : MetaMorpheusEngine
+    {
+        private readonly string AnalysisType;
+        public TmtPostProcessingEngine(CommonParameters commonParameters, List<(string FileName, CommonParameters Parameters)> fileSpecificParameters, List<string> nestedIds) : base(commonParameters, fileSpecificParameters, nestedIds)
+        {
+        }
+
+        protected override MetaMorpheusEngineResults RunSpecific()
+        {
+            TmtPostProcessingResults myAnalysisResults = new TmtPostProcessingResults(this, AnalysisType);
+
+            Status("Running TMT Post-processing...");
+            ProcessTmtData(myAnalysisResults);
+            Status("Done.");
+            //myAnalysisResults.PsmsWithin1PercentFdr = AllPsms.Count(b => b.FdrInfo.QValue <= 0.01 && !b.IsDecoy);
+
+            return myAnalysisResults;
+        }
+
+        private void ProcessTmtData(TmtPostProcessingResults myAnalysisResults)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ MetaMorpheus/EngineLayer/TmtPostProcessing/TmtPostProcessingResults.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EngineLayer.TmtPostProcessing
+{
+    public class TmtPostProcessingResults : MetaMorpheusEngineResults
+    {
+        public TmtPostProcessingResults(MetaMorpheusEngine s, object analysisType) : base(s)
+        {
+        }
+        public override string ToString()
+        {
+            var sb = new StringBuilder();
+            //sb.AppendLine(base.ToString());
+            //sb.AppendLine($"{AnalysisType}s within 1% FDR: {PsmsWithin1PercentFdr.ToString()}");
+            //sb.AppendLine($"Delta Score Used for FDR Analysis: {DeltaScoreImprovement.ToString()}");
+            //sb.AppendLine(BinarySearchTreeMetrics);
+            return sb.ToString();
+        }
+    }
+}
