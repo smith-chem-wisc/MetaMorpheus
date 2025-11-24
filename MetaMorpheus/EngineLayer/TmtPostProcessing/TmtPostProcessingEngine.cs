3a0c1f5e2da97702a1056b5edea14abf27d96075
trishorts <mshort@chem.wisc.edu>
2025-11-24 10:07:15 -0600

    idk

--- MetaMorpheus/EngineLayer/TmtPostProcessing/TmtPostProcessingEngine.cs
+++ MetaMorpheus/EngineLayer/TmtPostProcessing/TmtPostProcessingEngine.cs
@@ -12,9 +12,19 @@ namespace EngineLayer.TmtPostProcessing
         private List<SpectralMatch> AllPsms;
         private TmtProcessingParams Tmtparams;
         private readonly string AnalysisType;
+        /// <summary>
+        /// Protein parsimony must be completed prior to TMT post-processing and correct protein IDs must be present on PSMs
+        /// </summary>
+        /// <param name="psms"></param>
+        /// <param name="tmtParams"></param>
+        /// <param name="commonParameters"></param>
+        /// <param name="fileSpecificParameters"></param>
+        /// <param name="nestedIdsstring"></param>
+        /// <param name="analysisType"></param>
+        /// <exception cref="ArgumentNullException"></exception>
         public TmtPostProcessingEngine(List<SpectralMatch> psms, TmtProcessingParams tmtParams, CommonParameters commonParameters, List<(string FileName, CommonParameters Parameters)> fileSpecificParameters, List<string> nestedIdsstring, string analysisType = "PSM") : base(commonParameters, fileSpecificParameters, nestedIds)
         {
-            AllPsms = psms.OrderByDescending(p => p).ToList();
+            AllPsms = psms.Where(p=>p != null && p.IsobaricMassTagReporterIonIntensities.Any()).ToList();
             AnalysisType = analysisType;
             Tmtparams = tmtParams;
             if (fileSpecificParameters == null) throw new ArgumentNullException("file specific parameters cannot be null");
@@ -37,8 +47,55 @@ namespace EngineLayer.TmtPostProcessing
         {
             // Stop if canceled
             if (GlobalVariables.StopLoops) { return; }
-            
+            ConstructIntensityTable();
+            EliminateLowIntensityPsms();
+            NormalizeLoading();
+            NormalizeToReference();
+            SelectPsmForProteinQuant();
             throw new NotImplementedException();
+        }
+        private static void ConstructIntensityTable()
+        {
+                       // Stop if canceled
+            if (GlobalVariables.StopLoops) { return; }
+        }
+        private static void EliminateLowIntensityPsms()
+        {
+            // Stop if canceled
+            if (GlobalVariables.StopLoops) { return; }
+
+        }
+        private static void NormalizeLoading()
+        {
+            // Stop if canceled
+            if (GlobalVariables.StopLoops) { return; }
+
+        }
+        private static void NormalizeToReference()
+        {
+            // Stop if canceled
+            if (GlobalVariables.StopLoops) { return; }
+        }
+        private static void SelectPsmForProteinQuant() 
+        { 
+            // Stop if canceled
+            if (GlobalVariables.StopLoops) { return; }
+        }
+        /// <summary>
+        /// PAW Pipeline sums PSMs for peptide quantification
+        /// </summary>
+        private static void SumPsmsForPeptideQuant()
+        {
+            // Stop if canceled
+            if (GlobalVariables.StopLoops) { return; }
+        }
+        /// <summary>
+        /// PAW Pipeline sums peptides for protein quantification
+        /// </summary>
+        private static void SumPeptidesForProteinQuant()
+        {
+            // Stop if canceled
+            if (GlobalVariables.StopLoops) { return; }
         }
     }
 }
