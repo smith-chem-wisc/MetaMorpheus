44d0ff651e28834cab9cb2f80b6041508fe1bd32
nbollis <nbollis@comcast.net>
2026-05-07 18:33:11 -0500

    Test Coverage

--- MetaMorpheus/GuiFunctions/MetaDraw/MetaDrawLogic.cs
+++ MetaMorpheus/GuiFunctions/MetaDraw/MetaDrawLogic.cs
@@ -24,6 +24,7 @@ using Omics.SpectrumMatch;
 using System.Runtime.CompilerServices;
 using Readers.SpectralLibrary;
 using GuiFunctions.MetaDraw;
+using GuiFunctions.Util;
 
 [assembly: InternalsVisibleTo("Test")]
 namespace GuiFunctions
@@ -462,24 +463,6 @@ namespace GuiFunctions
         }
 
         #region Plot Export
-
-        private class SpectralMatchComparer : IEqualityComparer<SpectrumMatchFromTsv>
-        {
-            public static SpectralMatchComparer Instance = new();
-            public bool Equals(SpectrumMatchFromTsv x, SpectrumMatchFromTsv y)
-            {
-                if (ReferenceEquals(x, y)) return true;
-                if (x is null) return false;
-                if (y is null) return false;
-                if (x.GetType() != y.GetType()) return false;
-                return x.FullSequence == y.FullSequence && x.Ms2ScanNumber == y.Ms2ScanNumber && x.FileNameWithoutExtension == y.FileNameWithoutExtension && x.PrecursorScanNum == y.PrecursorScanNum;
-            }
-
-            public int GetHashCode(SpectrumMatchFromTsv obj)
-            {
-                return HashCode.Combine(obj.FullSequence, obj.Ms2ScanNumber, obj.FileNameWithoutExtension, obj.PrecursorScanNum);
-            }
-        }
 
         public void ExportPlot(PlotView plotView, Canvas stationaryCanvas, List<SpectrumMatchFromTsv> spectrumMatches,
             ParentChildScanPlotsView parentChildScanPlotsView, string directory, out List<string> errors,
