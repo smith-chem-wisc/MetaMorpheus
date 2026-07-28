5d5757f76b7fa0b2e54284d32388463a8178d14b
nbollis <nbollis@comcast.net>
2026-07-28 13:51:54 -0500

    UI View Model Complete

--- MetaMorpheus/GUI/Util/Converters/BooleanToVisibilityConverter.cs
+++ MetaMorpheus/GUI/Util/Converters/BooleanToVisibilityConverter.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Globalization;
 using System.Linq;
@@ -26,6 +26,44 @@ namespace MetaMorpheusGUI
                 return true;
             }
             return false;
+        }
+    }
+
+    public class InverseBoolConverter : BaseValueConverter<InverseBoolConverter>
+    {
+        public override object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value is bool b)
+                return !b;
+            return value;
+        }
+
+        public override object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value is bool b)
+                return !b;
+            return value;
+        }
+    }
+
+    public class BooleanToInvertedVisibilityConverter : BaseValueConverter<BooleanToInvertedVisibilityConverter>
+    {
+        public override object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value is bool && (bool)value)
+            {
+                return Visibility.Collapsed;
+            }
+            return Visibility.Visible;
+        }
+
+        public override object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value is Visibility && (Visibility)value == Visibility.Visible)
+            {
+                return false;
+            }
+            return true;
         }
     }
 }
--- MetaMorpheus/TaskLayer/SearchTask/MassDiffAcceptorType.cs
+++ MetaMorpheus/TaskLayer/SearchTask/MassDiffAcceptorType.cs
@@ -1,4 +1,7 @@
-﻿namespace TaskLayer
+﻿using System;
+using System.Data.Entity.Core.Objects.DataClasses;
+
+namespace TaskLayer
 {
     public enum MassDiffAcceptorType
     {
@@ -9,6 +12,36 @@
         ModOpen,
         Open,
         Custom,
-        PlusOrMinusThreeMM
+        PlusOrMinusThreeMM,
+        MostAbundant_Exact,
+        MostAbundant_PlusMinusOne,
+        MostAbundant_PlusMinusTwo
+    }
+
+    public static class MassDiffAcceptorTypeExtensions
+    {
+        public static bool IsMostAbundant(this MassDiffAcceptorType type)
+        {
+            switch (type)
+            {
+                case MassDiffAcceptorType.Exact:
+                case MassDiffAcceptorType.OneMM:
+                case MassDiffAcceptorType.TwoMM:
+                case MassDiffAcceptorType.ThreeMM:
+                case MassDiffAcceptorType.ModOpen:
+                case MassDiffAcceptorType.Open:
+                case MassDiffAcceptorType.Custom:
+                case MassDiffAcceptorType.PlusOrMinusThreeMM:
+                    return false;
+
+                case MassDiffAcceptorType.MostAbundant_Exact:
+                case MassDiffAcceptorType.MostAbundant_PlusMinusOne:
+                case MassDiffAcceptorType.MostAbundant_PlusMinusTwo:
+                    return true;
+
+                    default:
+                    throw new ArgumentException($"Unknown MassDiffAcceptorType: {type}");
+            }
+        }
     }
 }
