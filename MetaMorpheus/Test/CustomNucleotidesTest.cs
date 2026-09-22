6559084e7967636196fcdd766936c97f035fd589
nbollis <nbollis@comcast.net>
2026-09-22 11:57:45 -0500

    custom nucleotide

---
+++ MetaMorpheus/Test/CustomNucleotidesTest.cs
@@ -0,0 +1,36 @@
+﻿using EngineLayer;
+using NUnit.Framework;
+using System.IO;
+using Transcriptomics;
+
+namespace Test
+{
+    [TestFixture]
+    public static class CustomNucleotidesTest
+    {
+        [Test]
+        public static void TestCustomNucleotideReading()
+        {
+            string nucleotidePath = Path.Combine(GlobalVariables.DataDir, "CustomNucleotides", "CustomNucleotides.txt");
+            string[] originalLines = File.ReadAllLines(nucleotidePath);
+            string[] updatedLines = new string[originalLines.Length + 1];
+            originalLines.CopyTo(updatedLines, 0);
+            updatedLines[^1] = "TestNucleotide\tq\tTnt\tC5H5N2O2";
+            File.WriteAllLines(nucleotidePath, updatedLines);
+
+            try
+            {
+                GlobalVariables.LoadCustomNucleotides();
+
+                Assert.That(Nucleotide.TryGetResidue('q', out Nucleotide nucleotide), Is.True);
+                Assert.That(nucleotide.Name, Is.EqualTo("TestNucleotide"));
+                Assert.That(nucleotide.Symbol, Is.EqualTo("Tnt"));
+                Assert.That(nucleotide.BaseChemicalFormula.Formula, Is.EqualTo("C5H5N2O2"));
+            }
+            finally
+            {
+                File.WriteAllLines(nucleotidePath, originalLines);
+            }
+        }
+    }
+}
